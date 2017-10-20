e36110ae161324526f1d9a20a65e050c719ce0b2
webeweb <support@webeweb.fr>
2017-10-20 13:14:00 +0200

    Add HookUtility

---
+++ Tests/Utility/HookUtilityTest.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * This file is part of the WBWCoreLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Utility;
+
+use Exception;
+use PHPUnit_Framework_TestCase;
+use ReflectionClass;
+use ReflectionMethod;
+use WBW\Library\Core\Utility\HookUtility;
+
+/**
+ * Hook utility test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Utility
+ * @final
+ */
+final class HookUtilityTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Test the getHooks() method.
+     */
+    public function testGetHooks() {
+
+        $classpath = getcwd() . "/Tests/Utility/";
+        $namespace = "WBW\\Library\\Core\\Tests\\Utility\\";
+
+        $hooks1 = HookUtility::getHooks($classpath, $namespace);
+        foreach ($hooks1 as $current) {
+
+            $this->assertEquals($classpath, $current["classpath"], "The method getHooks() does not return the expected class path");
+            $this->assertEquals($namespace, $current["namespace"], "The method getHooks() does not return the expected namespace");
+            $this->assertStringEndsWith(".php", $current["filename"], "The method getHooks() does not return the expected filename");
+            $this->assertContainsOnlyInstancesOf(ReflectionClass::class, [$current["class"]], "The methog getHooks() does not return the expected class");
+            $this->assertNull($current["method"]);
+        }
+
+        $hooks2 = HookUtility::getHooks($classpath, $namespace, "/HookUtilityTest/");
+        $this->assertCount(1, $hooks2, "The method getHooks() does not return the expected hooks with classname");
+        foreach ($hooks2 as $current) {
+
+            $this->assertEquals($classpath, $current["classpath"], "The method getHooks() does not return the expected class path with classname");
+            $this->assertEquals($namespace, $current["namespace"], "The method getHooks() does not return the expected namespace with classname");
+            $this->assertEquals("HookUtilityTest.php", $current["filename"], "The method getHooks() does not return the expected filename with classname");
+            $this->assertContainsOnlyInstancesOf(ReflectionClass::class, [$current["class"]], "The methog getHooks() does not return the expected class with classname");
+            $this->assertNull($current["method"]);
+        }
+
+        $hooks3 = HookUtility::getHooks($classpath, $namespace, "/HookUtilityTest/", PHPUnit_Framework_TestCase::class);
+        $this->assertCount(1, $hooks3, "The method getHooks() does not return the expected hooks with extends");
+        foreach ($hooks3 as $current) {
+
+            $this->assertEquals($classpath, $current["classpath"], "The method getHooks() does not return the expected class path with extends");
+            $this->assertEquals($namespace, $current["namespace"], "The method getHooks() does not return the expected namespace with extends");
+            $this->assertEquals("HookUtilityTest.php", $current["filename"], "The method getHooks() does not return the expected filename with extends");
+            $this->assertContainsOnlyInstancesOf(ReflectionClass::class, [$current["class"]], "The methog getHooks() does not return the expected class with extends");
+            $this->assertNull($current["method"]);
+        }
+
+        $hooks4 = HookUtility::getHooks($classpath, $namespace, "/HookUtilityTest/", PHPUnit_Framework_TestCase::class, "testGetHooks");
+        $this->assertCount(1, $hooks4, "The method getHooks() does not return the expected hooks with method");
+        foreach ($hooks4 as $current) {
+
+            $this->assertEquals($classpath, $current["classpath"], "The method getHooks() does not return the expected class path with method");
+            $this->assertEquals($namespace, $current["namespace"], "The method getHooks() does not return the expected namespace with method");
+            $this->assertEquals("HookUtilityTest.php", $current["filename"], "The method getHooks() does not return the expected filename with method");
+            $this->assertContainsOnlyInstancesOf(ReflectionClass::class, [$current["class"]], "The methog getHooks() does not return the expected class with method");
+            $this->assertContainsOnlyInstancesOf(ReflectionMethod::class, [$current["method"]]);
+        }
+
+        try {
+            HookUtility::getHooks($classpath, $namespace, "/HookUtilityTest/", PHPUnit_Framework_TestCase::class, "testGetHook");
+        } catch (Exception $ex) {
+            $this->assertEquals("The hook method \"testGetHook\" is not found", $ex->getMessage(), "The method getHooks() does not return the expected exception");
+        }
+    }
+
+}
---
+++ Utility/HookUtility.php
@@ -0,0 +1,105 @@
+<?php
+
+/*
+ * This file is part of the WBWCoreLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Utility;
+
+use ReflectionClass;
+use ReflectionException;
+use WBW\Library\Core\Exception\Hook\HookClassNotFoundException;
+use WBW\Library\Core\Exception\Hook\HookMethodNotFoundException;
+use WBW\Library\Core\Exception\Hook\HookSyntaxErrorException;
+
+/**
+ * File utility.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Utility
+ * @final
+ */
+final class HookUtility {
+
+    /**
+     * Get the hooks.
+     *
+     * @param type $classpath The class path.
+     * @param type $namespace The namespace.
+     * @param type $classname The class name.
+     * @param type $method The class method.
+     * @return array Returns the hooks array.
+     * @throws HookClassNotFoundException Throws a hook class not found if the classname is not found.
+     */
+    public static function getHooks($classpath, $namespace, $classname = null, $extends = null, $method = null) {
+
+        //
+        $hooks = [];
+
+        // Get the filenames.
+        $filenames = FileUtility::getFileNames($classpath, ".php");
+
+        // Handle each filenames.
+        foreach ($filenames as $filename) {
+
+            // Check the class name.
+            if (!(is_null($classname) || preg_match($classname, $filename) !== 0)) {
+                continue;
+            }
+
+            // Import the class.
+            try {
+                require_once $classpath . "/" . $filename;
+            } catch (Exception $ex) {
+                throw new HookSyntaxErrorException($classpath . "/" . $filename);
+            }
+
+            // Init. the complete class name.
+            $completeClassname = $namespace . basename($filename, ".php");
+
+            try {
+                $rc = new ReflectionClass($completeClassname);
+            } catch (ReflectionException $ex) {
+                throw new HookClassNotFoundException($completeClassname);
+            }
+
+            // Check the extends.
+            if (!(is_null($extends) || $rc->isSubclassOf($extends))) {
+                continue;
+            }
+
+            // Initialize the hook.
+            $hook = [];
+
+            $hook["classpath"] = $classpath;
+            $hook["namespace"] = $namespace;
+            $hook["filename"]  = $filename;
+            $hook["class"]     = $rc;
+            $hook["method"]    = null;
+
+            $hooks[] = $hook;
+
+            // Check the method.
+            if (is_null($method)) {
+                continue;
+            }
+
+            try {
+                $rm = $rc->getMethod($method);
+
+                $hooks[count($hooks) - 1]["method"] = $rm;
+            } catch (ReflectionException $ex) {
+                throw new HookMethodNotFoundException($method);
+            }
+        }
+
+        // Returns the hooks.
+        return $hooks;
+    }
+
+}
