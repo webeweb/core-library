7a4ce1b032bbbd8c4fd385deb681f939c947d4c5
webeweb <support@webeweb.fr>
2017-10-20 09:29:41 +0200

    Add FileUtility

---
+++ Tests/Utility/FileUtilityTest.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the WBWCoreLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Utility;
+
+use Exception;
+use PHPUnit_Framework_TestCase;
+use WBW\Library\Core\Exception\File\FileNotFoundException;
+use WBW\Library\Core\Utility\FileUtility;
+
+/**
+ * File utility.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Utility
+ * @final
+ */
+final class FileUtilityTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Test the getContents() method.
+     *
+     * @return void
+     */
+    public function testGetContents() {
+
+        $filename = getcwd() . "/Tests/Utility/FileUtilityTest.txt";
+
+        $this->assertEquals("FileUtilityTest", FileUtility::getContents($filename), 'The method getContents() does not return the expected content');
+
+        try {
+            FileUtility::getContents("FileNotFoundException");
+        } catch (Exception $ex) {
+            $this->assertInstanceOf(FileNotFoundException::class, $ex, "The method getContents() does not return the expected exception");
+            $this->assertEquals("The file \"FileNotFoundException\" is not found", $ex->getMessage(), "The method getMessage() does not return the expected string");
+        }
+    }
+
+    /**
+     * Test the getFilenames() method.
+     *
+     * @return void
+     */
+    public function testGetFilenames() {
+
+        $pathname = getcwd() . "/Tests/Utility";
+
+        $this->assertContains("FileUtilityTest.php", FileUtility::getFilenames($pathname), "The method getFilenames() does not return the expected filenames");
+        $this->assertEquals(["FileUtilityTest.txt"], FileUtility::getFilenames($pathname, ".txt"), "The method getFilenames() does not return the expected filenames with extension");
+    }
+
+}
---
+++ Utility/FileUtility.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * This file is part of the WBWCoreLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Utility;
+
+use WBW\Library\Core\Exception\File\FileNotFoundException;
+
+/**
+ * File utility.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Utility
+ * @final
+ */
+final class FileUtility {
+
+    /**
+     * Constructor.
+     */
+    private function __construct() {
+        // NOTHING TO DO.
+    }
+
+    /**
+     * Get a file contents.
+     *
+     * @param string $filename The filename.
+     * @return string Returns the file contents.
+     * @throws FileNotFoundException Throws a file not found exception if the file does not exists.
+     */
+    public static function getContents($filename) {
+        if (file_exists($filename)) {
+            return file_get_contents($filename);
+        } else {
+            throw new FileNotFoundException($filename);
+        }
+    }
+
+    /**
+     * Get the filenames.
+     *
+     * @param string $pathname The pathname.
+     * @param string $extension The file extension.
+     * @return array Returns the filenames.
+     */
+    public static function getFilenames($pathname, $extension = null) {
+
+        // Initialize the filenames.
+        $filenames = [];
+
+        // Check if the directory exists.
+        if (!file_exists($pathname)) {
+            return $filenames;
+        }
+
+        // Open the directory.
+        if (($directory = opendir($pathname)) !== false) {
+
+            // Initialize the offset.
+            $offset = strlen($extension);
+
+            // Read the directory.
+            while (($file = readdir($directory)) !== false) {
+
+                // Determines if the file should be added.
+                if ($file !== "." && $file !== ".." && ((is_null($extension)) || substr_compare($file, $extension, -$offset) === 0)) {
+                    $filenames[] = $file;
+                }
+            }
+
+            // Close the directory.
+            closedir($directory);
+        }
+
+        // Return the filenames.
+        return $filenames;
+    }
+
+}
