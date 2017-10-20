305c9f76a9463927fd3913179f752db62ff14f73
webeweb <support@webeweb.fr>
2017-10-20 15:25:41 +0200

    Add argument interface

---
+++ Argument/ArgumentInterface.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * This file is part of the WBWCoreLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Argument;
+
+/**
+ * Argument interface.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Argument
+ */
+interface ArgumentInterface {
+
+    /**
+     * Type "Array".
+     */
+    const TYPE_ARRAY = 63;
+
+    /**
+     * Type "Boolean".
+     */
+    const TYPE_BOOLEAN = 64;
+
+    /**
+     * Type "Date".
+     */
+    const TYPE_DATE = 30;
+
+    /**
+     * Type "Double".
+     */
+    const TYPE_DOUBLE = 59;
+
+    /**
+     * Type "Float".
+     */
+    const TYPE_FLOAT = 54;
+
+    /**
+     * Type "Integer".
+     */
+    const TYPE_INTEGER = 78;
+
+    /**
+     * Type "Number".
+     */
+    const TYPE_NUMBER = 73;
+
+    /**
+     * Type "Object".
+     */
+    const TYPE_OBJECT = 55;
+
+    /**
+     * Type "Resource".
+     */
+    const TYPE_RESOURCE = 104;
+
+    /**
+     * Type "String".
+     */
+    const TYPE_STRING = 87;
+
+    /**
+     * Type "Timestamp".
+     */
+    const TYPE_TIMESTAMP = 116;
+
+}
---
+++ Tests/Argument/ArgumentInterfaceTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the WBWCoreLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Argument;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Argument interface test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Argument
+ * @final
+ */
+final class ArgumentInterfaceTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Test the __construct() method.
+     *
+     * @return void
+     */ public function testConstruct() {
+
+        $this->assertEquals(63, ArgumentInterface::TYPE_ARRAY, "The constant TYPE_ARRAY does not return the expecetd value");
+        $this->assertEquals(64, ArgumentInterface::TYPE_BOOLEAN, "The constant TYPE_BOOLEAN does not return the expecetd value");
+        $this->assertEquals(30, ArgumentInterface::TYPE_DATE, "The constant TYPE_DATE does not return the expecetd value");
+        $this->assertEquals(59, ArgumentInterface::TYPE_DOUBLE, "The constant TYPE_DOUBLE does not return the expecetd value");
+        $this->assertEquals(54, ArgumentInterface::TYPE_FLOAT, "The constant TYPE_FLOAT does not return the expecetd value");
+        $this->assertEquals(78, ArgumentInterface::TYPE_INTEGER, "The constant TYPE_INTEGER does not return the expecetd value");
+        $this->assertEquals(73, ArgumentInterface::TYPE_NUMBER, "The constant TYPE_NUMBER does not return the expecetd value");
+        $this->assertEquals(55, ArgumentInterface::TYPE_OBJECT, "The constant TYPE_OBJECT does not return the expecetd value");
+        $this->assertEquals(104, ArgumentInterface::TYPE_RESOURCE, "The constant TYPE_RESOURCE does not return the expecetd value");
+        $this->assertEquals(87, ArgumentInterface::TYPE_STRING, "The constant TYPE_STRING does not return the expecetd value");
+        $this->assertEquals(116, ArgumentInterface::TYPE_TIMESTAMP, "The constant TYPE_TIMESTAMP does not return the expecetd value");
+    }
+
+}
