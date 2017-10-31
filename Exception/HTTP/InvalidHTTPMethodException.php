7a462b188686feb99cee6a9341225b9a304cc434
webeweb <support@webeweb.fr>
2017-10-31 09:30:32 +0100

    Add Invalid HTTP method exception

---
+++ Exception/HTTP/InvalidHTTPMethodException.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the WBWCoreLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Exception\HTTP;
+
+use Exception;
+use WBW\Library\Core\Exception\AbstractWBWException;
+
+/**
+ * Invalid HTTP method exception.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Exception\HTTP
+ * @final
+ */
+final class InvalidHTTPMethodException extends AbstractWBWException {
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $method The method.
+	 * @param Exception $previous The previous exception.
+	 */
+	public function __construct($method, Exception $previous = null) {
+		parent::__construct("The HTTP method \"" . $method . "\" is not found", $previous);
+	}
+
+}
---
+++ Tests/Exception/HTTP/InvaliHTTPMethodExceptionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the WBWCoreLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Exception\HTTP;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Library\Core\Exception\HTTP\InvalidHTTPMethodException;
+
+/**
+ * Invalid HTTP method exception test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Exception\HTTP
+ * @final
+ */
+final class InvaliHTTPMethodExceptionTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Tests the __construct() method.
+	 */
+	public function testConstruct() {
+
+		$ex = new InvalidHTTPMethodException("exception");
+
+		$res = "The HTTP method \"exception\" is invalid";
+		$this->assertEquals($res, $ex->getMessage(), "The method getMessage() does not return the expected string");
+	}
+
+}
