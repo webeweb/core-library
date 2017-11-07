9c55c1d9a6dcf95e7743b98787da161bdb212ebf
webeweb <support@webeweb.fr>
2017-11-07 15:00:31 +0100

    Add string utility

---
+++ Tests/Utility/StringUtilityTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the WBWCoreLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Utility;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Library\Core\Utility\StringUtility;
+
+/**
+ * String utility test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Utility
+ * @final
+ */
+final class StringUtilityTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Tests the parseBoolean() method.
+	 *
+	 * @return void
+	 */
+	public function testParseBoolean() {
+
+		$this->assertEquals("false", StringUtility::parseBoolean(null), "The method parseBoolean() does not return the expected value with \"null\"");
+		$this->assertEquals("false", StringUtility::parseBoolean(false), "The method parseBoolean() does not return the expected value with \"false\"");
+		$this->assertEquals("true", StringUtility::parseBoolean(true), "The method parseBoolean() does not return the expected value with \"true\"");
+	}
+
+}
---
+++ Utility/StringUtility.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the WBWCoreLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Utility;
+
+/**
+ * String utility.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Utility
+ * @final
+ */
+final class StringUtility {
+
+	/**
+	 * Parse a boolean.
+	 *
+	 * @param boolean $value The boolean value.
+	 * @return string Returns "true" in case of success, "false" otherwise.
+	 */
+	public static function parseBoolean($value) {
+		return $value === true ? "true" : "false";
+	}
+
+}
