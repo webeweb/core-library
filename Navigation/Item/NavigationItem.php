68950b63ef15b92719aca82a27820fb8d1933840
webeweb <support@webeweb.fr>
2017-11-08 12:30:14 +0100

    Add navigation item

---
+++ Navigation/Item/NavigationItem.php
@@ -0,0 +1,167 @@
+<?php
+
+/*
+ * This file is part of the WBWCoreLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Navigation\Item;
+
+use WBW\Library\Core\Node\AbstractNode;
+
+/**
+ * Navigation item.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Navigation\Item
+ * @final
+ * @deprecated
+ */
+final class NavigationItem extends AbstractNode {
+
+	/**
+	 * Href.
+	 *
+	 * @var string
+	 */
+	private $href;
+
+	/**
+	 * Icon.
+	 *
+	 * @var string
+	 */
+	private $icon;
+
+	/**
+	 * Span.
+	 *
+	 * @var string
+	 */
+	private $span;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $span The span.
+	 * @param string $icon The icon.
+	 * @param string $href The href.
+	 */
+	public function __construct($span, $icon = null, $href = "javascript: void(0);") {
+		parent::__construct($span);
+		$this->span	 = $span;
+		$this->icon	 = $icon;
+		$this->href	 = $href;
+	}
+
+	/**
+	 * Get the href.
+	 *
+	 * @return string Returns the href.
+	 */
+	public function getHref() {
+		return $this->href;
+	}
+
+	/**
+	 * Get the icon.
+	 *
+	 * @return string Returns the icon.
+	 */
+	public function getIcon() {
+		return $this->icon;
+	}
+
+	/**
+	 * Get the items.
+	 *
+	 * @return NavigationItem[] Returns the items.
+	 */
+	public function getItems() {
+		return $this->_items;
+	}
+
+	/**
+	 * Get the span.
+	 *
+	 * @return string Returns the span.
+	 */
+	public function getSpan() {
+		return $this->span;
+	}
+
+	/**
+	 * Set the href.
+	 *
+	 * @param string $href The href.
+	 * @return NavigationItem Returns the navigation item.
+	 */
+	public function setHref($href) {
+		$this->href = $href;
+		return $this;
+	}
+
+	/**
+	 * Set the icon.
+	 *
+	 * @param string $icon The icon.
+	 * @return NavigationItem Returns the navigation item.
+	 */
+	public function setIcon($icon) {
+		$this->icon = $icon;
+		return $this;
+	}
+
+	/**
+	 * Set the span.
+	 *
+	 * @param string $span The span.
+	 * @return NavigationItem Returns the navigation item.
+	 */
+	public function setSpan($span) {
+		$this->span = $span;
+		return $this;
+	}
+
+	/**
+	 * Convert into an array representing this instance.
+	 *
+	 * @return array Returns an array representing this instance.
+	 */
+	public function toArray() {
+
+		// Initialize the output.
+		$output = [];
+
+		// Check the href.
+		if (!is_null($this->href)) {
+			$output["href"] = $this->href;
+		}
+
+		// Check the icon.
+		if (!is_null($this->icon)) {
+			$output["icon"] = $this->icon;
+		}
+
+		// Check the span.
+		if (!is_null($this->span)) {
+			$output["span"] = $this->span;
+		}
+
+		// Check the nodes.
+		if (0 < $this->size()) {
+			$output["subitems"] = [];
+			foreach ($this->getNodes() as $current) {
+				$output["subitems"][] = $current->toArray();
+			}
+		}
+
+		// Return the output.
+		return $output;
+	}
+
+}
---
+++ Tests/Navigation/Item/NavigationItemTest.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of the WBWCoreLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Navigation\Item;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Library\Core\Navigation\Item\NavigationItem;
+
+/**
+ * Navigation item test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Navigation\Item
+ * @final
+ */
+final class NavigationItemTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Tests the __construct() method.
+	 *
+	 * @return void
+	 */
+	public function testConstructor() {
+
+		$obj = new NavigationItem("id");
+
+		$this->assertEquals("id", $obj->getId(), "The method getId() does not return the expected value");
+
+		$this->assertEquals(null, $obj->getFirstNode(), "The method getFirstNode() does not return the expected value");
+		$this->assertEquals(null, $obj->getLastNode(), "The method getLastNode() does not return the expected value");
+		$this->assertEquals([], $obj->getNodes(), "The method getNodes() does not return the expected value");
+		$this->assertEquals(null, $obj->getParent(), "The method getParent() does not return the expected value");
+
+		$this->assertEquals("id", $obj->getSpan(), "The method getSpan() does not return the expected value");
+		$this->assertEquals("javascript: void(0);", $obj->getHref(), "The method getHref() does not return the expected value");
+		$this->assertEquals(null, $obj->getIcon(), "The method getIcon() does not return the expected value");
+	}
+
+	/**
+	 * Tests the addNode() method.
+	 *
+	 * @return void.
+	 */
+	public function testAddNode() {
+
+		$obj = new NavigationItem("id");
+		$add = new NavigationItem("id2");
+
+		$this->assertEquals(0, $obj->size(), "The method size() does no return the expected value");
+
+		$obj->addNode($add);
+
+		$this->assertEquals(1, $obj->size(), "The method size() does no return the expected value");
+		$this->assertEquals($add, $obj->getFirstNode(), "The method getFirstNode() does not return the expecetd value");
+		$this->assertEquals($add, $obj->getLastNode(), "The method getLastNode() does not return the expecetd value");
+		$this->assertEquals($add, $obj->getNodeAt(0), "The method getNodeAt() does not return the expecetd value");
+		$this->assertEquals($add, $obj->getNodeById("id2"), "The method getNodeById() does not return the expecetd value");
+		$this->assertEquals(null, $obj->getNodeById("exception"), "The method getNodeById() does not return the expecetd value");
+	}
+
+}
