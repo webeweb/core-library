2668560634a60a91049386ccbe707492aaf423cd
webeweb <support@webeweb.fr>
2017-11-17 12:11:44 +0100

    Add Alphabetical tree sort interface

---
+++ Sort/Tree/Alphabetical/AlphabeticalTreeSortInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Sort\Tree\Alphabetical;
+
+/**
+ * Alphabetical tree sort interface.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Sort\Tree\Alphabetical
+ */
+interface AlphabeticalTreeSortInterface {
+
+	/**
+	 * Get the label.
+	 *
+	 * @return string Returns the label.
+	 */
+	public function getLabel();
+
+	/**
+	 * Get the parent.
+	 *
+	 * @return AlphabeticalTreeSortInterface Returns the parent.
+	 */
+	public function getParent();
+}
