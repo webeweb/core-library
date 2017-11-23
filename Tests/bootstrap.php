de1c871df8846937f4f72c1d3ca12936781140e6
webeweb <support@webeweb.fr>
2017-11-23 09:06:46 +0100

    Checkstyle

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the core-library package.
  *
@@ -7,7 +8,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 if (file_exists($file = __DIR__ . "/autoload.php")) {
 	require_once $file;
 } elseif (file_exists($file = __DIR__ . "/autoload.php.dist")) {
