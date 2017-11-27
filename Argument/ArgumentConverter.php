f3aa75931b79ab9eaa57072a0b571616e4547e8d
webeweb <support@webeweb.fr>
2017-11-27 11:25:23 +0100

    Add date/time support

--- Argument/ArgumentConverter.php
+++ Argument/ArgumentConverter.php
@@ -11,7 +11,10 @@
 
 namespace WBW\Library\Core\Argument;
 
+use DateTime;
+use WBW\Library\Core\Exception\Argument\DateArgumentException;
 use WBW\Library\Core\Exception\Argument\IllegalArgumentException;
+use WBW\Library\Core\Exception\Pointer\NullPointerException;
 use WBW\Library\Core\Utility\BooleanUtility;
 use WBW\Library\Core\Utility\DoubleUtility;
 use WBW\Library\Core\Utility\FloatUtility;
@@ -31,19 +34,39 @@ final class ArgumentConverter implements ArgumentInterface {
 	 *
 	 * @param string $value The string value.
 	 * @param integer $type The type.
+	 * @param string $type The type.
 	 * @return mixed Returns the value.
 	 * @throws IllegalArgumentException Throws an illegal argument exception if the value is not of expected type.
+	 * @throws NullPointerException Throws a null pointer exception if the type is date and the date format is null.
 	 */
-	public static function convert($value, $type) {
+	public static function convert($value, $type, $dateFormat = null) {
 		switch ($type) {
 			case self::TYPE_BOOLEAN:
 				return BooleanUtility::parseString($value);
+			case self::TYPE_DATE:
+				if (is_null($dateFormat)) {
+					throw new NullPointerException("The date format is null");
+				}
+				$datetime = DateTime::createFromFormat($dateFormat, $value);
+				if ($datetime === false) {
+					throw new DateArgumentException($value);
+				}
+				return $datetime;
 			case self::TYPE_DOUBLE:
 				return DoubleUtility::parseString($value);
 			case self::TYPE_FLOAT:
 				return FloatUtility::parseString($value);
 			case self::TYPE_INTEGER:
 				return IntegerUtility::parseString($value);
+			case self::TYPE_TIMESTAMP:
+				if (is_null($dateFormat)) {
+					throw new NullPointerException("The datetime format is null");
+				}
+				$datetime = DateTime::createFromFormat($dateFormat, $value);
+				if ($datetime === false) {
+					throw new DateArgumentException($value);
+				}
+				return $datetime;
 			default:
 				throw new IllegalArgumentException("The type \"" . $type . "\" is not implemented");
 		}
--- Tests/Argument/ArgumentConverterTest.php
+++ Tests/Argument/ArgumentConverterTest.php
@@ -11,11 +11,13 @@
 
 namespace WBW\Library\Core\Tests\Argument;
 
+use DateTime;
 use Exception;
 use PHPUnit_Framework_TestCase;
 use WBW\Library\Core\Argument\ArgumentConverter;
 use WBW\Library\Core\Argument\ArgumentInterface;
 use WBW\Library\Core\Exception\Argument\IllegalArgumentException;
+use WBW\Library\Core\Exception\Pointer\NullPointerException;
 
 /**
  * Argument converter test.
@@ -43,8 +45,8 @@ final class ArgumentConverterTest extends PHPUnit_Framework_TestCase {
 		try {
 			ArgumentConverter::convert(null, ArgumentInterface::TYPE_DATE);
 		} catch (Exception $ex) {
-			$this->assertInstanceOf(IllegalArgumentException::class, $ex);
-			$this->assertEquals("The type \"30\" is not implemented", $ex->getMessage());
+			$this->assertInstanceOf(NullPointerException::class, $ex);
+			$this->assertEquals("The date format is null", $ex->getMessage());
 		}
 
 		try {
@@ -52,6 +54,13 @@ final class ArgumentConverterTest extends PHPUnit_Framework_TestCase {
 		} catch (Exception $ex) {
 			$this->assertInstanceOf(IllegalArgumentException::class, $ex);
 			$this->assertEquals("The type \"73\" is not implemented", $ex->getMessage());
+		}
+
+		try {
+			ArgumentConverter::convert(null, ArgumentInterface::TYPE_OBJECT);
+		} catch (Exception $ex) {
+			$this->assertInstanceOf(IllegalArgumentException::class, $ex);
+			$this->assertEquals("The type \"55\" is not implemented", $ex->getMessage());
 		}
 
 		try {
@@ -68,10 +77,19 @@ final class ArgumentConverterTest extends PHPUnit_Framework_TestCase {
 			$this->assertEquals("The type \"87\" is not implemented", $ex->getMessage());
 		}
 
+		try {
+			ArgumentConverter::convert(null, ArgumentInterface::TYPE_TIMESTAMP);
+		} catch (Exception $ex) {
+			$this->assertInstanceOf(NullPointerException::class, $ex);
+			$this->assertEquals("The datetime format is null", $ex->getMessage());
+		}
+
 		$this->assertEquals(true, ArgumentConverter::convert("1", ArgumentInterface::TYPE_BOOLEAN));
+		$this->assertInstanceOf(DateTime::class, ArgumentConverter::convert("2017-11-27", ArgumentInterface::TYPE_DATE, "Y-m-d"));
 		$this->assertEquals(1, ArgumentConverter::convert("1", ArgumentInterface::TYPE_DOUBLE));
 		$this->assertEquals(1, ArgumentConverter::convert("1", ArgumentInterface::TYPE_FLOAT));
 		$this->assertEquals(1, ArgumentConverter::convert("1", ArgumentInterface::TYPE_INTEGER));
+		$this->assertInstanceOf(DateTime::class, ArgumentConverter::convert("2017-11-27 11:20:00", ArgumentInterface::TYPE_DATE, "Y-m-d H:m:s"));
 	}
 
 }
