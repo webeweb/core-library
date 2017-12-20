1dc7996b2b076fba436f9c14d1370032799bc959
webeweb <support@webeweb.fr>
2017-12-20 09:42:34 +0100

    Add new methods to manipulate directories

--- Tests/Utility/DirectoryUtilityTest.php
+++ Tests/Utility/DirectoryUtilityTest.php
@@ -24,6 +24,21 @@ use WBW\Library\Core\Utility\DirectoryUtility;
 final class DirectoryUtilityTest extends PHPUnit_Framework_TestCase {
 
 	/**
+	 * Tests the create() method.
+	 *
+	 * @return void
+	 */
+	public function testCreate() {
+
+		$arg1	 = getcwd() . "/phpunit";
+		$arg2	 = $arg1 . "/unittest";
+
+		$this->assertEquals(true, DirectoryUtility::create($arg1));
+		$this->assertEquals(null, DirectoryUtility::create($arg1));
+		$this->assertEquals(true, DirectoryUtility::create($arg2));
+	}
+
+	/**
 	 * Tests the isEmpty() method.
 	 *
 	 * @return void
@@ -32,6 +47,38 @@ final class DirectoryUtilityTest extends PHPUnit_Framework_TestCase {
 
 		$this->assertEquals(null, DirectoryUtility::isEmpty("exception"));
 		$this->assertEquals(false, DirectoryUtility::isEmpty(getcwd()));
+	}
+
+	/**
+	 * Tests the rename() method.
+	 *
+	 * @return void
+	 * @depends testCreate
+	 */
+	public function testRename() {
+
+		$arg1	 = getcwd() . "/phpunit";
+		$arg2	 = $arg1 . "/unittest";
+
+		$this->assertEquals(true, DirectoryUtility::rename($arg2, $arg2 . "2"));
+		$this->assertEquals(null, DirectoryUtility::rename($arg2, $arg2 . "2"));
+		$this->assertEquals(null, DirectoryUtility::rename($arg2 . "2", $arg1));
+	}
+
+	/**
+	 * Tests the delete() method.
+	 *
+	 * @return void
+	 * @depends testRename
+	 */
+	public function testDelete() {
+
+		$arg1	 = getcwd() . "/phpunit";
+		$arg2	 = $arg1 . "/unittest2";
+
+		$this->assertEquals(null, DirectoryUtility::delete($arg1));
+		$this->assertEquals(true, DirectoryUtility::delete($arg2));
+		$this->assertEquals(true, DirectoryUtility::delete($arg1));
 	}
 
 }
--- Utility/DirectoryUtility.php
+++ Utility/DirectoryUtility.php
@@ -21,6 +21,32 @@ namespace WBW\Library\Core\Utility;
 final class DirectoryUtility {
 
 	/**
+	 * Create a directory.
+	 *
+	 * @param string $dirname The directory name.
+	 * @return boolean Returns true in case of success, false otherwise or null if the directory exists.
+	 */
+	public static function create($dirname) {
+		if (file_exists($dirname) === true) {
+			return null;
+		}
+		return mkdir($dirname);
+	}
+
+	/**
+	 * Delete a directory.
+	 *
+	 * @param string $dirname The directory name.
+	 * @return boolean Returns true in case of success, false otherwise or null if the directory can't be deleted.
+	 */
+	public static function delete($dirname) {
+		if (self::isEmpty($dirname) !== true) {
+			return null;
+		}
+		return rmdir($dirname);
+	}
+
+	/**
 	 * Determines if a directory is empty.
 	 *
 	 * @param string $dirname The directory name.
@@ -31,6 +57,20 @@ final class DirectoryUtility {
 			return null;
 		}
 		return (count(scandir($dirname)) == 2);
+	}
+
+	/**
+	 * Rename a directory.
+	 *
+	 * @param string $oldname The old directory name.
+	 * @param string $newname The new directory name.
+	 * @return boolean Returns true in case of success, false otherwise or null if the directory can't be renamed.
+	 */
+	public static function rename($oldname, $newname) {
+		if (file_exists($oldname) === false || file_exists($newname) === true) {
+			return null;
+		}
+		return rename($oldname, $newname);
 	}
 
 }
