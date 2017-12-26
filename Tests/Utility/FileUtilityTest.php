2fd52c3c5a27515c5ff2435c51d8536d5671f2bf
webeweb <support@webeweb.fr>
2017-12-26 13:05:36 +0100

    Add formatSize() method

--- Tests/Utility/FileUtilityTest.php
+++ Tests/Utility/FileUtilityTest.php
@@ -27,6 +27,33 @@ use WBW\Library\Core\Utility\FileUtility;
 final class FileUtilityTest extends PHPUnit_Framework_TestCase {
 
 	/**
+	 * Tests the formatSize() method.
+	 *
+	 * @return void
+	 */
+	public function testFormatSize() {
+
+		$this->assertEquals("99.00 B", FileUtility::formatSize(99));
+		$this->assertEquals("1.00 KB", FileUtility::formatSize(1000));
+		$this->assertEquals("1.00 MB", FileUtility::formatSize(1000000));
+		$this->assertEquals("1.00 GB", FileUtility::formatSize(1000000000));
+		$this->assertEquals("1.00 TB", FileUtility::formatSize(1000000000000));
+		$this->assertEquals("1.00 PB", FileUtility::formatSize(1000000000000000));
+		$this->assertEquals("1.00 EB", FileUtility::formatSize(1000000000000000000));
+		$this->assertEquals("1.00 ZB", FileUtility::formatSize(1000000000000000000000));
+		$this->assertEquals("1.00 YB", FileUtility::formatSize(1000000000000000000000000));
+
+		$this->assertEquals("0.099 KB", FileUtility::formatSize(99, "KB", 3));
+		$this->assertEquals("0.001 MB", FileUtility::formatSize(1000, "MB", 3));
+		$this->assertEquals("0.001 GB", FileUtility::formatSize(1000000, "GB", 3));
+		$this->assertEquals("0.001 TB", FileUtility::formatSize(1000000000, "TB", 3));
+		$this->assertEquals("0.001 PB", FileUtility::formatSize(1000000000000, "PB", 3));
+		$this->assertEquals("0.001 EB", FileUtility::formatSize(1000000000000000, "EB", 3));
+		$this->assertEquals("0.001 ZB", FileUtility::formatSize(1000000000000000000, "ZB", 3));
+		$this->assertEquals("0.001 YB", FileUtility::formatSize(1000000000000000000000, "YB", 3));
+	}
+
+	/**
 	 * Tests the getContents() method.
 	 *
 	 * @return void
@@ -58,11 +85,39 @@ final class FileUtilityTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals(["FileUtilityTest.txt"], FileUtility::getFilenames($pathname, ".txt"));
 
 		try {
-			FileUtility::getFilenames("DirectoryNotFoundException");
+			FileUtility::getFilenames("exception");
 		} catch (Exception $ex) {
 			$this->assertInstanceOf(DirectoryNotFoundException::class, $ex);
-			$this->assertEquals("The directory \"DirectoryNotFoundException\" is not found", $ex->getMessage());
+			$this->assertEquals("The directory \"exception\" is not found", $ex->getMessage());
 		}
+	}
+
+	/**
+	 * Tests the getSize() method.
+	 *
+	 * @return void
+	 */
+	public function testGetSize() {
+
+		$this->assertEquals(15, FileUtility::getSize(getcwd() . "/Tests/Utility/FileUtilityTest.txt"));
+
+		try {
+			FileUtility::getSize("exception");
+		} catch (Exception $ex) {
+			$this->assertInstanceOf(FileNotFoundException::class, $ex);
+			$this->assertEquals("The file \"exception\" is not found", $ex->getMessage());
+		}
+	}
+
+	/**
+	 * Tests the getUnits() method.
+	 *
+	 * @return void
+	 */
+	public function testGetUnits() {
+
+		$res = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+		$this->assertEquals($res, FileUtility::getUnits());
 	}
 
 }
--- Utility/FileUtility.php
+++ Utility/FileUtility.php
@@ -11,8 +11,10 @@
 
 namespace WBW\Library\Core\Utility;
 
+use WBW\Library\Core\Exception\Argument\IllegalArgumentException;
 use WBW\Library\Core\Exception\Directory\DirectoryNotFoundException;
 use WBW\Library\Core\Exception\File\FileNotFoundException;
+use WBW\Library\Core\File\FileSizeInterface;
 
 /**
  * File utility.
@@ -21,7 +23,39 @@ use WBW\Library\Core\Exception\File\FileNotFoundException;
  * @package WBW\Library\Core\Utility
  * @final
  */
-final class FileUtility {
+final class FileUtility implements FileSizeInterface {
+
+	/**
+	 * Format a size.
+	 *
+	 * @param  $size The size.
+	 * @param string $unit The unit.
+	 * @return string Returns the formated size.
+	 */
+	public static function formatSize($size, $unit = null, $decimals = 2) {
+
+
+		// Initialize the units.
+		$units = self::getUnits();
+
+		// Find the unit.
+		$index = array_search($unit, $units);
+		if (is_null($unit) === false && $index === false) {
+			throw new IllegalArgumentException("The unit \"" . $unit . "\" does not exists");
+		}
+
+		// Initialize the output.
+		$output = $size;
+
+		$iteration = 0;
+		while (1000 <= $output || $iteration < $index) {
+			$output /= 1000;
+			++$iteration;
+		}
+
+		// Return the output.
+		return implode(" ", [sprintf("%." . $decimals . "f", $output), $units[$iteration]]);
+	}
 
 	/**
 	 * Get a file contents.
@@ -76,6 +110,40 @@ final class FileUtility {
 
 		// Return the filenames.
 		return $filenames;
+	}
+
+	/**
+	 * Get a file size.
+	 *
+	 * @param string $filename The filename.
+	 * @return integer Returns the file size.
+	 * @throws FileNotFoundException Throws a File not found exception if the file does not exists.
+	 */
+	public static function getSize($filename) {
+		if (file_exists($filename) === false) {
+			throw new FileNotFoundException($filename);
+		}
+		clearstatcache();
+		return filesize($filename);
+	}
+
+	/**
+	 * Get the units.
+	 *
+	 * @return array Returns the units.
+	 */
+	public static function getUnits() {
+		return [
+			self::FILE_SIZE_UNIT_B,
+			self::FILE_SIZE_UNIT_KB,
+			self::FILE_SIZE_UNIT_MB,
+			self::FILE_SIZE_UNIT_GB,
+			self::FILE_SIZE_UNIT_TB,
+			self::FILE_SIZE_UNIT_PB,
+			self::FILE_SIZE_UNIT_EB,
+			self::FILE_SIZE_UNIT_ZB,
+			self::FILE_SIZE_UNIT_YB,
+		];
 	}
 
 }
