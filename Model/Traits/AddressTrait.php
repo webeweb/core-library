c783ca040500e4de7769ac77c260f120620deca7
webeweb <support@webeweb.fr>
2018-01-08 11:20:53 +0100

    Add Address trait

---
+++ Model/Traits/AddressTrait.php
@@ -0,0 +1,211 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Model\Traits;
+
+/**
+ * AddressTrait.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Model\Traits
+ */
+trait AddressTrait {
+
+	/**
+	 * Address.
+	 *
+	 * @var string
+	 */
+	private $address;
+
+	/**
+	 * City.
+	 *
+	 * @var string
+	 */
+	private $city;
+
+	/**
+	 * Company.
+	 *
+	 * @var string
+	 */
+	private $company;
+
+	/**
+	 * Country.
+	 *
+	 * @var string
+	 */
+	private $country;
+
+	/**
+	 * Firstname.
+	 *
+	 * @var string
+	 */
+	private $firstname;
+
+	/**
+	 * Lastname.
+	 *
+	 * @var string
+	 */
+	private $lastname;
+
+	/**
+	 * Postal code.
+	 *
+	 * @var string
+	 */
+	private $postalCode;
+
+	/**
+	 * Get the address.
+	 *
+	 * @return string Returns the address.
+	 */
+	public function getAddress() {
+		return $this->address;
+	}
+
+	/**
+	 * Get the city.
+	 *
+	 * @return string Retuns the city.
+	 */
+	public function getCity() {
+		return $this->city;
+	}
+
+	/**
+	 * Get the company.
+	 *
+	 * @return string Returns the company.
+	 */
+	public function getCompany() {
+		return $this->company;
+	}
+
+	/**
+	 * Get the country.
+	 *
+	 * @return string Returns the country.
+	 */
+	public function getCountry() {
+		return $this->country;
+	}
+
+	/**
+	 * Get the firstname.
+	 *
+	 * @return string Returns the firstname.
+	 */
+	public function getFirstname() {
+		return $this->firstname;
+	}
+
+	/**
+	 * Get the lastname.
+	 *
+	 * @return string Returns the lastname.
+	 */
+	public function getLastname() {
+		return $this->lastname;
+	}
+
+	/**
+	 * Get the postal code.
+	 *
+	 * @return string Returns the postal code.
+	 */
+	public function getPostalCode() {
+		return $this->postalCode;
+	}
+
+	/**
+	 * Set the address.
+	 *
+	 * @param string $address The address
+	 * @return AddressTrait Returns the address.
+	 */
+	public function setAddress($address) {
+		$this->address = $address;
+		return $this;
+	}
+
+	/**
+	 * Set the city.
+	 *
+	 * @param string $city The city.
+	 * @return AddressTrait Returns the address.
+	 */
+	public function setCity($city) {
+		$this->city = $city;
+		return $this;
+	}
+
+	/**
+	 * Set the company.
+	 *
+	 * @param string $company The company.
+	 * @return AddressTrait Returns the address.
+	 */
+	public function setCompany($company) {
+		$this->company = $company;
+		return $this;
+	}
+
+	/**
+	 * Set the country.
+	 *
+	 * @param string $country The country.
+	 * @return AddressTrait Returns the address.
+	 */
+	public function setCountry($country) {
+		$this->country = $country;
+		return $this;
+	}
+
+	/**
+	 * Set the firstname.
+	 *
+	 * @param string $firstname The firstname.
+	 * @return AddressTrait Returns the address.
+	 */
+	public function setFirstname($firstname) {
+		$this->firstname = $firstname;
+		return $this;
+	}
+
+	/**
+	 * Set the lastname.
+	 *
+	 * @param string $lastname The lastname.
+	 * @return AddressTrait Returns the address.
+	 */
+	public function setLastname($lastname) {
+		$this->lastname = $lastname;
+		return $this;
+	}
+
+	/**
+	 * Set the postal code.
+	 *
+	 * @param string $postalCode The postal code.
+	 * @return AddressTrait Returns the address.
+	 */
+	public function setPostalCode($postalCode) {
+		$this->postalCode = $postalCode;
+		return $this;
+	}
+
+}
---
+++ Tests/Model/Traits/AddressTraitTest.php
@@ -0,0 +1,134 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Model\Traits;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Library\Core\Model\Traits\AddressTrait;
+
+/**
+ * Address trait test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Model\Traits
+ * @final
+ */
+final class AddressTraitTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Address.
+	 *
+	 * @var AddressTrait
+	 */
+	private $address;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function setUp() {
+
+		$this->address = $this->getObjectForTrait(AddressTrait::class);
+	}
+
+	/**
+	 * Tests the __constructor() method.
+	 *
+	 * @return void
+	 */
+	public function testConstructor() {
+
+		$this->assertEquals(null, $this->address->getAddress());
+		$this->assertEquals(null, $this->address->getCity());
+		$this->assertEquals(null, $this->address->getCompany());
+		$this->assertEquals(null, $this->address->getCountry());
+		$this->assertEquals(null, $this->address->getFirstname());
+		$this->assertEquals(null, $this->address->getLastname());
+		$this->assertEquals(null, $this->address->getPostalCode());
+	}
+
+	/**
+	 * Tests the setAddress() method.
+	 *
+	 * @return void
+	 */
+	public function testSetAddress() {
+
+		$this->address->setAddress("address");
+		$this->assertEquals("address", $this->address->getAddress());
+	}
+
+	/**
+	 * Tests the setCity() method.
+	 *
+	 * @return void
+	 */
+	public function testSetCity() {
+
+		$this->address->setCity("city");
+		$this->assertEquals("city", $this->address->getCity());
+	}
+
+	/**
+	 * Tests the setCompany() method.
+	 *
+	 * @return void
+	 */
+	public function testSetCompany() {
+
+		$this->address->setCompany("company");
+		$this->assertEquals("company", $this->address->getCompany());
+	}
+
+	/**
+	 * Tests the setCountry() method.
+	 *
+	 * @return void
+	 */
+	public function testSetCountry() {
+
+		$this->address->setCountry("country");
+		$this->assertEquals("country", $this->address->getCountry());
+	}
+
+	/**
+	 * Tests the setFirstname() method.
+	 *
+	 * @return void
+	 */
+	public function testSetFirstname() {
+
+		$this->address->setFirstname("firstname");
+		$this->assertEquals("firstname", $this->address->getFirstname());
+	}
+
+	/**
+	 * Tests the setLastname() method.
+	 *
+	 * @return void
+	 */
+	public function testSetLastname() {
+
+		$this->address->setLastname("lastname");
+		$this->assertEquals("lastname", $this->address->getLastname());
+	}
+
+	/**
+	 * Tests the setPostalCode() method.
+	 *
+	 * @return void
+	 */
+	public function testSetPostalCode() {
+
+		$this->address->setPostalCode("postalCode");
+		$this->assertEquals("postalCode", $this->address->getPostalCode());
+	}
+
+}
