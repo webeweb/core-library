3ad0baa334d12894c2f723103981233768bbdf60
webeweb <support@webeweb.fr>
2018-01-09 17:26:12 +0100

    Add Phone numbers trait

---
+++ Model/Traits/PhoneNumbersTrait.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Model\Traits;
+
+/**
+ * Phone numbers trait.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Model\Traits
+ */
+trait PhoneNumbersTrait {
+
+	/**
+	 * Mobile number.
+	 *
+	 * @var string
+	 */
+	private $mobileNumber;
+
+	/**
+	 * Phone number.
+	 *
+	 * @var string
+	 */
+	private $phoneNumber;
+
+	/**
+	 * Get the mobile number.
+	 *
+	 * @return string Returns the mobile number.
+	 */
+	public function getMobileNumber() {
+		return $this->mobileNumber;
+	}
+
+	/**
+	 * Get the phone number.
+	 *
+	 * @return string Returns the phone number.
+	 */
+	public function getPhoneNumber() {
+		return $this->phoneNumber;
+	}
+
+	/**
+	 * Set the mobile number.
+	 *
+	 * @param type $mobileNumber
+	 * @return PhoneNumbersTrait Returns the phone.
+	 */
+	public function setMobileNumber($mobileNumber) {
+		$this->mobileNumber = $mobileNumber;
+		return $this;
+	}
+
+	/**
+	 * Set the phone number.
+	 *
+	 * @param string $phoneNumber The phone number.
+	 * @return PhoneNumbersTrait Returns the phone.
+	 */
+	public function setPhoneNumber($phoneNumber) {
+		$this->phoneNumber = $phoneNumber;
+		return $this;
+	}
+
+}
---
+++ Tests/Model/Traits/PhoneNumbersTraitTest.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Model\Traits;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Library\Core\Model\Traits\PhoneNumbersTrait;
+
+/**
+ * Phone numbers trait test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Model\Traits
+ * @final
+ */
+final class PhoneNumbersTraitTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Phone numbers.
+	 *
+	 * @var PhoneNumbersTrait
+	 */
+	private $phoneNumbers;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function setUp() {
+
+		$this->phoneNumbers = $this->getObjectForTrait(PhoneNumbersTrait::class);
+	}
+
+	/**
+	 * Tests the __constructor() method.
+	 *
+	 * @return void
+	 */
+	public function testConstructor() {
+
+		$this->assertEquals(null, $this->phoneNumbers->getMobileNumber());
+		$this->assertEquals(null, $this->phoneNumbers->getPhoneNumber());
+	}
+
+	/**
+	 * Tests the setMobileNumber() method.
+	 *
+	 * @return void
+	 */
+	public function testSetMobileNumber() {
+
+		$this->phoneNumbers->setMobileNumber("mobileNumber");
+		$this->assertEquals("mobileNumber", $this->phoneNumbers->getMobileNumber());
+	}
+
+	/**
+	 * Tests the setPhoneNumber() method.
+	 *
+	 * @return void
+	 */
+	public function testSetCity() {
+
+		$this->phoneNumbers->setPhoneNumber("phoneNumber");
+		$this->assertEquals("phoneNumber", $this->phoneNumbers->getPhoneNumber());
+	}
+
+}
