9928eefa608321d6f6abc9d0211ef9c91752fa3c
webeweb <support@webeweb.fr>
2018-03-02 13:15:51 +0100

    Add Luhn algorithm

---
+++ Algorithm/LuhnAlgorithm.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Algorithm;
+
+/**
+ * Luhn algorithm.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Algorithm
+ * @final
+ */
+final class LuhnAlgorithm {
+
+    /**
+     * Check.
+     *
+     * @param string $str The string.
+     * @return boolean Returns true in case of success, false otherwise.
+     */
+    public static function check($str) {
+
+        // Initialize.
+        $sum    = 0;
+        $length = strlen($str);
+        $parity = $length % 2;
+
+        // Get the sum.
+        $sum += substr($str, $length - 1);
+
+        // Handle each character.
+        for ($i = $length - 2; 0 <= $i; --$i) {
+
+            // Get the digit.
+            $digit = intval(substr($str, $i, 1));
+
+            // Check the parity.
+            if ($parity === $i % 2) {
+                $digit *= 2;
+            }
+            if (9 < $digit) {
+                $digit -= 9;
+            }
+
+            // Add the digit.
+            $sum += $digit;
+        }
+
+        // Return.
+        return 0 === $sum % 10;
+    }
+
+}
---
+++ Tests/Algorithm/LuhnAlgorithmTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Algorithm;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Library\Core\Algorithm\LuhnAlgorithm;
+
+/**
+ * Luhn algorithm test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Algorithm
+ * @final
+ */
+final class LuhnAlgorithmTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Tests the check() method.
+     *
+     * @return void
+     */
+    public function testCheck() {
+
+        $this->assertEquals(true, LuhnAlgorithm::check("51538089700014"));
+        $this->assertEquals(true, LuhnAlgorithm::check("53897790100015"));
+        $this->assertEquals(true, LuhnAlgorithm::check("53395168700011"));
+    }
+
+}
