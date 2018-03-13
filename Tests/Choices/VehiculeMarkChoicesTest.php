adc3d2926e4d9051ad3e401a2986ce84e1e29dee
webeweb <support@webeweb.fr>
2018-03-13 14:33:04 +0100

    Add Vehicule mark choices interface

---
+++ Tests/Choices/VehiculeMarkChoicesTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Choices;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Library\Core\Choices\VehiculeMarkChoices;
+
+/**
+ * Vehicule mark choices test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Choices
+ * @final
+ */
+final class VehiculeMakChoicesTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Tests the getChoices() method.
+     *
+     * @return void
+     */
+    public function testGetChoices() {
+
+        $res = [
+            1  => "Audi",
+            2  => "BMW",
+            3  => "Citroën",
+            4  => "Dacia",
+            5  => "Fiat",
+            6  => "Ford",
+            7  => "Honda",
+            8  => "Hyundai",
+            9  => "Kia",
+            10 => "Lancia",
+            11 => "Land Rover",
+            12 => "Mercedes",
+            13 => "Mni",
+            14 => "Nissan",
+            15 => "Opel",
+            16 => "Peugeot",
+            17 => "Renault",
+            18 => "Rover",
+            19 => "Seat",
+            20 => "Skoda",
+            21 => "Smart",
+            22 => "Toyota",
+            23 => "Volkswagen",
+            24 => "Volvo",
+        ];
+        $this->assertEquals($res, VehiculeMarkChoices::getChoices());
+    }
+
+}
