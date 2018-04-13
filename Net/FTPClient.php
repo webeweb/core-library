39e8d76f8e0b6a5b59ba39e2bd3b64b17eaf3214
webeweb <support@webeweb.fr>
2018-04-13 16:51:15 +0200

    Add FTP client

---
+++ Net/FTPClient.php
@@ -0,0 +1,121 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Net;
+
+use WBW\Library\Core\IO\IOException;
+
+/**
+ * FTP client.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Net
+ */
+class FTPClient {
+
+    /**
+     * Authenticator.
+     *
+     * @var Authenticator
+     */
+    private $authenticator;
+
+    /**
+     * Connection.
+     *
+     * @var ressource.
+     */
+    private $connection;
+
+    /**
+     * Constructor.
+     *
+     * @param Authenticator $authenticator The authenticator.
+     */
+    public function __construct(Authenticator $authenticator) {
+        $this->authenticator = $authenticator;
+    }
+
+    /**
+     * Closes this FTP connection.
+     *
+     * @return FTPClient Returns this FTP client.
+     * @throws IOException Throws an IO exception if an I/O error occurs.
+     */
+    public function close() {
+        if (false === ftp_close($this->connection)) {
+            throw new IOException(sprintf("ftp://%s disconnection failed", $this->authenticator->getHost()));
+        }
+        return $this;
+    }
+
+    /**
+     * Opens this FTP connection.
+     *
+     * @param integer $timeout The timeout.
+     * @return FTPClient Returns this FTP client.
+     * @throws IOException Throws an IO exception if an I/O error occurs.
+     */
+    public function connect($timeout = 90) {
+        if (false === ($this->connection = ftp_connect($this->authenticator->getHost(), $this->authenticator->getPort(), $timeout))) {
+            throw new IOException(sprintf("ftp://%s connection failed", $this->authenticator->getHost()));
+        }
+        return $this;
+    }
+
+    /**
+     * Get the authenticator.
+     *
+     * @return Auhtenticator Returns the authenticator.
+     */
+    public function getAuthenticator() {
+        return $this->authenticator;
+    }
+
+    /**
+     * Logs in to this FTP connection.
+     *
+     * @return FTPClient Returns this FTP client.
+     * @throws IOException Throws an IO exception if an I/O error occurs.
+     */
+    public function login() {
+        if (false === ftp_login($this->connection, $this->authenticator->getPasswordAuthentication()->getUsername(), $this->authenticator->getPasswordAuthentication()->getPassword())) {
+            throw new IOException(sprintf("ftp://%s:%s@%s failed", $this->authenticator->getHost(), $this->authenticator->getPasswordAuthentication()->getUsername(), $this->authenticator->getPasswordAuthentication()->getPassword()));
+        }
+        return $this;
+    }
+
+    /**
+     * Creates a directory.
+     *
+     * @param string $directory The directory.
+     * @return FTPClient Returns this FTP client.
+     * @throws IOException Throws an IO exception if an I/O error occurs.
+     */
+    public function mkdir($directory) {
+        if (false === ftp_mkdir($this->connection, $directory)) {
+            throw new IOException(sprintf("mkdir %s failed", $directory));
+        }
+        return $this;
+    }
+
+    /**
+     * Set the authenticator.
+     *
+     * @param Authenticator $authenticator The authenticator.
+     * @return FTPClient Returns this FTP client.
+     */
+    public function setAuthenticator(Authenticator $authenticator) {
+        $this->authenticator = $authenticator;
+        return $this;
+    }
+
+}
---
+++ Tests/Net/FTPClientTest.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Net;
+
+use Exception;
+use PHPUnit_Framework_TestCase;
+use WBW\Library\Core\IO\IOException;
+use WBW\Library\Core\Net\Authenticator;
+use WBW\Library\Core\Net\FTPClient;
+use WBW\Library\Core\Net\PasswordAuthentication;
+
+/**
+ * FTP client test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Net
+ * @final
+ */
+final class FTPClientTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Authenticator.
+     *
+     * @var Authenticator
+     */
+    private $authenticator;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        // Set a Password authentication.
+        $passwordAuthentication = new PasswordAuthentication("anonymous", "guest");
+
+        // Set the Authenticator mock.
+        $this->authenticator = new Authenticator("speedtest.tele2.net", $passwordAuthentication);
+        $this->authenticator->setPort(21);
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $obj = new FTPClient($this->authenticator);
+
+        $this->assertEquals($this->authenticator, $obj->getAuthenticator());
+    }
+
+    /**
+     * Tests the connect() method.
+     *
+     * @return void
+     */
+    public function testConnect() {
+
+        $obj = new FTPClient($this->authenticator);
+
+        $this->assertEquals($obj, $obj->connect());
+
+        try {
+            $obj->getAuthenticator()->setHost("github.com");
+            $obj->connect(2);
+        } catch (Exception $ex) {
+            $this->assertInstanceOf(IOException::class, $ex);
+            $this->assertEquals("ftp://github.com connection failed", $ex->getMessage());
+        }
+    }
+
+    /**
+     * Tests the login() method.
+     *
+     * @return void
+     * @depends testConnect
+     */
+    public function testLogin() {
+
+        $obj = new FTPClient($this->authenticator);
+        $obj->connect();
+
+        $this->assertEquals($obj, $obj->login());
+    }
+
+    /**
+     * Tests the mkdir() method.
+     *
+     * @return void
+     * @depends testLogin
+     */
+    public function testMkdir() {
+
+        $obj = new FTPClient($this->authenticator);
+        $obj->connect();
+        $obj->login();
+    }
+
+    /**
+     * Tests the close() method.
+     *
+     * @return void
+     * @depends testConnect
+     */
+    public function testClose() {
+
+        $obj = new FTPClient($this->authenticator);
+        $obj->connect();
+
+        $this->assertEquals($obj, $obj->close());
+    }
+
+}
