47362ec255eb294810b4896aba5e0b0e2f1bf213
webeweb <support@webeweb.fr>
2018-04-16 11:02:01 +0200

    Move Date utility

--- Tests/Utility/Argument/DateUtilityTest.php
+++ Tests/Utility/Argument/DateUtilityTest.php
@@ -9,17 +9,17 @@
  * file that was distributed with this source code.
  */
 
-namespace WBW\Library\Core\Tests\Utility;
+namespace WBW\Library\Core\Tests\Utility\Argument;
 
 use DateTime;
 use PHPUnit_Framework_TestCase;
-use WBW\Library\Core\Utility\DateUtility;
+use WBW\Library\Core\Utility\Argument\DateUtility;
 
 /**
  * Date utility test.
  *
  * @author webeweb <https://github.com/webeweb/>
- * @package WBW\Library\Core\Tests\Utility
+ * @package WBW\Library\Core\Tests\Utility\Argument
  * @final
  */
 final class DateUtilityTest extends PHPUnit_Framework_TestCase {
