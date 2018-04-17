95bced5ffe903be19b459a13475c9657045865ef
webeweb <support@webeweb.fr>
2018-04-17 13:39:39 +0200

    Add Not implemented exception

---
+++ Exception/Runtime/NotImplementedException.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Exception\Runtime;
+
+use Exception;
+use WBW\Library\Core\Exception\AbstractCoreException;
+
+/**
+ * Not implemented exception.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Exception\Runtime
+ */
+class NotImplementedException extends AbstractCoreException {
+
+    /**
+     * Constructor.
+     *
+     * @param string $message The message.
+     * @param Exception $previous The previous exception.
+     */
+    public function __construct($message, Exception $previous = null) {
+        parent::__construct($message, $previous);
+    }
+
+}
---
+++ Tests/Exception/Runtime/NotImplementedExceptionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Exception\Runtime;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Library\Core\Exception\Runtime\NotImplementedException;
+
+/**
+ * Not implemented exception test .
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Exception\Runtime
+ * @final
+ */
+final class NotImplementedExceptionTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Tests the __construct() method.
+     */
+    public function testConstruct() {
+
+        $ex = new NotImplementedException("exception");
+
+        $res = "exception";
+        $this->assertEquals($res, $ex->getMessage());
+    }
+
+}
