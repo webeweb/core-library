674066239ebd238c1ffc76a92e0437328887768c
webeweb <support@webeweb.fr>
2018-08-06 18:22:21 +0200

    Reorganize AlphabeticalTreeSort

---
+++ Tests/Sorting/AlphabeticalTreeSortTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Sorting;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Library\Core\Model\Sorting\AlphabeticalTreeNodeInterface;
+use WBW\Library\Core\Sorting\AlphabeticalTreeSort;
+use WBW\Library\Core\Tests\Fixtures\TestFixtures;
+
+/**
+ * Alphabetical tree sort test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Sorting
+ * @final
+ */
+final class AlphabeticalTreeSortTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Nodes.
+     *
+     * @var AlphabeticalTreeNodeInterface[]
+     */
+    private $nodes;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        // Initialize the nodes.
+        $this->nodes = TestFixtures::getTestNodes();
+    }
+
+    /**
+     * Tests the sort() method.
+     *
+     * @return void
+     */
+    public function testSort() {
+
+        $obj = new AlphabeticalTreeSort($this->nodes);
+
+        $obj->sort();
+        $res = $obj->getNodes();
+        $this->assertEquals($this->nodes[0]->getAlphabeticalTreeNodeLabel(), $res[0]->getAlphabeticalTreeNodeLabel());
+        $this->assertEquals($this->nodes[7]->getAlphabeticalTreeNodeLabel(), $res[1]->getAlphabeticalTreeNodeLabel());
+        $this->assertEquals($this->nodes[8]->getAlphabeticalTreeNodeLabel(), $res[2]->getAlphabeticalTreeNodeLabel());
+        $this->assertEquals($this->nodes[9]->getAlphabeticalTreeNodeLabel(), $res[3]->getAlphabeticalTreeNodeLabel());
+        $this->assertEquals($this->nodes[1]->getAlphabeticalTreeNodeLabel(), $res[4]->getAlphabeticalTreeNodeLabel());
+        $this->assertEquals($this->nodes[4]->getAlphabeticalTreeNodeLabel(), $res[5]->getAlphabeticalTreeNodeLabel());
+        $this->assertEquals($this->nodes[2]->getAlphabeticalTreeNodeLabel(), $res[6]->getAlphabeticalTreeNodeLabel());
+        $this->assertEquals($this->nodes[3]->getAlphabeticalTreeNodeLabel(), $res[7]->getAlphabeticalTreeNodeLabel());
+        $this->assertEquals($this->nodes[5]->getAlphabeticalTreeNodeLabel(), $res[8]->getAlphabeticalTreeNodeLabel());
+        $this->assertEquals($this->nodes[6]->getAlphabeticalTreeNodeLabel(), $res[9]->getAlphabeticalTreeNodeLabel());
+    }
+
+}
