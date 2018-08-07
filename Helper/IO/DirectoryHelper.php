0ae87daeedbdedf8e3665efafc4c0c0644b0db49
webeweb <support@webeweb.fr>
2018-08-07 09:16:39 +0200

    Move DirectoryUtility into DirectoryHelper

---
+++ Helper/IO/DirectoryHelper.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Helper\IO;
+
+use WBW\Library\Core\Exception\IO\FileNotFoundException;
+
+/**
+ * Directory helper.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Helper\IO
+ */
+final class DirectoryHelper {
+
+    /**
+     * Create a directory.
+     *
+     * @param string $dirname The directory name.
+     * @param integer $mode The directory mode.
+     * @param boolean $recursive Recursive ?
+     * @return boolean Returns true in case of success, false otherwise or null if the directory exists.
+     */
+    public static function create($dirname, $mode = 0755, $recursive = false) {
+        if (true === file_exists($dirname)) {
+            return null;
+        }
+        return mkdir($dirname, $mode, $recursive);
+    }
+
+    /**
+     * Delete a directory.
+     *
+     * @param string $dirname The directory name.
+     * @return boolean Returns true in case of success, false otherwise or null if the directory can't be deleted.
+     */
+    public static function delete($dirname) {
+        if (true !== self::isEmpty($dirname)) {
+            return null;
+        }
+        return rmdir($dirname);
+    }
+
+    /**
+     * Determines if a directory is empty.
+     *
+     * @param string $dirname The directory name.
+     * @return boolean Returns true in case of success, false otherwise or null if the directory is not readable.
+     */
+    public static function isEmpty($dirname) {
+        if (false === is_readable($dirname)) {
+            return null;
+        }
+        return (count(scandir($dirname)) == 2);
+    }
+
+    /**
+     * Rename a directory.
+     *
+     * @param string $oldDirname The old directory name.
+     * @param string $newDirname The new directory name.
+     * @return boolean Returns true in case of success, false otherwise or null if the new directory name already exists.
+     * @throws FileNotFoundException Throws a file not found exception if the directory does not exists.
+     * @see FileHelper
+     */
+    public static function rename($oldDirname, $newDirname) {
+        return FileHelper::rename($oldDirname, $newDirname);
+    }
+
+}
---
+++ Tests/Helper/IO/DirectoryHelperTest.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Helper\IO;
+
+use Exception;
+use PHPUnit_Framework_TestCase;
+use WBW\Library\Core\Exception\IO\FileNotFoundException;
+use WBW\Library\Core\Helper\IO\DirectoryHelper;
+
+/**
+ * Directory utility test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Helper\IO
+ * @final
+ */
+final class DirectoryHelperTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Tests the create() method.
+     *
+     * @return void
+     */
+    public function testCreate() {
+
+        $arg1 = getcwd() . "/phpunit";
+        $arg2 = $arg1 . "/unittest";
+
+        $this->assertTrue(DirectoryHelper::create($arg1));
+        $this->assertNull(DirectoryHelper::create($arg1));
+        $this->assertTrue(DirectoryHelper::create($arg2));
+    }
+
+    /**
+     * Tests the isEmpty() method.
+     *
+     * @return void
+     * @depends testCreate
+     */
+    public function testIsEmpty() {
+
+        $arg1 = getcwd() . "/phpunit";
+        $arg2 = $arg1 . "/unittest";
+
+        $this->assertNull(DirectoryHelper::isEmpty("exception"));
+        $this->assertFalse(DirectoryHelper::isEmpty($arg1));
+        $this->assertTrue(DirectoryHelper::isEmpty($arg2));
+    }
+
+    /**
+     * Tests the rename() method.
+     *
+     * @return void
+     * @depends testCreate
+     */
+    public function testRename() {
+
+        $arg1 = getcwd() . "/phpunit";
+        $arg2 = $arg1 . "/unittest";
+
+        $this->assertTrue(DirectoryHelper::rename($arg2, $arg2 . "2"));
+
+        try {
+            DirectoryHelper::rename($arg2, $arg2 . "2");
+        } catch (Exception $ex) {
+            $this->assertInstanceOf(FileNotFoundException::class, $ex);
+            $this->assertEquals("The file \"" . $arg2 . "\" is not found", $ex->getMessage());
+        }
+
+        $this->assertNull(DirectoryHelper::rename($arg2 . "2", $arg1));
+    }
+
+    /**
+     * Tests the delete() method.
+     *
+     * @return void
+     * @depends testRename
+     */
+    public function testDelete() {
+
+        $arg1 = getcwd() . "/phpunit";
+        $arg2 = $arg1 . "/unittest2";
+
+        $this->assertNull(DirectoryHelper::delete($arg1));
+        $this->assertTrue(DirectoryHelper::delete($arg2));
+        $this->assertTrue(DirectoryHelper::delete($arg1));
+    }
+
+}
