b241216d126a952d6c2405ab354b99542f7c16e3
webeweb <support@webeweb.fr>
2018-08-07 09:35:27 +0200

    Merge ArgumentConverter and ArgumentValidator into ArgumentHelper

--- Helper/Argument/ArgumentHelper.php
+++ Helper/Argument/ArgumentHelper.php
@@ -3,33 +3,27 @@
 /**
  * This file is part of the core-library package.
  *
- * (c) 2017 WEBEWEB
+ * (c) 2018 WEBEWEB
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-namespace WBW\Library\Core\Argument;
+namespace WBW\Library\Core\Helper\Argument;
 
 use DateTime;
 use WBW\Library\Core\Exception\Argument\DateArgumentException;
 use WBW\Library\Core\Exception\Argument\IllegalArgumentException;
 use WBW\Library\Core\Exception\Argument\TimestampArgumentException;
 use WBW\Library\Core\Exception\Pointer\NullPointerException;
-use WBW\Library\Core\Helper\Argument\ArgumentInterface;
-use WBW\Library\Core\Helper\Argument\BooleanHelper;
-use WBW\Library\Core\Helper\Argument\DoubleHelper;
-use WBW\Library\Core\Helper\Argument\FloatHelper;
-use WBW\Library\Core\Helper\Argument\IntegerHelper;
 
 /**
- * Argument converter.
+ * Argument helper.
  *
  * @author webeweb <https://github.com/webeweb/>
- * @package WBW\Library\Core\Argument
- * @final
+ * @package WBW\Library\Core\Helper\Argument
  */
-final class ArgumentConverter implements ArgumentInterface {
+class ArgumentHelper implements ArgumentInterface {
 
     /**
      * Convert a string value into type $type.
@@ -74,6 +68,54 @@ final class ArgumentConverter implements ArgumentInterface {
             default:
                 throw new IllegalArgumentException("The type \"" . $type . "\" is not implemented");
         }
+    }
+
+    /**
+     * Determines if the $value is of type $type.
+     *
+     * @param mixed $value The value.
+     * @param integer $type The type.
+     * @throws IllegalArgumentException Throws an illegal argument exception if the value is not of expected type.
+     */
+    public static function isTypeOf($value, $type) {
+        switch ($type) {
+            case self::ARGUMENT_ARRAY:
+                ArrayHelper::isArray($value);
+                break;
+            case self::ARGUMENT_BOOLEAN:
+                BooleanHelper::isBoolean($value);
+                break;
+            case self::ARGUMENT_DATE:
+                DateTimeHelper::isDate($value);
+                break;
+            case self::ARGUMENT_DOUBLE:
+                DoubleHelper::isDouble($value);
+                break;
+            case self::ARGUMENT_FLOAT:
+                FloatHelper::isFloat($value);
+                break;
+            case self::ARGUMENT_INTEGER:
+                IntegerHelper::isInteger($value);
+                break;
+            case self::ARGUMENT_NUMBER:
+                NumberHelper::isNumber($value);
+                break;
+            case self::ARGUMENT_OBJECT:
+                ObjectHelper::isObject($value);
+                break;
+            case self::ARGUMENT_RESOURCE:
+                ResourceHelper::isResource($value);
+                break;
+            case self::ARGUMENT_STRING:
+                StringHelper::isString($value);
+                break;
+            case self::ARGUMENT_TIMESTAMP:
+                TimestampHelper::isTimestamp($value);
+                break;
+            default:
+                throw new IllegalArgumentException("The type \"" . $type . "\" is not implemented");
+        }
+        return true;
     }
 
 }
