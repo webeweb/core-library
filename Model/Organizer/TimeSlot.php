88e6f6a22d43cc256488c98f6b8342113be23d6f
webeweb <support@webeweb.fr>
2018-08-22 17:10:33 +0200

    Externalize static methods into Helper class

--- Model/Organizer/TimeSlot.php
+++ Model/Organizer/TimeSlot.php
@@ -12,9 +12,8 @@
 namespace WBW\Library\Core\Model\Organizer;
 
 use DateTime;
-use WBW\Library\Core\Exception\Argument\IllegalArgumentException;
 use WBW\Library\Core\Argument\DateTimeHelper;
-use WBW\Library\Core\Sorting\QuickSort;
+use WBW\Library\Core\Exception\Argument\IllegalArgumentException;
 
 /**
  * Time slot.
@@ -73,104 +72,6 @@ class TimeSlot {
     }
 
     /**
-     * Determines if a time slot A contains a time slot b.
-     *
-     * @param TimeSlot $a The time slot A.
-     * @param TimeSlot $b The time slot B.
-     * @return bool Returns true in case of success, false otherwise.
-     */
-    public static function contains(TimeSlot $a, TimeSlot $b) {
-        $c1 = DateTimeHelper::isBetween($b->getStartDate(), $a->getStartDate(), $a->getEndDate());
-        $c2 = DateTimeHelper::isBetween($b->getEndDate(), $a->getStartDate(), $a->getEndDate());
-        return $c1 && $c2;
-    }
-
-    /**
-     * Determines if two time slots are equals.
-     *
-     * @param TimeSlot $a The time slot A.
-     * @param TimeSlot $b The time slot B.
-     * @return boolean Returns true in case of success, false otherwise.
-     */
-    public static function equals(TimeSlot $a, TimeSlot $b) {
-
-        // Compare the start dates.
-        if (false === DateTimeHelper::equals($a->getStartDate(), $b->getStartDate())) {
-            return false;
-        }
-
-        // Compare the end dates.
-        if (false === DateTimeHelper::equals($a->getEndDate(), $b->getEndDate())) {
-            return false;
-        }
-
-        // Compare the time slots count.
-        if (count($a->getTimeSlots()) !== count($b->getTimeSlots())) {
-            return false;
-        }
-
-        // Handle each time slot.
-        for ($i = count($a->getTimeSlots()) - 1; 0 <= $i; --$i) {
-            if (false === self::equals($a->getTimeSlots()[$i], $b->getTimeSlots()[$i])) {
-                return false;
-            }
-        }
-
-        //
-        return true;
-    }
-
-    /**
-     * Full join two time slots.
-     *
-     * @param TimeSlot $a The time slot A.
-     * @param TimeSlot $b The time slot B.
-     * @return TimeSlot|null Returns a time slot in case of success, null otherwise.
-     */
-    public static function fullJoin(TimeSlot $a, TimeSlot $b) {
-
-        // Has full join ?
-        if (false === self::hasFullJoin($a, $b)) {
-            return null;
-        }
-
-        // Initialize the date/times.
-        $startDate = DateTimeHelper::getSmaller($a->getStartDate(), $b->getStartDate());
-        $endDate   = DateTimeHelper::getGreater($a->getEndDate(), $b->getEndDate());
-
-        // Return the time slot.
-        return new TimeSlot(clone $startDate, clone $endDate);
-    }
-
-    /**
-     * Full join two time slots without time slots intersection.
-     *
-     * @param TimeSlot $a The time slot A.
-     * @param TimeSlot $b The time slot B.
-     * @return TimeSlot[] Returns the time slots in case of success, null otherwise.
-     */
-    public static function fullJoinWithout(TimeSlot $a, TimeSlot $b) {
-
-        // Initialize the time slots.
-        $leftJoins  = self::leftJoinWithout($a, $b);
-        $rightJoins = self::rightJoinWithout($a, $b);
-
-        // Check the time slots.
-        if (null === $leftJoins && null === $rightJoins) {
-            return null;
-        }
-        if (null === $leftJoins) {
-            return $rightJoins;
-        }
-        if (null === $rightJoins) {
-            return $leftJoins;
-        }
-
-        // Return the time slots.
-        return self::sort(array_merge($leftJoins, $rightJoins));
-    }
-
-    /**
      * Get the duration.
      *
      * @return int Returns the duration.
@@ -207,104 +108,6 @@ class TimeSlot {
     }
 
     /**
-     * Determines if a time slot A has full join with time slot B.
-     *
-     * @param TimeSlot $a The time slot A.
-     * @param TimeSlot $b The time slot B.
-     * @return bool Returns true in case of success, false otherwise.
-     */
-    public static function hasFullJoin(TimeSlot $a, TimeSlot $b) {
-        return true === self::hasInnerJoin($a, $b);
-    }
-
-    /**
-     * Determines if a time slot A has an inner join with time slot B.
-     *
-     * @param TimeSlot $a The time slot A.
-     * @param TimeSlot $b The time slot B.
-     * @return bool Returns true in case of success, false otherwise.
-     */
-    public static function hasInnerJoin(TimeSlot $a, TimeSlot $b) {
-        $c1 = DateTimeHelper::isBetween($b->getStartDate(), $a->getStartDate(), $a->getEndDate());
-        $c2 = DateTimeHelper::isBetween($b->getEndDate(), $a->getStartDate(), $a->getEndDate());
-        $c3 = DateTimeHelper::isBetween($a->getStartDate(), $b->getStartDate(), $b->getEndDate());
-        $c4 = DateTimeHelper::isBetween($a->getEndDate(), $b->getStartDate(), $b->getEndDate());
-        return $c1 || $c2 || $c3 || $c4;
-    }
-
-    /**
-     * Inner join two time slots.
-     *
-     * @param TimeSlot $a The time slot A.
-     * @param TimeSlot $b The time slot B.
-     * @return TimeSlot|null Returns a time slot in case of success, null otherwise.
-     */
-    public static function innerJoin(TimeSlot $a, TimeSlot $b) {
-
-        // Has inner join ?
-        if (false === self::hasInnerJoin($a, $b)) {
-            return null;
-        }
-
-        // Initialize the date/times.
-        $startDate = DateTimeHelper::getGreater($a->getStartDate(), $b->getStartDate());
-        $endDate   = DateTimeHelper::getSmaller($a->getEndDate(), $b->getEndDate());
-
-        // Return the time slot.
-        return new TimeSlot(clone $startDate, clone $endDate);
-    }
-
-    /**
-     * Left join two time slots.
-     *
-     * @param TimeSlot $a The time slot A.
-     * @param TimeSlot $b The time slot B.
-     * @return TimeSlot Returns the time slot in case of success, null otherwise.
-     */
-    public static function leftJoin(TimeSlot $a, TimeSlot $b) {
-
-        // Has inner join ?
-        if (false === self::hasInnerJoin($a, $b)) {
-            return null;
-        }
-
-        // Return the time slot.
-        return new TimeSlot(clone $a->getStartDate(), clone $a->getEndDate());
-    }
-
-    /**
-     * Left join two time slots without time slot B intersection.
-     *
-     * @param TimeSlot $a The time slot A.
-     * @param TimeSlot $b The time slot B.
-     * @return TimeSlot[] Returns the time slots in case of success, null otherwise.
-     */
-    public static function leftJoinWithout(TimeSlot $a, TimeSlot $b) {
-
-        // Has inner join ?
-        if (false === self::hasInnerJoin($a, $b) || true === self::contains($b, $a)) {
-            return null;
-        }
-
-        // Contains ?
-        if (true === self::contains($a, $b)) {
-            return self::sort([
-                    new Timeslot(clone $a->getStartDate(), clone $b->getStartDate()),
-                    new Timeslot(clone $b->getEndDate(), clone $a->getEndDate()),
-            ]);
-        }
-
-        // Initialize the date/times.
-        $startDate = true === DateTimeHelper::isLessThan($a->getStartDate(), $b->getStartDate()) ? $a->getStartDate() : $b->getEndDate();
-        $endDate   = true === DateTimeHelper::isGreaterThan($a->getEndDate(), $b->getEndDate()) ? $a->getEndDate() : $b->getStartDate();
-
-        // Return the time slots.
-        return [
-            new TimeSlot(clone $startDate, clone $endDate),
-        ];
-    }
-
-    /**
      * Remove a time slot.
      *
      * @param TimeSlot $timeSlot The time slot.
@@ -312,34 +115,12 @@ class TimeSlot {
      */
     public function removeTimeSlot(TimeSlot $timeSlot) {
         for ($i = count($this->timeSlots) - 1; 0 <= $i; --$i) {
-            if (true === self::equals($timeSlot, $this->timeSlots[$i])) {
+            if (true === TimeSlotHelper::equals($timeSlot, $this->timeSlots[$i])) {
                 unset($this->timeSlots[$i]);
                 break;
             }
         }
         return $this;
-    }
-
-    /**
-     * Right join two time slots.
-     *
-     * @param TimeSlot $a The time slot A.
-     * @param TimeSlot $b The time slot B.
-     * @return TimeSlot Returns the time slot in case of success, null otherwise.
-     */
-    public static function rightJoin(TimeSlot $a, TimeSlot $b) {
-        return self::leftJoin($b, $a);
-    }
-
-    /**
-     * Right join two time slots without time slot B intersection.
-     *
-     * @param TimeSlot $a The time slot A.
-     * @param TimeSlot $b The time slot B.
-     * @return TimeSlot[] Returns the time slots in case of success, null otherwise.
-     */
-    public static function rightJoinWithout(TimeSlot $a, TimeSlot $b) {
-        return self::leftJoinWithout($b, $a);
     }
 
     /**
@@ -373,22 +154,6 @@ class TimeSlot {
     protected function setTimeSlots($timeSlots) {
         $this->timeSlots = $timeSlots;
         return $this;
-    }
-
-    /**
-     * Sort time slots.
-     *
-     * @param TimeSlot[] $timeSlots The time slots.
-     * @return TimeSlot[] Returns the sorted time slots.
-     */
-    public static function sort($timeSlots) {
-
-        // Initialize a Qucik sort.
-        $sorter = new QuickSort($timeSlots, new TimeSlotFunctor());
-        $sorter->sort();
-
-        // Return the time slots.
-        return $sorter->getValues();
     }
 
 }
