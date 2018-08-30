05df5dbe6bb973761deed76eeefe4988d6a40478
webeweb <support@webeweb.fr>
2018-08-30 09:30:29 +0200

    Create FTPException

---
+++ Tests/Exception/Network/FTPExceptionTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Exception\Network;
+
+use WBW\Library\Core\Tests\Cases\AbstractCoreFrameworkTestCase;
+use WBW\Library\Core\Exception\Network\FTPException;
+
+/**
+ * FTP exception test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Exception\Network
+ */
+final class FTPExceptionTest extends AbstractCoreFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new FTPException("exception");
+
+        $res = "exception";
+        $this->assertEquals($res, $obj->getMessage());
+    }
+
+}
