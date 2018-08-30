35b538ce046a3cee338ffe70c2b38cec695fad58
webeweb <support@webeweb.fr>
2018-08-30 10:31:36 +0200

    Add SkiData too long data exception

---
+++ Tests/Exception/SkiData/SkiDataTooLongDataExceptionTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Exception\SkiData;
+
+use WBW\Library\Core\Exception\SkiData\SkiDataTooLongDataException;
+use WBW\Library\Core\Tests\Cases\AbstractCoreFrameworkTestCase;
+
+/**
+ * SkiData too long data exception test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Exception\SkiData
+ * @final
+ */
+final class SkiDataTooLongDataExceptionTest extends AbstractCoreFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $ex = new SkiDataTooLongDataException("", 0);
+
+        $res = "The data \"\" exceeds the length \"0\" allowed";
+        $this->assertEquals($res, $ex->getMessage());
+    }
+
+}
