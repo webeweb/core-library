50e5604dda70e88f769438ac59adf589e83831f9
webeweb <support@webeweb.fr>
2018-09-01 13:20:51 +0200

    Add cURL request call allowed exception

---
+++ src/Exception/Network/CURLRequestCallException.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Exception\Network;
+
+use WBW\Library\Core\Network\CURL\Response\CURLResponse;
+
+/**
+ * cURL request call exception.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Exception\Network
+ */
+class CURLRequestCallException extends AbstractCURLException {
+
+    /**
+     * cURL response.
+     *
+     * @var CURLResponse
+     */
+    private $response;
+
+    /**
+     * Constructor.
+     *
+     * @param string $message The message.
+     * @param int $code The code.
+     * @param CURLResponse $response The response.
+     */
+    public function __construct($message, $code, CURLResponse $response) {
+        parent::__construct($message, $code);
+        $this->setResponse($response);
+    }
+
+    /**
+     * Get the response.
+     *
+     * @return CURLResponse Returns the response.
+     */
+    public function getResponse() {
+        return $this->response;
+    }
+
+    /**
+     * Set the response.
+     *
+     * @param CURLResponse $response The response.
+     * @return CURLRequestCallException Returns this request call exception.
+     */
+    protected function setResponse(CURLResponse $response) {
+        $this->response = $response;
+        return $this;
+    }
+
+}
