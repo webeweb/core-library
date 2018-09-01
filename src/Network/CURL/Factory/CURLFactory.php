bb6b00b70ee25afae9b9614ba09fcd2e1db3c0d6
webeweb <support@webeweb.fr>
2018-09-01 13:43:53 +0200

    Add cURL factory

---
+++ src/Network/CURL/Factory/CURLFactory.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Network\CURL\Factory;
+
+use WBW\Library\Core\Exception\IO\InvalidHTTPMethodException;
+use WBW\Library\Core\Network\CURL\Configuration\CURLConfiguration;
+use WBW\Library\Core\Network\CURL\Request\CURLDeleteRequest;
+use WBW\Library\Core\Network\CURL\Request\CURLGetRequest;
+use WBW\Library\Core\Network\CURL\Request\CURLHeadRequest;
+use WBW\Library\Core\Network\CURL\Request\CURLOptionsRequest;
+use WBW\Library\Core\Network\CURL\Request\CURLPatchRequest;
+use WBW\Library\Core\Network\CURL\Request\CURLPostRequest;
+use WBW\Library\Core\Network\CURL\Request\CURLPutRequest;
+use WBW\Library\Core\Network\CURL\Request\CURLRequestInterface;
+use WBW\Library\Core\Network\HTTP\HTTPInterface;
+
+/**
+ * cURL factory.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Network\CURL\Factory
+ */
+class CURLFactory implements HTTPInterface {
+
+    /**
+     * Get an instance.
+     *
+     * @param string $method The method.
+     * @param CURLConfiguration $configuration The configuration.
+     * @return CURLRequestInterface Returns this request.
+     * @throws InvalidHTTPMethodException Throws an invalid HTTP method exception if the method is not implemented.
+     */
+    public static function getInstance($method, CURLConfiguration $configuration = null, $resourcePath = null) {
+
+        // Check the configuration.
+        if (null === $configuration) {
+            $configuration = new CURLConfiguration();
+        }
+
+        // Switch into $method.
+        switch ($method) {
+
+            case self::HTTP_METHOD_DELETE:
+                return new CURLDeleteRequest($configuration, $resourcePath);
+
+            case self::HTTP_METHOD_GET:
+                return new CURLGetRequest($configuration, $resourcePath);
+
+            case self::HTTP_METHOD_HEAD:
+                return new CURLHeadRequest($configuration, $resourcePath);
+
+            case self::HTTP_METHOD_OPTIONS:
+                return new CURLOptionsRequest($configuration, $resourcePath);
+
+            case self::HTTP_METHOD_PATCH:
+                return new CURLPatchRequest($configuration, $resourcePath);
+
+            case self::HTTP_METHOD_POST:
+                return new CURLPostRequest($configuration, $resourcePath);
+
+            case self::HTTP_METHOD_PUT:
+                return new CURLPutRequest($configuration, $resourcePath);
+
+            default:
+                throw new InvalidHTTPMethodException($method);
+        }
+    }
+
+}
---
+++ tests/Network/CURL/Factory/CURLFactoryTest.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Network\CURL\Factory;
+
+use Exception;
+use WBW\Library\Core\Exception\IO\InvalidHTTPMethodException;
+use WBW\Library\Core\Network\CURL\Factory\CURLFactory;
+use WBW\Library\Core\Network\CURL\Request\CURLDeleteRequest;
+use WBW\Library\Core\Network\CURL\Request\CURLGetRequest;
+use WBW\Library\Core\Network\CURL\Request\CURLHeadRequest;
+use WBW\Library\Core\Network\CURL\Request\CURLOptionsRequest;
+use WBW\Library\Core\Network\CURL\Request\CURLPatchRequest;
+use WBW\Library\Core\Network\CURL\Request\CURLPostRequest;
+use WBW\Library\Core\Network\CURL\Request\CURLPutRequest;
+use WBW\Library\Core\Network\HTTP\HTTPInterface;
+use WBW\Library\Core\Tests\Cases\AbstractCoreFrameworkTestCase;
+
+/**
+ * cURL factory test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Network\CURL\Factory
+ * @final
+ */
+final class CURLFactoryTest extends AbstractCoreFrameworkTestCase {
+
+    /**
+     * Tests the getInstance() method.
+     *
+     * @return void
+     */
+    public function testGetInstance() {
+
+        // ===
+        try {
+
+            CURLFactory::getInstance("exception");
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(InvalidHTTPMethodException::class, $ex);
+            $this->assertEquals("The HTTP method \"exception\" is invalid", $ex->getMessage());
+        }
+
+        // ===
+        $res1 = CURLFactory::getInstance(HTTPInterface::HTTP_METHOD_DELETE);
+        $this->assertInstanceOf(CURLDeleteRequest::class, $res1);
+
+        // ===
+        $res2 = CURLFactory::getInstance(HTTPInterface::HTTP_METHOD_GET);
+        $this->assertInstanceOf(CURLGetRequest::class, $res2);
+
+        // ===
+        $res3 = CURLFactory::getInstance(HTTPInterface::HTTP_METHOD_HEAD);
+        $this->assertInstanceOf(CURLHeadRequest::class, $res3);
+
+        // ===
+        $res4 = CURLFactory::getInstance(HTTPInterface::HTTP_METHOD_OPTIONS);
+        $this->assertInstanceOf(CURLOptionsRequest::class, $res4);
+
+        // ===
+        $res5 = CURLFactory::getInstance(HTTPInterface::HTTP_METHOD_PATCH);
+        $this->assertInstanceOf(CURLPatchRequest::class, $res5);
+
+        // ===
+        $res6 = CURLFactory::getInstance(HTTPInterface::HTTP_METHOD_POST);
+        $this->assertInstanceOf(CURLPostRequest::class, $res6);
+
+        // ===
+        $res7 = CURLFactory::getInstance(HTTPInterface::HTTP_METHOD_PUT);
+        $this->assertInstanceOf(CURLPutRequest::class, $res7);
+    }
+
+}
