0c0b888cfda4b4a0e85a60f970f6418eeb97ff50
webeweb <support@webeweb.fr>
2018-09-01 13:24:39 +0200

    Add cURL configuration

---
+++ src/Network/CURL/Configuration/CURLConfiguration.php
@@ -0,0 +1,525 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Network\CURL\Configuration;
+
+use WBW\Library\Core\Exception\Argument\StringArgumentException;
+use WBW\Library\Core\Argument\ArgumentHelper;
+
+/**
+ * cURL configuration.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Network\CURL\Configuration
+ */
+class CURLConfiguration {
+
+    /**
+     * Allow encoding.
+     *
+     * @var boolean
+     */
+    private $allowEncoding = false;
+
+    /**
+     * HTTP connect timeout.
+     *
+     * @var integer
+     */
+    private $connectTimeout = 0;
+
+    /**
+     * Debug.
+     *
+     * @var boolean
+     */
+    private $debug = false;
+
+    /**
+     * Debug file.
+     *
+     * @var string
+     */
+    private $debugFile = "php://output";
+
+    /**
+     * Headers.
+     *
+     * @var array
+     */
+    private $headers = [];
+
+    /**
+     * Host.
+     *
+     * @var string
+     */
+    private $host;
+
+    /**
+     * HTTP basic password.
+     *
+     * @var string
+     */
+    private $httpPassword;
+
+    /**
+     * HTTP basic username.
+     *
+     * @var string
+     */
+    private $httpUsername;
+
+    /**
+     * Proxy host.
+     *
+     * @var string
+     */
+    private $proxyHost;
+
+    /**
+     * Proxy password.
+     *
+     * @var string
+     */
+    private $proxyPassword;
+
+    /**
+     * Proxy port.
+     *
+     * @var integer
+     */
+    private $proxyPort;
+
+    /**
+     * Proxy type.
+     *
+     * @var integer
+     */
+    private $proxyType;
+
+    /**
+     * Proxy username.
+     *
+     * @var string
+     */
+    private $proxyUsername;
+
+    /**
+     * HTTP request timeout.
+     *
+     * @var integer
+     */
+    private $requestTimeout = 0;
+
+    /**
+     * SSL verification.
+     *
+     * @var boolean
+     */
+    private $sslVerification = true;
+
+    /**
+     * User agent.
+     *
+     * @var string
+     */
+    private $userAgent = "webeweb/curl-library";
+
+    /**
+     * Verbose.
+     *
+     * @var boolean
+     */
+    private $verbose = false;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO.
+    }
+
+    /**
+     * Add an header.
+     *
+     * @param string $name The header name.
+     * @param string $value The header value.
+     * @return CURLConfiguration Returns this CURL configuration.
+     * @throws StringArgumentException Throws a string argument exception if the argument is not a string.
+     */
+    public function addHeader($name, $value) {
+        ArgumentHelper::isTypeOf($name, ArgumentHelper::ARGUMENT_STRING);
+        $this->headers[$name] = $value;
+        return $this;
+    }
+
+    /**
+     * Clear the headers.
+     *
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    public function clearHeaders() {
+        return $this->setHeaders();
+    }
+
+    /**
+     * Get the allow encoding.
+     *
+     * @return boolean Returns the allow encoding.
+     */
+    public function getAllowEncoding() {
+        return $this->allowEncoding;
+    }
+
+    /**
+     * Get the connect timeout.
+     *
+     * @return integer Returns the connect timeout.
+     */
+    public function getConnectTimeout() {
+        return $this->connectTimeout;
+    }
+
+    /**
+     * Ge the debug.
+     *
+     * @return boolen Returns the debug.
+     */
+    public function getDebug() {
+        return $this->debug;
+    }
+
+    /**
+     * Get the debug file.
+     *
+     * @return string Returns the debug file.
+     */
+    public function getDebugFile() {
+        return $this->debugFile;
+    }
+
+    /**
+     * Get the headers.
+     *
+     * @return array Returns the headers.
+     */
+    public function getHeaders() {
+        return $this->headers;
+    }
+
+    /**
+     * Get the host.
+     *
+     * @return string Returns the host.
+     */
+    public function getHost() {
+        return $this->host;
+    }
+
+    /**
+     * Get the HTTP password.
+     *
+     * @return string Returns the HTTP password.
+     */
+    public function getHttpPassword() {
+        return $this->httpPassword;
+    }
+
+    /**
+     * Get the HTTP username.
+     *
+     * @return string Returns the HTTP username.
+     */
+    public function getHttpUsername() {
+        return $this->httpUsername;
+    }
+
+    /**
+     * Get the proxy host.
+     *
+     * @return string Returns the proxy host.
+     */
+    public function getProxyHost() {
+        return $this->proxyHost;
+    }
+
+    /**
+     * Get the proxy password.
+     *
+     * @return string Returns the proxy password.
+     */
+    public function getProxyPassword() {
+        return $this->proxyPassword;
+    }
+
+    /**
+     * Get the proxy port.
+     *
+     * @return integer Returns the proxy port.
+     */
+    public function getProxyPort() {
+        return $this->proxyPort;
+    }
+
+    /**
+     * Get the proxy type.
+     *
+     * @return integer Returns the proxy type.
+     */
+    public function getProxyType() {
+        return $this->proxyType;
+    }
+
+    /**
+     * Get the proxy username.
+     *
+     * @return string Returns the proxy username.
+     */
+    public function getProxyUsername() {
+        return $this->proxyUsername;
+    }
+
+    /**
+     * Get the request timeout.
+     *
+     * @return integer Returns the request timeout.
+     */
+    public function getRequestTimeout() {
+        return $this->requestTimeout;
+    }
+
+    /**
+     * Get the SSL verification.
+     *
+     * @return boolean Returns the SSL verification.
+     */
+    public function getSslVerification() {
+        return $this->sslVerification;
+    }
+
+    /**
+     * Get the user agent.
+     *
+     * @return string Returns the user agent.
+     */
+    public function getUserAgent() {
+        return $this->userAgent;
+    }
+
+    /**
+     * Get the verbose.
+     *
+     * @return boolean Returns the verbose.
+     */
+    public function getVerbose() {
+        return $this->verbose;
+    }
+
+    /**
+     * Remove an header.
+     *
+     * @param string $name The header name.
+     */
+    public function removeHeader($name) {
+        if (true === array_key_exists($name, $this->headers)) {
+            unset($this->headers[$name]);
+        }
+    }
+
+    /**
+     * Set the allow encoding.
+     *
+     * @param boolean $allowEncoding The allow encoding.
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    public function setAllowEncoding($allowEncoding = false) {
+        $this->allowEncoding = $allowEncoding;
+        return $this;
+    }
+
+    /**
+     * Set the connect timeout.
+     *
+     * @param integer $connectTimeout The connect timeout.
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    public function setConnectTimeout($connectTimeout = 0) {
+        $this->connectTimeout = $connectTimeout;
+        return $this;
+    }
+
+    /**
+     * Set the debug.
+     *
+     * @param boolean $debug The debug.
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    public function setDebug($debug = false) {
+        $this->debug = $debug;
+        return $this;
+    }
+
+    /**
+     * Set the debug file.
+     *
+     * @param string $debugFile The debug file.
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    public function setDebugFile($debugFile = "php://output") {
+        $this->debugFile = $debugFile;
+        return $this;
+    }
+
+    /**
+     * Set the headers.
+     *
+     * @param array $headers The headers
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    protected function setHeaders(array $headers = []) {
+        $this->headers = $headers;
+        return $this;
+    }
+
+    /**
+     * Set the host.
+     *
+     * @param string $host The host.
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    public function setHost($host) {
+        $this->host = preg_replace("/\/$/", "", trim($host));
+        return $this;
+    }
+
+    /**
+     * Set the HTTP basic password.
+     *
+     * @param string $httpPassword The HTTP basic password.
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    public function setHttpPassword($httpPassword) {
+        $this->httpPassword = $httpPassword;
+        return $this;
+    }
+
+    /**
+     * Set the HTTP basic username.
+     *
+     * @param string $httpUsername The HTTP basic username.
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    public function setHttpUsername($httpUsername) {
+        $this->httpUsername = $httpUsername;
+        return $this;
+    }
+
+    /**
+     * Set the proxy host.
+     *
+     * @param string $proxyHost The proxy host.
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    public function setProxyHost($proxyHost) {
+        $this->proxyHost = $proxyHost;
+        return $this;
+    }
+
+    /**
+     * Set the proxy password.
+     *
+     * @param string $proxyPassword The proxy password.
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    public function setProxyPassword($proxyPassword) {
+        $this->proxyPassword = $proxyPassword;
+        return $this;
+    }
+
+    /**
+     * Set the proxy port.
+     *
+     * @param integer $proxyPort The proxy port.
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    public function setProxyPort($proxyPort) {
+        $this->proxyPort = $proxyPort;
+        return $this;
+    }
+
+    /**
+     * Set the proxy type.
+     *
+     * @param integer $proxyType The proxy type.
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    public function setProxyType($proxyType) {
+        $this->proxyType = $proxyType;
+        return $this;
+    }
+
+    /**
+     * Set the proxy username.
+     *
+     * @param string $proxyUsername The proxy username.
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    public function setProxyUsername($proxyUsername) {
+        $this->proxyUsername = $proxyUsername;
+        return $this;
+    }
+
+    /**
+     * Set the request timeout.
+     *
+     * @param integer $requestTimeout The request timeout.
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    public function setRequestTimeout($requestTimeout = 0) {
+        $this->requestTimeout = $requestTimeout;
+        return $this;
+    }
+
+    /**
+     * Set the SSL verification.
+     *
+     * @param boolean $sslVerification The SSL verification.
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    public function setSslVerification($sslVerification = true) {
+        $this->sslVerification = $sslVerification;
+        return $this;
+    }
+
+    /**
+     * Set the user agent.
+     *
+     * @param string $userAgent The user agent.
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    public function setUserAgent($userAgent = "webeweb/curl-library") {
+        $this->userAgent = $userAgent;
+        return $this;
+    }
+
+    /**
+     * Set the verbose.
+     *
+     * @param boolean $verbose The verbose.
+     * @return CURLConfiguration Returns this CURL configuration.
+     */
+    public function setVerbose($verbose = false) {
+        $this->verbose = $verbose;
+        return $this;
+    }
+
+}
---
+++ tests/Network/CURL/Configuration/CURLConfigurationTest.php
@@ -0,0 +1,326 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Network\CURL\Configuration;
+
+use Exception;
+use WBW\Library\Core\Exception\Argument\StringArgumentException;
+use WBW\Library\Core\Network\CURL\Configuration\CURLConfiguration;
+use WBW\Library\Core\Tests\Cases\AbstractCoreFrameworkTestCase;
+
+/**
+ * cURL configuration test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Network\CURL\Configuration
+ * @final
+ */
+final class CURLConfigurationTest extends AbstractCoreFrameworkTestCase {
+
+    /**
+     * Tests the __constructor() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new CURLConfiguration();
+
+        $this->assertFalse($obj->getAllowEncoding());
+        $this->assertEquals(0, $obj->getConnectTimeout());
+        $this->assertFalse($obj->getDebug());
+        $this->assertEquals("php://output", $obj->getDebugFile());
+        $this->assertEquals([], $obj->getHeaders());
+        $this->assertNull($obj->getHost());
+        $this->assertNull($obj->getHttpPassword());
+        $this->assertNull($obj->getHttpUsername());
+        $this->assertNull($obj->getProxyHost());
+        $this->assertNull($obj->getProxyPassword());
+        $this->assertNull($obj->getProxyPort());
+        $this->assertNull($obj->getProxyType());
+        $this->assertNull($obj->getProxyUsername());
+        $this->assertEquals(0, $obj->getRequestTimeout());
+        $this->assertTrue($obj->getSslVerification());
+        $this->assertEquals("webeweb/curl-library", $obj->getUserAgent());
+        $this->assertFalse($obj->getVerbose());
+    }
+
+    /**
+     * Tests the addHeader() method.
+     *
+     * @return void
+     */
+    public function testAddHeader() {
+
+        $obj = new CURLConfiguration();
+
+        // ===
+        $obj->addHeader("name", "value");
+        $res1 = ["name" => "value"];
+        $this->assertEquals($res1, $obj->getHeaders());
+
+        // ===
+        try {
+
+            $obj->addHeader(1, "value");
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(StringArgumentException::class, $ex);
+            $this->assertEquals("The argument \"1\" is not a string", $ex->getMessage());
+        }
+    }
+
+    /**
+     * Tests the clearHeader() method.
+     *
+     * @return void
+     */
+    public function testClearHeaders() {
+
+        $obj = new CURLConfiguration();
+
+        $obj->addHeader("name", "value");
+        $this->assertCount(1, $obj->getHeaders());
+
+        $obj->clearHeaders();
+        $this->assertCount(0, $obj->getHeaders());
+    }
+
+    /**
+     * Tests the removeHeader() method.
+     *
+     * @return void
+     */
+    public function testRemoveHeader() {
+
+        $obj = new CURLConfiguration();
+        $obj->addHeader("name", "value");
+
+        // ===
+        $obj->removeHeader("");
+        $res1 = ["name" => "value"];
+        $this->assertEquals($res1, $obj->getHeaders());
+
+        // ===
+        $obj->removeHeader("name");
+        $res2 = [];
+        $this->assertEquals($res2, $obj->getHeaders());
+    }
+
+    /**
+     * Tests the setAllowEncoding() method.
+     *
+     * @return void
+     */
+    public function testSetAllowEncoding() {
+
+        $obj = new CURLConfiguration();
+
+        $obj->setAllowEncoding(true);
+        $this->assertTrue($obj->getAllowEncoding());
+    }
+
+    /**
+     * Tests the setConnectTimeout() method.
+     *
+     * @return void
+     */
+    public function testSetConnectTimeout() {
+
+        $obj = new CURLConfiguration();
+
+        $obj->setConnectTimeout(1);
+        $this->assertEquals(1, $obj->getConnectTimeout());
+    }
+
+    /**
+     * Tests the setDebug() method.
+     *
+     * @return void
+     */
+    public function testSetDebug() {
+
+        $obj = new CURLConfiguration();
+
+        $obj->setDebug(true);
+        $this->assertTrue($obj->getDebug());
+    }
+
+    /**
+     * Tests the setDebugFile() method.
+     *
+     * @return void
+     */
+    public function testSetDebugFile() {
+
+        $obj = new CURLConfiguration();
+
+        $obj->setDebugFile("./debugfile.log");
+        $this->assertEquals("./debugfile.log", $obj->getDebugFile());
+    }
+
+    /**
+     * Tests the setHost() method.
+     *
+     * @return void
+     */
+    public function testSetHost() {
+
+        $obj = new CURLConfiguration();
+
+        $obj->setHost("host");
+        $this->assertEquals("host", $obj->getHost());
+    }
+
+    /**
+     * Tests the setHttpPassword() method.
+     *
+     * @return void
+     */
+    public function testSetHttpPassword() {
+
+        $obj = new CURLConfiguration();
+
+        $obj->setHttpPassword("httpPassword");
+        $this->assertEquals("httpPassword", $obj->getHttpPassword());
+    }
+
+    /**
+     * Tests the setHttpUsername() method.
+     *
+     * @return void
+     */
+    public function testSetHttpUsername() {
+
+        $obj = new CURLConfiguration();
+
+        $obj->setHttpUsername("httpUsername");
+        $this->assertEquals("httpUsername", $obj->getHttpUsername());
+    }
+
+    /**
+     * Tests the setProxyHost() method.
+     *
+     * @return void
+     */
+    public function testSetProxyHost() {
+
+        $obj = new CURLConfiguration();
+
+        $obj->setProxyHost("proxyHost");
+        $this->assertEquals("proxyHost", $obj->getProxyHost());
+    }
+
+    /**
+     * Tests the setProxyPassword() method.
+     *
+     * @return void
+     */
+    public function testSetProxyPassword() {
+
+        $obj = new CURLConfiguration();
+
+        $obj->setProxyPassword("proxyPassword");
+        $this->assertEquals("proxyPassword", $obj->getProxyPassword());
+    }
+
+    /**
+     * Tests the setProxyPort() method.
+     *
+     * @return void
+     */
+    public function testSetProxyPort() {
+
+        $obj = new CURLConfiguration();
+
+        $obj->setProxyPort("proxyPort");
+        $this->assertEquals("proxyPort", $obj->getProxyPort());
+    }
+
+    /**
+     * Tests the setProxyType() method.
+     *
+     * @return void
+     */
+    public function testSetProxyType() {
+
+        $obj = new CURLConfiguration();
+
+        $obj->setProxyType(1);
+        $this->assertEquals(1, $obj->getProxyType());
+    }
+
+    /**
+     * Tests the setProxyUsername() method.
+     *
+     * @return void
+     */
+    public function testSetProxyUsername() {
+
+        $obj = new CURLConfiguration();
+
+        $obj->setProxyUsername("proxyUsername");
+        $this->assertEquals("proxyUsername", $obj->getProxyUsername());
+    }
+
+    /**
+     * Tests the setRequestTimeout() method.
+     *
+     * @return void
+     */
+    public function testSetRequestTimeout() {
+
+        $obj = new CURLConfiguration();
+
+        $obj->setRequestTimeout(1);
+        $this->assertEquals(1, $obj->getRequestTimeout());
+    }
+
+    /**
+     * Tests the setSslVerification() method.
+     *
+     * @return void
+     */
+    public function testSetSslVerification() {
+
+        $obj = new CURLConfiguration();
+
+        $obj->setSslVerification(false);
+        $this->assertFalse($obj->getSslVerification());
+    }
+
+    /**
+     * Tests the setUserAgent() method.
+     *
+     * @return void
+     */
+    public function testSetUserAgent() {
+
+        $obj = new CURLConfiguration();
+
+        $obj->setUserAgent("userAgent");
+        $this->assertEquals("userAgent", $obj->getUserAgent());
+    }
+
+    /**
+     * Tests the setVerbose() method.
+     *
+     * @return void
+     */
+    public function testSetVerbose() {
+
+        $obj = new CURLConfiguration();
+
+        $obj->setVerbose(true);
+        $this->assertTrue($obj->getVerbose());
+    }
+
+}
