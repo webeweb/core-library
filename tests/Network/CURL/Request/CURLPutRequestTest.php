fbfb5cbe3f5582bcf4701b289b4c0bb46319c163
webeweb <support@webeweb.fr>
2018-09-01 13:43:42 +0200

    Add cURL PUT request

---
+++ tests/Network/CURL/Request/CURLPutRequestTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Network\CURL\Request;
+
+use WBW\Library\Core\Network\CURL\Request\CURLPutRequest;
+
+/**
+ * cURL PUT request test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Network\CURL\Request
+ * @final
+ */
+final class CURLPutRequestTest extends AbstractCURLRequestTest {
+
+    /**
+     * Tests __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new CURLPutRequest($this->configuration, self::RESOURCE_PATH);
+
+        $this->assertSame($this->configuration, $obj->getConfiguration());
+        $this->assertEquals([], $obj->getHeaders());
+        $this->assertEquals(CURLPutRequest::HTTP_METHOD_PUT, $obj->getMethod());
+        $this->assertEquals([], $obj->getPostData());
+        $this->assertEquals([], $obj->getQueryData());
+        $this->assertEquals("testCall.php", $obj->getResourcePath());
+    }
+
+    /**
+     * Tests call() method.
+     *
+     * @return void
+     */
+    public function testCall() {
+
+        $obj = new CURLPutRequest($this->configuration, self::RESOURCE_PATH);
+
+        $obj->addHeader("header", "header");
+        $obj->addQueryData("queryData", "queryData");
+
+        $res = $obj->call();
+
+        $this->assertContains("header: header", $res->getRequestHeader());
+        $this->assertContains("queryData=queryData", $res->getRequestURL());
+        $this->assertEquals(CURLPutRequest::HTTP_METHOD_PUT, json_decode($res->getResponseBody(), true)["method"]);
+        $this->assertEquals(200, $res->getResponseInfo()["http_code"]);
+    }
+
+}
