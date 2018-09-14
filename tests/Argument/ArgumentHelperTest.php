567f1e84640123a98a5259893f7dd15386f8a058
webeweb <support@webeweb.fr>
2018-09-14 18:59:56 +0200

    Split unit tests

--- tests/Argument/ArgumentHelperTest.php
+++ tests/Argument/ArgumentHelperTest.php
@@ -46,78 +46,6 @@ final class ArgumentHelperTest extends AbstractCoreFrameworkTestCase {
      */
     public function testConvert() {
 
-        try {
-
-            ArgumentHelper::convert(null, ArgumentInterface::ARGUMENT_ARRAY);
-        } catch (Exception $ex) {
-
-            $this->assertInstanceOf(IllegalArgumentException::class, $ex);
-            $this->assertEquals("The type \"63\" is not implemented", $ex->getMessage());
-        }
-
-        try {
-
-            ArgumentHelper::convert(null, ArgumentInterface::ARGUMENT_DATE);
-        } catch (Exception $ex) {
-
-            $this->assertInstanceOf(NullPointerException::class, $ex);
-            $this->assertEquals("The date format is null", $ex->getMessage());
-        }
-
-        try {
-
-            ArgumentHelper::convert("exception", ArgumentInterface::ARGUMENT_DATE, "Y-m-d");
-        } catch (Exception $ex) {
-
-            $this->assertInstanceOf(DateArgumentException::class, $ex);
-            $this->assertEquals("The argument \"exception\" is not a date", $ex->getMessage());
-        }
-
-        try {
-
-            ArgumentHelper::convert(null, ArgumentInterface::ARGUMENT_NUMBER);
-        } catch (Exception $ex) {
-
-            $this->assertInstanceOf(IllegalArgumentException::class, $ex);
-            $this->assertEquals("The type \"73\" is not implemented", $ex->getMessage());
-        }
-
-        try {
-
-            ArgumentHelper::convert(null, ArgumentInterface::ARGUMENT_OBJECT);
-        } catch (Exception $ex) {
-
-            $this->assertInstanceOf(IllegalArgumentException::class, $ex);
-            $this->assertEquals("The type \"55\" is not implemented", $ex->getMessage());
-        }
-
-        try {
-
-            ArgumentHelper::convert(null, ArgumentInterface::ARGUMENT_RESOURCE);
-        } catch (Exception $ex) {
-
-            $this->assertInstanceOf(IllegalArgumentException::class, $ex);
-            $this->assertEquals("The type \"104\" is not implemented", $ex->getMessage());
-        }
-
-        try {
-
-            ArgumentHelper::convert(null, ArgumentInterface::ARGUMENT_TIMESTAMP);
-        } catch (Exception $ex) {
-
-            $this->assertInstanceOf(NullPointerException::class, $ex);
-            $this->assertEquals("The datetime format is null", $ex->getMessage());
-        }
-
-        try {
-
-            ArgumentHelper::convert("exception", ArgumentInterface::ARGUMENT_TIMESTAMP, "Y-m-d H:m:s");
-        } catch (Exception $ex) {
-
-            $this->assertInstanceOf(TimestampArgumentException::class, $ex);
-            $this->assertEquals("The argument \"exception\" is not a timestamp", $ex->getMessage());
-        }
-
         $this->assertTrue(ArgumentHelper::convert("1", ArgumentInterface::ARGUMENT_BOOLEAN));
         $this->assertInstanceOf(DateTime::class, ArgumentHelper::convert("2017-11-27", ArgumentInterface::ARGUMENT_DATE, "Y-m-d"));
         $this->assertEquals(1, ArgumentHelper::convert("1", ArgumentInterface::ARGUMENT_DOUBLE));
@@ -128,13 +56,148 @@ final class ArgumentHelperTest extends AbstractCoreFrameworkTestCase {
     }
 
     /**
+     * Tests the convert() method.
+     *
+     * @return void
+     */
+    public function testConvertWithDateArgumentException() {
+
+        try {
+
+            ArgumentHelper::convert("exception", ArgumentInterface::ARGUMENT_DATE, "Y-m-d");
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(DateArgumentException::class, $ex);
+            $this->assertEquals("The argument \"exception\" is not a date", $ex->getMessage());
+        }
+    }
+
+    /**
+     * Tests the convert() method.
+     *
+     * @return void
+     */
+    public function testConvertWithIllegalArgumentException() {
+
+        try {
+
+            ArgumentHelper::convert(null, ArgumentInterface::ARGUMENT_ARRAY);
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(IllegalArgumentException::class, $ex);
+            $this->assertEquals("The type \"63\" is not implemented", $ex->getMessage());
+        }
+    }
+
+    /**
+     * Tests the convert() method.
+     *
+     * @return void
+     */
+    public function testConvertWithIllegalArgumentException55() {
+
+        try {
+
+            ArgumentHelper::convert(null, ArgumentInterface::ARGUMENT_OBJECT);
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(IllegalArgumentException::class, $ex);
+            $this->assertEquals("The type \"55\" is not implemented", $ex->getMessage());
+        }
+    }
+
+    /**
+     * Tests the convert() method.
+     *
+     * @return void
+     */
+    public function testConvertWithIllegalArgumentException73() {
+
+        try {
+
+            ArgumentHelper::convert(null, ArgumentInterface::ARGUMENT_NUMBER);
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(IllegalArgumentException::class, $ex);
+            $this->assertEquals("The type \"73\" is not implemented", $ex->getMessage());
+        }
+    }
+
+    /**
+     * Tests the convert() method.
+     *
+     * @return void
+     */
+    public function testConvertWithIllegalArgumentException104() {
+
+        try {
+
+            ArgumentHelper::convert(null, ArgumentInterface::ARGUMENT_RESOURCE);
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(IllegalArgumentException::class, $ex);
+            $this->assertEquals("The type \"104\" is not implemented", $ex->getMessage());
+        }
+    }
+
+    /**
+     * Tests the convert() method.
+     *
+     * @return void
+     */
+    public function testConvertWithNullPointerException30() {
+
+        try {
+
+            ArgumentHelper::convert(null, ArgumentInterface::ARGUMENT_DATE);
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(NullPointerException::class, $ex);
+            $this->assertEquals("The date format is null", $ex->getMessage());
+        }
+    }
+
+    /**
+     * Tests the convert() method.
+     *
+     * @return void
+     */
+    public function testConvertWithNullPointerException116() {
+
+        try {
+
+            ArgumentHelper::convert(null, ArgumentInterface::ARGUMENT_TIMESTAMP);
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(NullPointerException::class, $ex);
+            $this->assertEquals("The datetime format is null", $ex->getMessage());
+        }
+    }
+
+    /**
+     * Tests the convert() method.
+     *
+     * @return void
+     */
+    public function testConvertWithTimestampArgumentException() {
+
+        try {
+
+            ArgumentHelper::convert("exception", ArgumentInterface::ARGUMENT_TIMESTAMP, "Y-m-d H:m:s");
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(TimestampArgumentException::class, $ex);
+            $this->assertEquals("The argument \"exception\" is not a timestamp", $ex->getMessage());
+        }
+    }
+
+    /**
      * Tests the isValid() method.
      *
      * @return void
      */
     public function testIsValid() {
 
-        // ===
         $rsc = fopen(getcwd() . "/tests/Argument/ArgumentHelperTest.php", "r");
 
         $this->assertTrue(ArgumentHelper::isTypeOf([], ArgumentInterface::ARGUMENT_ARRAY));
@@ -148,18 +211,15 @@ final class ArgumentHelperTest extends AbstractCoreFrameworkTestCase {
         $this->assertTrue(ArgumentHelper::isTypeOf($rsc, ArgumentInterface::ARGUMENT_RESOURCE));
         $this->assertTrue(ArgumentHelper::isTypeOf("", ArgumentInterface::ARGUMENT_STRING));
         $this->assertTrue(ArgumentHelper::isTypeOf("2017-10-20 15:41:10", ArgumentInterface::ARGUMENT_TIMESTAMP));
+    }
 
-        // ===
-        try {
+    /**
+     * Tests the isValid() method.
+     *
+     * @return void
+     */
+    public function testIsValidWithArrayArgumentException() {
 
-            ArgumentHelper::isTypeOf(null, -1);
-        } catch (Exception $ex) {
-
-            $this->assertInstanceOf(IllegalArgumentException::class, $ex);
-            $this->assertEquals("The type \"-1\" is not implemented", $ex->getMessage());
-        }
-
-        // ===
         try {
 
             ArgumentHelper::isTypeOf(null, ArgumentInterface::ARGUMENT_ARRAY);
@@ -168,8 +228,15 @@ final class ArgumentHelperTest extends AbstractCoreFrameworkTestCase {
             $this->assertInstanceOf(ArrayArgumentException::class, $ex);
             $this->assertEquals("The argument \"\" is not an array", $ex->getMessage());
         }
+    }
 
-        // ===
+    /**
+     * Tests the isValid() method.
+     *
+     * @return void
+     */
+    public function testIsValidWithBooleanArgumentException() {
+
         try {
 
             ArgumentHelper::isTypeOf(null, ArgumentInterface::ARGUMENT_BOOLEAN);
@@ -178,8 +245,15 @@ final class ArgumentHelperTest extends AbstractCoreFrameworkTestCase {
             $this->assertInstanceOf(BooleanArgumentException::class, $ex);
             $this->assertEquals("The argument \"\" is not a boolean", $ex->getMessage());
         }
+    }
 
-        // ===
+    /**
+     * Tests the isValid() method.
+     *
+     * @return void
+     */
+    public function testIsValidWithDateArgumentException() {
+
         try {
 
             ArgumentHelper::isTypeOf(null, ArgumentInterface::ARGUMENT_DATE);
@@ -188,8 +262,15 @@ final class ArgumentHelperTest extends AbstractCoreFrameworkTestCase {
             $this->assertInstanceOf(DateArgumentException::class, $ex);
             $this->assertEquals("The argument \"\" is not a date", $ex->getMessage());
         }
+    }
 
-        // ===
+    /**
+     * Tests the isValid() method.
+     *
+     * @return void
+     */
+    public function testIsValidWithDoubleArgumentException() {
+
         try {
 
             ArgumentHelper::isTypeOf(null, ArgumentInterface::ARGUMENT_DOUBLE);
@@ -198,8 +279,15 @@ final class ArgumentHelperTest extends AbstractCoreFrameworkTestCase {
             $this->assertInstanceOf(DoubleArgumentException::class, $ex);
             $this->assertEquals("The argument \"\" is not a double", $ex->getMessage());
         }
+    }
 
-        // ===
+    /**
+     * Tests the isValid() method.
+     *
+     * @return void
+     */
+    public function testIsValidWithFloatArgumentException() {
+
         try {
 
             ArgumentHelper::isTypeOf(null, ArgumentInterface::ARGUMENT_FLOAT);
@@ -208,8 +296,32 @@ final class ArgumentHelperTest extends AbstractCoreFrameworkTestCase {
             $this->assertInstanceOf(FloatArgumentException::class, $ex);
             $this->assertEquals("The argument \"\" is not a float", $ex->getMessage());
         }
+    }
 
-        // ===
+    /**
+     * Tests the isValid() method.
+     *
+     * @return void
+     */
+    public function testIsValidWithIllegalArgumentException() {
+
+        try {
+
+            ArgumentHelper::isTypeOf(null, -1);
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(IllegalArgumentException::class, $ex);
+            $this->assertEquals("The type \"-1\" is not implemented", $ex->getMessage());
+        }
+    }
+
+    /**
+     * Tests the isValid() method.
+     *
+     * @return void
+     */
+    public function testIsValidWithIntegerArgumentException() {
+
         try {
 
             ArgumentHelper::isTypeOf(null, ArgumentInterface::ARGUMENT_INTEGER);
@@ -218,8 +330,15 @@ final class ArgumentHelperTest extends AbstractCoreFrameworkTestCase {
             $this->assertInstanceOf(IntegerArgumentException::class, $ex);
             $this->assertEquals("The argument \"\" is not an integer", $ex->getMessage());
         }
+    }
 
-        // ===
+    /**
+     * Tests the isValid() method.
+     *
+     * @return void
+     */
+    public function testIsValidWithNumberArgumentException() {
+
         try {
 
             ArgumentHelper::isTypeOf(null, ArgumentInterface::ARGUMENT_NUMBER);
@@ -228,8 +347,15 @@ final class ArgumentHelperTest extends AbstractCoreFrameworkTestCase {
             $this->assertInstanceOf(NumberArgumentException::class, $ex);
             $this->assertEquals("The argument \"\" is not a number", $ex->getMessage());
         }
+    }
 
-        // ===
+    /**
+     * Tests the isValid() method.
+     *
+     * @return void
+     */
+    public function testIsValidWithObjectArgumentException() {
+
         try {
 
             ArgumentHelper::isTypeOf(null, ArgumentInterface::ARGUMENT_OBJECT);
@@ -238,8 +364,15 @@ final class ArgumentHelperTest extends AbstractCoreFrameworkTestCase {
             $this->assertInstanceOf(ObjectArgumentException::class, $ex);
             $this->assertEquals("The argument \"\" is not an object", $ex->getMessage());
         }
+    }
 
-        // ===
+    /**
+     * Tests the isValid() method.
+     *
+     * @return void
+     */
+    public function testIsValidWithResourceArgumentException() {
+
         try {
 
             ArgumentHelper::isTypeOf(null, ArgumentInterface::ARGUMENT_RESOURCE);
@@ -248,8 +381,15 @@ final class ArgumentHelperTest extends AbstractCoreFrameworkTestCase {
             $this->assertInstanceOf(ResourceArgumentException::class, $ex);
             $this->assertEquals("The argument \"\" is not a resource", $ex->getMessage());
         }
+    }
 
-        // ===
+    /**
+     * Tests the isValid() method.
+     *
+     * @return void
+     */
+    public function testIsValidWithStringArgumentException() {
+
         try {
 
             ArgumentHelper::isTypeOf(null, ArgumentInterface::ARGUMENT_STRING);
@@ -258,8 +398,15 @@ final class ArgumentHelperTest extends AbstractCoreFrameworkTestCase {
             $this->assertInstanceOf(StringArgumentException::class, $ex);
             $this->assertEquals("The argument \"\" is not a string", $ex->getMessage());
         }
+    }
 
-        // ===
+    /**
+     * Tests the isValid() method.
+     *
+     * @return void
+     */
+    public function testIsValidWithTimestampArgumentException() {
+
         try {
 
             ArgumentHelper::isTypeOf(null, ArgumentInterface::ARGUMENT_TIMESTAMP);
