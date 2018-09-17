a1bb278ea46f0f8aee256a70e944fc5d352abeec
webeweb <support@webeweb.fr>
2018-09-17 10:30:23 +0200

    Create an abstract Chart of accounts exception

---
+++ src/Exception/ChartAccounts/AbstractChartAccountsException.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Exception\ChartAccounts;
+
+use WBW\Library\Core\Exception\AbstractCoreException;
+
+/**
+ * Abstract chart of accounts exception.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Exception\ChartAccounts
+ * @abstract
+ */
+abstract class AbstractChartAccountsException extends AbstractCoreException {
+
+    /**
+     * Constructor.
+     *
+     * @param string $message The message.
+     */
+    public function __construct($message) {
+        parent::__construct($message);
+    }
+
+}
