d3fe1ca44ccb9d481c98427725cbe40c254ce75f
webeweb <support@webeweb.fr>
2018-09-17 10:32:24 +0200

    Add Chart of accounts type model

---
+++ src/ChartAccounts/Model/ChartAccountsType.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\ChartAccounts\Model;
+
+/**
+ * Chart of accounts type model.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\ChartAccounts\Model
+ */
+class ChartAccountsType extends AbstractChartAccountsModel {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+}
