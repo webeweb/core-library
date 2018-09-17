a070a4ebdf84e0e0213a07666c9c0f1093b754da
webeweb <support@webeweb.fr>
2018-09-17 10:30:05 +0200

    Create an abstract Chart of accounts model

---
+++ src/ChartAccounts/Model/AbstractChartAccountsModel.php
@@ -0,0 +1,146 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\ChartAccounts\Model;
+
+use WBW\Library\Core\Exception\ChartAccounts\AccountAlreadyExistsException;
+
+/**
+ * Abstract chart of accounts model.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\ChartAccounts\Model
+ * @abstract
+ */
+abstract class AbstractChartAccountsModel {
+
+    /**
+     * Accounts.
+     *
+     * @var COAAccount[]
+     */
+    private $accounts;
+
+    /**
+     * Label.
+     *
+     * @var string
+     */
+    private $label;
+
+    /**
+     * Number
+     *
+     * @var string
+     */
+    private $number;
+
+    /**
+     * Constructor.
+     */
+    protected function __construct() {
+        $this->setAccounts([]);
+    }
+
+    /**
+     * Add an account.
+     *
+     * @param ChartAccountsAccount $account The account.
+     * @return AbstractChartAccountsModel Returns this charts of accounts model.
+     * @throws AccountAlreadyExistsException Throws an account already exists.
+     */
+    public function addAccount(ChartAccountsAccount $account) {
+        foreach ($this->accounts as $current) {
+            if ($current->getNumber() === $account->getNumber()) {
+                throw new AccountAlreadyExistsException($account);
+            }
+        }
+        $this->accounts[] = $account;
+        return $this;
+    }
+
+    /**
+     * Get the accounts.
+     *
+     * @return COAAccount[] Returns the accounts.
+     */
+    public function getAccounts() {
+        return $this->accounts;
+    }
+
+    /**
+     * Get the label.
+     *
+     * @return string Returns the label.
+     */
+    public function getLabel() {
+        return $this->label;
+    }
+
+    /**
+     * Get the number.
+     *
+     * @return string Returns the number.
+     */
+    public function getNumber() {
+        return $this->number;
+    }
+
+    /**
+     * Remove an account.
+     *
+     * @param ChartAccountsAccount $account The account.
+     * @return AbstractChartAccountsModel Returns this charts of accounts model.
+     */
+    public function removeAccount(ChartAccountsAccount $account) {
+        for ($i = count($this->accounts) - 1; 0 <= $i; --$i) {
+            if ($account->getNumber() !== $this->accounts[$i]->getNumber()) {
+                continue;
+            }
+            unset($this->accounts[$i]);
+        }
+        return $this;
+    }
+
+    /**
+     * Set the accounts.
+     *
+     * @param COAAccount[] $accounts The ccounts.
+     * @return AbstractChartAccountsModel Returns this charts of accounts model.
+     */
+    protected function setAccounts($accounts) {
+        $this->accounts = $accounts;
+        return $this;
+    }
+
+    /**
+     * Set the label.
+     *
+     * @param string $label The label.
+     * @return AbstractChartAccountsModel Returns this charts of accounts model.
+     */
+    public function setLabel($label) {
+        $this->label = $label;
+        return $this;
+    }
+
+    /**
+     * Set the number.
+     *
+     * @param string $number
+     * @return AbstractChartAccountsModel Returns this charts of accounts model.
+     */
+    public function setNumber($number) {
+        $this->number = $number;
+        return $this;
+    }
+
+}
