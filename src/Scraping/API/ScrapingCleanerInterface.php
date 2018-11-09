0f703260f829d53fe8ce16cc1eb5d9199abb637e
webeweb <support@webeweb.fr>
2018-11-09 18:00:59 +0100

    Add html_entity_decode scraping cleaner

---
+++ src/Scraping/API/ScrapingCleanerInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Scraping\API;
+
+/**
+ * Scraping cleaner interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Scraping\API
+ */
+interface ScrapingCleanerInterface {
+
+    /**
+     * Clean a value.
+     *
+     * @param string $value The value.
+     * @return mixed Return the cleaned value.
+     */
+    public function clean($value);
+}
---
+++ src/Scraping/Cleaner/HTMLEntityDecodeScrapingCleaner.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Scraping\Cleaner;
+
+use WBW\Library\Core\Scraping\API\ScrapingCleanerInterface;
+
+/**
+ * html_entity_decode scraping cleaner.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Scraping\Cleaner
+ */
+class HTMLEntityDecodeScrapingCleaner implements ScrapingCleanerInterface {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO.
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function clean($value) {
+        if (null === $value || false === is_string($value)) {
+            return null;
+        }
+        return html_entity_decode($value);
+    }
+
+}
