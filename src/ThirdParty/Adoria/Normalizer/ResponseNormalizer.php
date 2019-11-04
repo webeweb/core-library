f8fdc98a7fd6ec8a73a9a7a1a932fc700c890836
webeweb <support@webeweb.fr>
2019-11-04 18:51:18 +0100

    Add Adoria Response normalizer

---
+++ src/ThirdParty/Adoria/Normalizer/ResponseNormalizer.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\ThirdParty\Adoria\Normalizer;
+
+use DateTime;
+use WBW\Library\Core\Argument\ArrayHelper;
+use WBW\Library\Core\ThirdParty\Adoria\Model\Line;
+use WBW\Library\Core\ThirdParty\Adoria\Model\Result;
+
+/**
+ * Response normalizer.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\ThirdParty\Adoria\Normalizer
+ */
+class ResponseNormalizer {
+
+    /**
+     * Denormalize date format.
+     *
+     * @var string
+     */
+    const DENORMALIZE_DATE_FORMAT = RequestNormalizer::NORMALIZE_DATE_FORMAT;
+
+    /**
+     * Denormalize a line.
+     *
+     * @param string $rawResponse The raw response.
+     * @return Line Returns the line.
+     */
+    public static function denormalizeLine($rawResponse) {
+
+        $decodedResponse = json_decode($rawResponse, true);
+
+        $invoiceDate    = DateTime::createFromFormat(self::DENORMALIZE_DATE_FORMAT, ArrayHelper::get($decodedResponse, "InvoiceDate"));
+        $invoiceDueDate = DateTime::createFromFormat(self::DENORMALIZE_DATE_FORMAT, ArrayHelper::get($decodedResponse, "InvoiceDueDate"));
+
+        $model = new Line();
+
+        $model->setAccountingCode(ArrayHelper::get($decodedResponse, "AccountingCode"));
+        $model->setAnalyticCode(ArrayHelper::get($decodedResponse, "AnalyticCode"));
+        $model->setAmount(ArrayHelper::get($decodedResponse, "Amount"));
+        $model->setAmountSign(ArrayHelper::get($decodedResponse, "AmountSign"));
+        $model->setBookingJournalCode(ArrayHelper::get($decodedResponse, "BookingJournalCode"));
+        $model->setCurrency(ArrayHelper::get($decodedResponse, "Currency"));
+        $model->setFreeField(ArrayHelper::get($decodedResponse, "FreeField"));
+        $model->setInvoiceDate(false !== $invoiceDate ? $invoiceDate : null);
+        $model->setInvoiceDueDate(false !== $invoiceDueDate ? $invoiceDueDate : null);
+        $model->setInvoiceNumber(ArrayHelper::get($decodedResponse, "InvoiceNumber"));
+        $model->setReference(ArrayHelper::get($decodedResponse, "Reference"));
+        $model->setSubledgerAccount(ArrayHelper::get($decodedResponse, "SubledgerAccount"));
+
+        return $model;
+    }
+
+    /**
+     * Denormalize a result.
+     *
+     * @param string $rawResponse The raw response.
+     * @return Result Returns the result.
+     */
+    public static function denormalizeResult($rawResponse) {
+
+        $model = new Result();
+
+        $decodedResponse = json_decode($rawResponse, true);
+        if (null === $decodedResponse) {
+            return $model;
+        }
+
+        $lines = [];
+
+        $data = ArrayHelper::get($decodedResponse, "Datas", []);
+        foreach ($data as $current) {
+            $lines[] = static::denormalizeLine(json_encode($current));
+        }
+
+        $model->setData($lines);
+        $model->setErrors(ArrayHelper::get($decodedResponse, "Errors", []));
+        $model->setReturnCode(intval(ArrayHelper::get($decodedResponse, "ReturnCode", -1)));
+
+        return $model;
+    }
+}
---
+++ tests/ThirdParty/Adoria/Normalizer/ResponseNormalizerTest.php
@@ -0,0 +1,144 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Adoria\Normalizer;
+
+use DateTime;
+use Exception;
+use WBW\Library\Core\Tests\AbstractTestCase;
+use WBW\Library\Core\ThirdParty\Adoria\Model\Line;
+use WBW\Library\Core\ThirdParty\Adoria\Model\RequestData;
+use WBW\Library\Core\ThirdParty\Adoria\Model\Result;
+use WBW\Library\Core\ThirdParty\Adoria\Normalizer\RequestNormalizer;
+use WBW\Library\Core\ThirdParty\Adoria\Normalizer\ResponseNormalizer;
+
+/**
+ * Object normalizer test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Adoria\Normalizer
+ */
+class ResponseNormalizerTest extends AbstractTestCase {
+
+    /**
+     * Raw response "line".
+     *
+     * @var string
+     */
+    const RAW_RESPONSE_LINE = <<< JSON
+{
+    "BookingJournalCode": "bookingJournalCode",
+    "InvoiceDate": "2019-01-16",
+    "AccountingCode": "401",
+    "SubledgerAccount": "subledgerAccount",
+    "InvoiceNumber": "invoiceNumber",
+    "Reference": "reference",
+    "AnalyticCode": "analyticCode",
+    "Amount": "amount",
+    "Currency": "currency",
+    "AmountSign": "D",
+    "InvoiceDueDate": "2019-01-31",
+    "FreeField": "freeField"
+}
+JSON;
+
+    /**
+     * Raw response "result".
+     *
+     * @var string
+     */
+    const RAW_RESPONSE_RESULT = <<< JSON
+{
+    "ReturnCode": 1,
+    "Errors": [],
+    "Datas": [
+        {
+            "BookingJournalCode": "bookingJournalCode",
+            "InvoiceDate": "2019-01-16",
+            "AccountingCode": "401",
+            "SubledgerAccount": "subledgerAccount",
+            "InvoiceNumber": "invoiceNumber",
+            "Reference": "reference",
+            "AnalyticCode": "analyticCode",
+            "Amount": "amount",
+            "Currency": "currency",
+            "AmountSign": "D",
+            "InvoiceDueDate": "2019-01-31",
+            "FreeField": "freeField"
+        }
+    ]
+}
+JSON;
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $this->assertEquals(RequestNormalizer::NORMALIZE_DATE_FORMAT, ResponseNormalizer::DENORMALIZE_DATE_FORMAT);
+    }
+
+    /**
+     * Tests the denormalizeLine() method.
+     *
+     * @return void
+     */
+    public function testDenormalizeLine() {
+
+        $obj = ResponseNormalizer::denormalizeLine(self::RAW_RESPONSE_LINE);
+
+        $this->assertInstanceOf(Line::class, $obj);
+
+        $this->assertEquals("401", $obj->getAccountingCode());
+        $this->assertEquals("amount", $obj->getAmount());
+        $this->assertEquals("D", $obj->getAmountSign());
+        $this->assertEquals("analyticCode", $obj->getAnalyticCode());
+        $this->assertEquals("bookingJournalCode", $obj->getBookingJournalCode());
+        $this->assertEquals("currency", $obj->getCurrency());
+        $this->assertEquals("freeField", $obj->getFreeField());
+        $this->assertEquals("2019-01-16", $obj->getInvoiceDate()->format("Y-m-d"));
+        $this->assertEquals("2019-01-31", $obj->getInvoiceDueDate()->format("Y-m-d"));
+        $this->assertEquals("invoiceNumber", $obj->getInvoiceNumber());
+        $this->assertEquals("reference", $obj->getReference());
+        $this->assertEquals("subledgerAccount", $obj->getSubledgerAccount());
+    }
+
+    /**
+     * Tests the denormalizeResult() method.
+     *
+     * @return void
+     */
+    public function testDenormalizeResult() {
+
+        $obj = ResponseNormalizer::denormalizeResult(self::RAW_RESPONSE_RESULT);
+
+        $this->assertInstanceOf(Result::class, $obj);
+
+        $this->assertCount(1, $obj->getData());
+        $this->assertEquals([], $obj->getErrors());
+        $this->assertEquals(1, $obj->getReturnCode());
+
+        $this->assertEquals("401", $obj->getData()[0]->getAccountingCode());
+        $this->assertEquals("amount", $obj->getData()[0]->getAmount());
+        $this->assertEquals("D", $obj->getData()[0]->getAmountSign());
+        $this->assertEquals("analyticCode", $obj->getData()[0]->getAnalyticCode());
+        $this->assertEquals("bookingJournalCode", $obj->getData()[0]->getBookingJournalCode());
+        $this->assertEquals("currency", $obj->getData()[0]->getCurrency());
+        $this->assertEquals("freeField", $obj->getData()[0]->getFreeField());
+        $this->assertEquals("2019-01-16", $obj->getData()[0]->getInvoiceDate()->format("Y-m-d"));
+        $this->assertEquals("2019-01-31", $obj->getData()[0]->getInvoiceDueDate()->format("Y-m-d"));
+        $this->assertEquals("invoiceNumber", $obj->getData()[0]->getInvoiceNumber());
+        $this->assertEquals("reference", $obj->getData()[0]->getReference());
+        $this->assertEquals("subledgerAccount", $obj->getData()[0]->getSubledgerAccount());
+    }
+}
