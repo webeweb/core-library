5798ea227d9c338ca39838c554f035471b84dd88
webeweb <support@webeweb.fr>
2019-11-04 18:26:18 +0100

    Add Civility interface

---
+++ tests/Model/CivilityInterfaceTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Model;
+
+use WBW\Library\Core\Model\CivilityInterface;
+use WBW\Library\Core\Tests\AbstractTestCase;
+
+/**
+ * Civility interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Core\Tests\Model
+ */
+class CivilityInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $this->assertEquals(-1, CivilityInterface::CIVILITY_EMPTY);
+        $this->assertEquals(164, CivilityInterface::CIVILITY_MISS);
+        $this->assertEquals(301, CivilityInterface::CIVILITY_MISTER);
+        $this->assertEquals(106, CivilityInterface::CIVILITY_MRS);
+    }
+}
