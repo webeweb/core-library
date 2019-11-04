ceb6cf5fa540a13e2dc2c1c03ad4623a83588636
webeweb <support@webeweb.fr>
2019-11-04 18:48:43 +0100

    Add Adoria abstract exception

---
+++ tests/Fixtures/ThirdParty/Adoria/Exception/TestException.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Fixtures\ThirdParty\Adoria\Exception;
+
+use WBW\Library\Core\ThirdParty\Adoria\Exception\AbstractException;
+
+/**
+ * Test exception.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Fixtures\Adoria\Exception
+ */
+class TestException extends AbstractException {
+
+}
