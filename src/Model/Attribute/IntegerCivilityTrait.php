1b4f31f74523c9e0e6e9d942f7605e9113e0a51a
webeweb <support@webeweb.fr>
2019-11-04 18:24:39 +0100

    Add integer Civility trait

---
+++ src/Model/Attribute/IntegerCivilityTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Model\Attribute;
+
+/**
+ * Integer civility trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Core\Model\Attribute
+ */
+trait IntegerCivilityTrait {
+
+    /**
+     * Civility.
+     *
+     * @var int
+     */
+    protected $civility;
+
+    /**
+     * Get the civility.
+     *
+     * @return int Returns the civility.
+     */
+    public function getCivility() {
+        return $this->civility;
+    }
+
+    /**
+     * Set the civility.
+     *
+     * @param int $civility The civility.
+     */
+    public function setCivility($civility) {
+        $this->civility = $civility;
+        return $this;
+    }
+
+}
