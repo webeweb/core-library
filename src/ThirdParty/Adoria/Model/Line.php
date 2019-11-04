fef43ed413af6276768bccc9ce525871c67a1118
webeweb <support@webeweb.fr>
2019-11-04 18:49:34 +0100

    Add Adoria Line model

---
+++ src/ThirdParty/Adoria/Model/Line.php
@@ -0,0 +1,354 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\ThirdParty\Adoria\Model;
+
+use DateTime;
+
+/**
+ * Line.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\ThirdParty\Adoria\Model
+ */
+class Line {
+
+    /**
+     * Accounting code.
+     *
+     * @var string
+     */
+    private $accountingCode;
+
+    /**
+     * Amount.
+     *
+     * @var string
+     */
+    private $amount;
+
+    /**
+     * Amount sign.
+     *
+     * @var string
+     */
+    private $amountSign;
+
+    /**
+     * Analytic code.
+     *
+     * @var string
+     */
+    private $analyticCode;
+
+    /**
+     * Booking journal code.
+     *
+     * @var string
+     */
+    private $bookingJournalCode;
+
+    /**
+     * Currency.
+     *
+     * @var string
+     */
+    private $currency;
+
+    /**
+     * Free field.
+     *
+     * @var string
+     */
+    private $freeField;
+
+    /**
+     * Invoice date.
+     *
+     * @var DateTime
+     */
+    private $invoiceDate;
+
+    /**
+     * Invoice due date.
+     *
+     * @var DateTime
+     */
+    private $invoiceDueDate;
+
+    /**
+     * Invoice number.
+     *
+     * @var string
+     */
+    private $invoiceNumber;
+
+    /**
+     * Reference.
+     *
+     * @var string
+     */
+    private $reference;
+
+    /**
+     * Subledger account.
+     *
+     * @var string
+     */
+    private $subledgerAccount;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO.
+    }
+
+    /**
+     * Get the accounting code.
+     *
+     * @return string Returns the accounting code.
+     */
+    public function getAccountingCode() {
+        return $this->accountingCode;
+    }
+
+    /**
+     * Get the amount.
+     *
+     * @return string Returns the amount.
+     */
+    public function getAmount() {
+        return $this->amount;
+    }
+
+    /**
+     * Get the amount sign.
+     *
+     * @return string Returns the amount sign.
+     */
+    public function getAmountSign() {
+        return $this->amountSign;
+    }
+
+    /**
+     * Get the analytic code.
+     *
+     * @return string Returns the analytic code.
+     */
+    public function getAnalyticCode() {
+        return $this->analyticCode;
+    }
+
+    /**
+     * Get the booking journal code.
+     *
+     * @return string Returns the booking journal code.
+     */
+    public function getBookingJournalCode() {
+        return $this->bookingJournalCode;
+    }
+
+    /**
+     * Get the currency.
+     *
+     * @return string Returns the currency.
+     */
+    public function getCurrency() {
+        return $this->currency;
+    }
+
+    /**
+     * Get the free field.
+     *
+     * @return string Returns the free field.
+     */
+    public function getFreeField() {
+        return $this->freeField;
+    }
+
+    /**
+     * Get the invoice date.
+     *
+     * @return DateTime Returns the invoice date.
+     */
+    public function getInvoiceDate() {
+        return $this->invoiceDate;
+    }
+
+    /**
+     * Get the invoice date.
+     *
+     * @return DateTime Returns the invoice date.
+     */
+    public function getInvoiceDueDate() {
+        return $this->invoiceDueDate;
+    }
+
+    /**
+     * Get the invoice number.
+     *
+     * @return string Returns the invoice number.
+     */
+    public function getInvoiceNumber() {
+        return $this->invoiceNumber;
+    }
+
+    /**
+     * Get the reference.
+     *
+     * @return string Returns the reference.
+     */
+    public function getReference() {
+        return $this->reference;
+    }
+
+    /**
+     * Get the subledger account.
+     *
+     * @return string Returns the subledger account.
+     */
+    public function getSubledgerAccount() {
+        return $this->subledgerAccount;
+    }
+
+    /**
+     * Set the accounting code.
+     *
+     * @param string $accountingCode The accounting code.
+     * @return Line Returns this line.
+     */
+    public function setAccountingCode($accountingCode) {
+        $this->accountingCode = $accountingCode;
+        return $this;
+    }
+
+    /**
+     * Set the amount.
+     *
+     * @param string $amount The amount.
+     * @return Line Returns this line.
+     */
+    public function setAmount($amount) {
+        $this->amount = $amount;
+        return $this;
+    }
+
+    /**
+     * Set the amount sign.
+     *
+     * @param string $amountSign The amount sign.
+     * @return Line Returns this line.
+     */
+    public function setAmountSign($amountSign) {
+        $this->amountSign = $amountSign;
+        return $this;
+    }
+
+    /**
+     * Set the analytic code.
+     *
+     * @param string $analyticCode The analytic code.
+     * @return Line Returns this line.
+     */
+    public function setAnalyticCode($analyticCode) {
+        $this->analyticCode = $analyticCode;
+        return $this;
+    }
+
+    /**
+     * Set the booking journal code.
+     *
+     * @param string $bookingJournalCode The booking journal code.
+     * @return Line Returns this line.
+     */
+    public function setBookingJournalCode($bookingJournalCode) {
+        $this->bookingJournalCode = $bookingJournalCode;
+        return $this;
+    }
+
+    /**
+     * Set the currency.
+     *
+     * @param string $currency The currency.
+     * @return Line Returns this line.
+     */
+    public function setCurrency($currency) {
+        $this->currency = $currency;
+        return $this;
+    }
+
+    /**
+     * Set the free field.
+     *
+     * @param string $freeField The free field.
+     * @return Line Returns this line.
+     */
+    public function setFreeField($freeField) {
+        $this->freeField = $freeField;
+        return $this;
+    }
+
+    /**
+     * Set the invoice date.
+     *
+     * @param DateTime $invoiceDate The invoice date.
+     * @return Line Returns this line.
+     */
+    public function setInvoiceDate(DateTime $invoiceDate = null) {
+        $this->invoiceDate = $invoiceDate;
+        return $this;
+    }
+
+    /**
+     * Set the invoice due date.
+     *
+     * @param DateTime $invoiceDueDate The invoice due date.
+     * @return Line Returns this line.
+     */
+    public function setInvoiceDueDate($invoiceDueDate) {
+        $this->invoiceDueDate = $invoiceDueDate;
+        return $this;
+    }
+
+    /**
+     * Set the invoice number.
+     *
+     * @param string $invoiceNumber The invoice number.
+     * @return Line Returns this line.
+     */
+    public function setInvoiceNumber($invoiceNumber) {
+        $this->invoiceNumber = $invoiceNumber;
+        return $this;
+    }
+
+    /**
+     * Set the reference.
+     *
+     * @param string $reference The reference.
+     * @return Line Returns this line.
+     */
+    public function setReference($reference) {
+        $this->reference = $reference;
+        return $this;
+    }
+
+    /**
+     * Set the subledger account.
+     *
+     * @param string $subledgerAccount The subledger account.
+     * @return Line Returns this line.
+     */
+    public function setSubledgerAccount($subledgerAccount) {
+        $this->subledgerAccount = $subledgerAccount;
+        return $this;
+    }
+}
---
+++ tests/ThirdParty/Adoria/Model/LineTest.php
@@ -0,0 +1,213 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Adoria\Model;
+
+use DateTime;
+use Exception;
+use WBW\Library\Core\Tests\AbstractTestCase;
+use WBW\Library\Core\ThirdParty\Adoria\Model\Line;
+
+/**
+ * Line test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Adoria\Model
+ */
+class LineTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new Line();
+
+        $this->assertNull($obj->getAccountingCode());
+        $this->assertNull($obj->getAmount());
+        $this->assertNull($obj->getAmountSign());
+        $this->assertNull($obj->getAnalyticCode());
+        $this->assertNull($obj->getBookingJournalCode());
+        $this->assertNull($obj->getCurrency());
+        $this->assertNull($obj->getFreeField());
+        $this->assertNull($obj->getInvoiceDate());
+        $this->assertNull($obj->getInvoiceDueDate());
+        $this->assertNull($obj->getInvoiceNumber());
+        $this->assertNull($obj->getReference());
+        $this->assertNull($obj->getSubledgerAccount());
+    }
+
+    /**
+     * Tests the setAccountingCode() method.
+     *
+     * @return void
+     */
+    public function testSetAccountingCode() {
+
+        $obj = new Line();
+
+        $obj->setAccountingCode("AccountingCode");
+        $this->assertEquals("AccountingCode", $obj->getAccountingCode());
+    }
+
+    /**
+     * Tests the setAmount() method.
+     *
+     * @return void
+     */
+    public function testSetAmount() {
+
+        $obj = new Line();
+
+        $obj->setAmount("Amount");
+        $this->assertEquals("Amount", $obj->getAmount());
+    }
+
+    /**
+     * Tests the setAmountSign() method.
+     *
+     * @return void
+     */
+    public function testSetAmountSign() {
+
+        $obj = new Line();
+
+        $obj->setAmountSign("AmountSign");
+        $this->assertEquals("AmountSign", $obj->getAmountSign());
+    }
+
+    /**
+     * Tests the setAnalyticCode() method.
+     *
+     * @return void
+     */
+    public function testSetAnalyticCode() {
+
+        $obj = new Line();
+
+        $obj->setAnalyticCode("AnalyticCode");
+        $this->assertEquals("AnalyticCode", $obj->getAnalyticCode());
+    }
+
+    /**
+     * Tests the setBookingJournalCode() method.
+     *
+     * @return void
+     */
+    public function testSetBookingJournalCode() {
+
+        $obj = new Line();
+
+        $obj->setBookingJournalCode("BookingJournalCode");
+        $this->assertEquals("BookingJournalCode", $obj->getBookingJournalCode());
+    }
+
+    /**
+     * Tests the setCurrency() method.
+     *
+     * @return void
+     */
+    public function testSetCurrency() {
+
+        $obj = new Line();
+
+        $obj->setCurrency("Currency");
+        $this->assertEquals("Currency", $obj->getCurrency());
+    }
+
+    /**
+     * Tests the setFreeField() method.
+     *
+     * @return void
+     */
+    public function testSetFreeField() {
+
+        $obj = new Line();
+
+        $obj->setFreeField("FreeField");
+        $this->assertEquals("FreeField", $obj->getFreeField());
+    }
+
+    /**
+     * Tests the setInvoiceDate() method.
+     *
+     * @return void
+     * @throws Exception Throws an exception if an error occurs.
+     */
+    public function testSetInvoiceDate() {
+
+        // Set an Invoice date mock.
+        $invoiceDate = new DateTime();
+
+        $obj = new Line();
+
+        $obj->setInvoiceDate($invoiceDate);
+        $this->assertSame($invoiceDate, $obj->getInvoiceDate());
+    }
+
+    /**
+     * Tests the setInvoiceDueDate() method.
+     *
+     * @return void
+     * @throws Exception Throws an exception if an error occurs.
+     */
+    public function testSetInvoiceDueDate() {
+
+        // Set a Invoice due date mock.
+        $invoiceDueDate = new DateTime();
+
+        $obj = new Line();
+
+        $obj->setInvoiceDueDate($invoiceDueDate);
+        $this->assertSame($invoiceDueDate, $obj->getInvoiceDueDate());
+    }
+
+    /**
+     * Tests the setInvoiceNumber() method.
+     *
+     * @return void
+     */
+    public function testSetInvoiceNumber() {
+
+        $obj = new Line();
+
+        $obj->setInvoiceNumber("InvoiceNumber");
+        $this->assertEquals("InvoiceNumber", $obj->getInvoiceNumber());
+    }
+
+    /**
+     * Tests the setReference() method.
+     *
+     * @return void
+     */
+    public function testSetReference() {
+
+        $obj = new Line();
+
+        $obj->setReference("Reference");
+        $this->assertEquals("Reference", $obj->getReference());
+    }
+
+    /**
+     * Tests the setSubledgerAccount() method.
+     *
+     * @return void
+     */
+    public function testSetSubledgerAccount() {
+
+        $obj = new Line();
+
+        $obj->setSubledgerAccount("SubledgerAccount");
+        $this->assertEquals("SubledgerAccount", $obj->getSubledgerAccount());
+    }
+}
