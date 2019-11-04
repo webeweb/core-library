97338ec04f48375757938424f038a1fa3a1be1a8
webeweb <support@webeweb.fr>
2019-11-04 18:12:06 +0100

    Add float VAT total trait

---
+++ src/Model/Attribute/FloatVatTotalTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Model\Attribute;
+
+/**
+ * Float VAT total trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Model\Attribute
+ */
+trait FloatVatTotalTrait {
+
+    /**
+     *  VAT total.
+     *
+     * @var float
+     */
+    protected $vatTotal;
+
+    /**
+     * Get the VAT total.
+     *
+     * @return float Returns the VAT total.
+     */
+    public function getVatTotal() {
+        return $this->vatTotal;
+    }
+
+    /**
+     * Set the VAT total.
+     *
+     * @param float $vatTotal The VAT total.
+     */
+    public function setVatTotal($vatTotal) {
+        $this->vatTotal = $vatTotal;
+        return $this;
+    }
+}
---
+++ tests/Model/Attribute/FloatVatTotalTraitTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Model\Attribute;
+
+use WBW\Library\Core\Tests\AbstractTestCase;
+use WBW\Library\Core\Tests\Fixtures\Model\Attribute\TestFloatVatTotalTrait;
+
+/**
+ * Float VAT total trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Model\Attribute
+ */
+class FloatVatTotalTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $obj = new TestFloatVatTotalTrait();
+
+        $this->assertNull($obj->getVatTotal());
+    }
+
+    /**
+     * Tests the setVatTotal() method.
+     *
+     * @return void
+     */
+    public function testSetVatTotal() {
+
+        $obj = new TestFloatVatTotalTrait();
+
+        $obj->setVatTotal(1.01);
+        $this->assertEquals(1.01, $obj->getVatTotal());
+    }
+}
