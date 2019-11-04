c53ce7d26ee74c1eb4b7230a436f0aed532a17f6
webeweb <support@webeweb.fr>
2019-11-04 18:28:14 +0100

    Add integer Id trait

---
+++ src/Model/Attribute/IntegerIdTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Model\Attribute;
+
+/**
+ * Integer id trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Model\Attribute
+ */
+trait IntegerIdTrait {
+
+    /**
+     * Id.
+     *
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * Get the id.
+     *
+     * @return int Returns the id.
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * Set the id.
+     *
+     * @param int $id The id.
+     */
+    private function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+}
---
+++ tests/Fixtures/Model/Attribute/TestIntegerIdTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Fixtures\Model\Attribute;
+
+use WBW\Library\Core\Model\Attribute\IntegerIdTrait;
+
+/**
+ * Test integer id trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Fixtures\Model\Attribute
+ */
+class TestIntegerIdTrait {
+
+    use IntegerIdTrait {
+        setId as public;
+    }
+}
