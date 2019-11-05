be80ad47e82826aca36231664b2801ebb6a263b6
webeweb <support@webeweb.fr>
2019-11-05 19:15:00 +0100

    Add string Type trait

---
+++ src/Model/Attribute/StringTypeTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Model\Attribute;
+
+/**
+ * String type trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Model\Attribute
+ */
+trait StringTypeTrait {
+
+    /**
+     * Type.
+     *
+     * @var string
+     */
+    protected $type;
+
+    /**
+     * Get the type.
+     *
+     * @return string Returns the type.
+     */
+    public function getType() {
+        return $this->type;
+    }
+
+    /**
+     * Set the type.
+     *
+     * @param string $type The type.
+     */
+    public function setType($type) {
+        $this->type = $type;
+        return $this;
+    }
+}
