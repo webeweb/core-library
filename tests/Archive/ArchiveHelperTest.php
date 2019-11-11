4999d5899f8955c41369c776021abfbd3c5e9ac7
webeweb <support@webeweb.fr>
2019-11-11 12:33:24 +0100

    Rename File helper

---
+++ tests/Archive/ArchiveHelperTest.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\FileSystem;
+
+use Exception;
+use InvalidArgumentException;
+use WBW\Library\Core\Archive\ArchiveHelper;
+use WBW\Library\Core\Tests\AbstractTestCase;
+
+/**
+ * File helper test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\IO
+ */
+class ArchiveHelperTest extends AbstractTestCase {
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tearDownAfterClass() {
+
+        $filenames = [
+            getcwd() . "/README.md.zip",
+            getcwd() . "/tests.zip",
+        ];
+
+        foreach ($filenames as $current) {
+            if (false === file_exists($current)) {
+                continue;
+            }
+            unlink($current); // Remove files for local unit tests.
+        }
+    }
+
+    /**
+     * Tests the zip() method.
+     *
+     * @return void
+     */
+    public function testZip() {
+
+        $src = getcwd() . "/README.md";
+        $dst = getcwd() . "/README.md.zip";
+
+        ArchiveHelper::zip($src, $dst);
+        $this->assertFileExists($dst);
+        $this->assertGreaterThan(900, filesize($dst));
+    }
+
+    /**
+     * Tests the zip() method.
+     *
+     * @return void
+     */
+    public function testZipWithDirectory() {
+
+        $src = getcwd() . "/tests";
+        $dst = getcwd() . "/tests.zip";
+
+        ArchiveHelper::zip($src, $dst);
+        $this->assertFileExists($dst);
+        $this->assertGreaterThan(38000, filesize($dst));
+    }
+
+    /**
+     * Tests the zip() method.
+     *
+     * @return void
+     */
+    public function testZipWithInvalidArgumentException() {
+
+        try {
+
+            ArchiveHelper::zip("exception", "exception.zip");
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(InvalidArgumentException::class, $ex);
+            $this->assertEquals("The file \"exception\" was not found", $ex->getMessage());
+        }
+    }
+}
