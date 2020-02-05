517a9134b10d146bf74b1bd963bf6d22f22316ab
webeweb <support@webeweb.fr>
2020-02-05 18:38:58 +0100

    Add float X trait

---
+++ src/Model/Attribute/FloatXTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Model\Attribute;
+
+/**
+ * Float x trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Model\Attribute
+ */
+trait FloatXTrait {
+
+    /**
+     * X.
+     *
+     * @var float
+     */
+    protected $x;
+
+    /**
+     * Get the x.
+     *
+     * @return float Returns the x.
+     */
+    public function getX() {
+        return $this->x;
+    }
+
+    /**
+     * Set the x.
+     *
+     * @param float $x The x.
+     */
+    public function setX($x) {
+        $this->x = $x;
+        return $this;
+    }
+}
---
+++ tests/Model/Attribute/FloatXTraitTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Model\Attribute;
+
+use WBW\Library\Core\Tests\AbstractTestCase;
+use WBW\Library\Core\Tests\Fixtures\Model\Attribute\TestFloatXTrait;
+
+/**
+ * Float x trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Core\Tests\Model\Attribute
+ */
+class FloatXTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $obj = new TestFloatXTrait();
+
+        $this->assertNull($obj->getX());
+    }
+
+    /**
+     * Tests the setX() method.
+     *
+     * @return void
+     */
+    public function testSetX() {
+
+        $obj = new TestFloatXTrait();
+
+        $obj->setX(1.01);
+        $this->assertEquals(1.01, $obj->getX());
+    }
+}
