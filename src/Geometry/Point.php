21c64077673db6ec1c52cc1d103847b42c718d41
webeweb <support@webeweb.fr>
2020-02-09 17:14:31 +0100

    Add Point

---
+++ src/Geometry/Point.php
@@ -0,0 +1,113 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Geometry;
+
+use WBW\Library\Core\Model\Attribute\FloatXTrait;
+use WBW\Library\Core\Model\Attribute\FloatYTrait;
+
+/**
+ * Point.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Core\Geometry
+ */
+class Point {
+
+    use FloatXTrait;
+    use FloatYTrait;
+
+    /**
+     * Constructor.
+     *
+     * @param float $x The x.
+     * @param float $y The y.
+     */
+    public function __construct($x = null, $y = null) {
+        $this->setX($x);
+        $this->setY($y);
+    }
+
+    /**
+     * Distance d = sqrt(a² + b²).
+     *
+     * @param Point $p The point.
+     * @return float Returns the distance.
+     */
+    public function d(Point $p) {
+
+        $x = $this->getX() - $p->getY();
+        $y = $this->getY() - $p->getY();
+
+        $x2 = $x * $x;
+        $y2 = $y * $y;
+
+        return sqrt($x2 + $y2);
+    }
+
+    /**
+     * Degree.
+     *
+     * @param Point $p The point.
+     * @return float Returns the degree.
+     */
+    public function deg(Point $p) {
+        return $this->rad($p) * 180 / pi();
+    }
+
+    /**
+     * Slope m = (y2 - y1) / (x2 - x1).
+     *
+     * @param Point $p The point.
+     * @return float Returns the slope.
+     */
+    public function m(Point $p) {
+
+        $x = $this->getX() - $p->getX();
+        $y = $this->getY() - $p->getY();
+
+        if (0 === $x) {
+            return 0.0;
+        }
+
+        return $y / $x;
+    }
+
+    /**
+     * Radian.
+     *
+     * @param Point $p The point.
+     * @return float Returns the radian.
+     */
+    public function rad(Point $p) {
+        return atan($this->m($p));
+    }
+
+    /**
+     * Rotate.
+     *
+     * @param Point $o The point.
+     * @param float $deg The degrees.
+     * @return Point Returns the rotated point.
+     */
+    public function rotate(Point $o, $deg) {
+
+        $rad = $deg * pi() / 180;
+
+        $dX = $this->getX() - $o->getX();
+        $dY = $this->getY() - $o->getY();
+
+        $x = $dX * cos($rad) + $dY * sin($rad) + $o->getX();
+        $y = -$dX * sin($rad) + $dY * cos($rad) + $o->getY();
+
+        return new Point($x, $y);
+    }
+}
---
+++ tests/Geometry/PointTest.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\Geometry;
+
+use WBW\Library\Core\Geometry\Point;
+use WBW\Library\Core\Tests\AbstractTestCase;
+
+/**
+ * Point test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Core\Tests\Geometry
+ */
+class PointTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new Point();
+
+        $this->assertNull($obj->getX());
+        $this->assertNull($obj->getY());
+    }
+
+    /**
+     * Tests the d() method.
+     *
+     * @return void
+     */
+    public function testD() {
+
+        $obj = new Point(3, 4);
+
+        $this->assertEquals(5, $obj->d(new Point(0, 0)));
+    }
+
+    /**
+     * Tests the deg() method.
+     *
+     * @return void
+     */
+    public function testDeg() {
+
+        $obj = new Point(5, 5);
+
+        $this->assertEquals(45, $obj->deg(new Point(0, 0)));
+    }
+
+    /**
+     * Tests the m() method.
+     *
+     * @return void
+     */
+    public function testM() {
+
+        $obj = new Point(5, 5);
+
+        $this->assertEquals(1, $obj->m(new Point(0, 0)));
+        $this->assertEquals(0, $obj->m(new Point(0, 5)));
+    }
+
+    /**
+     * Tests the rotate() method.
+     *
+     * @return void
+     */
+    public function testRotate() {
+
+        $obj = new Point(5, 5);
+
+        $res = $obj->rotate(new Point(0, 0), 90);
+        $this->assertEquals(5, $res->getX());
+        $this->assertEquals(-5, $res->getY());
+    }
+}
