ad739d27ed51d7c83525b2f24a52898909815aef
webeweb <support@webeweb.fr>
2020-03-25 18:37:11 +0100

    Add Document helper

---
+++ src/ThirdParty/OcrLad/Helper/DocumentHelper.php
@@ -0,0 +1,151 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\ThirdParty\OcrLad\Helper;
+
+use WBW\Library\Core\Geometry\Point;
+use WBW\Library\Core\ThirdParty\OcrLad\Model\Document;
+use WBW\Library\Core\ThirdParty\OcrLad\Model\Page;
+use WBW\Library\Core\ThirdParty\OcrLad\Model\Word;
+
+/**
+ * Document helper.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Core\ThirdParty\OcrLad\Helper
+ */
+class DocumentHelper {
+
+    /**
+     * Document.
+     *
+     * @var Document
+     */
+    private $document;
+
+    /**
+     * Constructor.
+     *
+     * @param Document $document The document.
+     */
+    public function __construct(Document $document) {
+        $this->setDocument($document);
+    }
+
+    /**
+     * Find a word.
+     *
+     * @param Point $position The position.
+     * @param Page|null $page The page.
+     * @return Word Returns the word in case of success, null otherwise.
+     */
+    public function findWordAt(Point $position, Page $page = null) {
+
+        foreach ($this->getWords($page) as $current) {
+
+            $dX = $position->getXInt() === $current->getPoint1()->getXInt();
+            $dY = $position->getYInt() === $current->getPoint1()->getYInt();
+
+            if (true === $dX && true === $dY) {
+                return $current;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Find the words.
+     *
+     * @param Point $start The start position.
+     * @param Point $end The end position.
+     * @param Page|null $page The page.
+     * @return Word[] Returns the words.
+     */
+    public function findWordsIn(Point $start, Point $end, Page $page = null) {
+
+        $matches = [];
+
+        foreach ($this->getWords($page) as $current) {
+
+            $dX1 = $start->getXInt() <= $current->getPoint1()->getXInt();
+            $dY1 = $start->getYInt() === $current->getPoint1()->getYInt();
+
+            $dX2 = $current->getPoint2()->getXInt() < $end->getXInt();
+            $dY2 = $current->getPoint1()->getYInt() <= $end->getYInt();
+
+            if (true === $dX1 && true === $dY1 && true === $dX2 && true === $dY2) {
+                $matches[] = $current;
+            }
+        }
+
+        return $matches;
+    }
+
+    /**
+     * Find the words.
+     *
+     * @param Point $start The start position.
+     * @param Page|null $page The page.
+     * @return Word[] Returns the words.
+     */
+    public function findWordsStartAt(Point $start, Page $page = null) {
+
+        $matches = [];
+
+        foreach ($this->getWords($page) as $current) {
+
+            $dX = $start->getXInt() <= $current->getPoint1()->getXInt();
+            $dY = $start->getYInt() === $current->getPoint1()->getYInt();
+
+            if (true === $dX && true === $dY) {
+                $matches[] = $current;
+            }
+        }
+
+        return $matches;
+    }
+
+    /**
+     * Get the document.
+     *
+     * @return Document Returns the document.
+     */
+    public function getDocument() {
+        return $this->document;
+    }
+
+    /**
+     * Get the words.
+     *
+     * @param Page|null $page The page.
+     * @return Word[] Returns the words.
+     */
+    protected function getWords(Page $page = null) {
+
+        if (null !== $page) {
+            return $page->getWords();
+        }
+
+        return $this->getDocument()->getWords();
+    }
+
+    /**
+     * Set the document.
+     *
+     * @param Document $document The document.
+     * @return DocumentHelper Returns this document helper.
+     */
+    protected function setDocument($document) {
+        $this->document = $document;
+        return $this;
+    }
+}
---
+++ tests/ThirdParty/OcrLab/Helper/DocumentHelperTest.php
@@ -0,0 +1,103 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Tests\ThirdParty\OcrLab\Helper;
+
+use WBW\Library\Core\Geometry\Point;
+use WBW\Library\Core\Tests\AbstractTestCase;
+use WBW\Library\Core\ThirdParty\OcrLad\Helper\DocumentHelper;
+use WBW\Library\Core\ThirdParty\OcrLad\Model\Document;
+use WBW\Library\Core\ThirdParty\OcrLad\Serializer\DerDeserializer;
+
+/**
+ * Document helper test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Core\Tests\ThirdParty\OcrLab\Helper
+ */
+class DocumentHelperTest extends AbstractTestCase {
+
+    /**
+     * Document.
+     *
+     * @var Document
+     */
+    private $document;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        // Set a filename mock.
+        $filename = getcwd() . "/tests/Fixtures/ThirdParty/OcrLad/OcrLad.der";
+
+        // Set a Document mock.
+        $this->document = DerDeserializer::deserializeDocument($filename);
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DocumentHelper($this->document);
+
+        $this->assertSame($this->document, $obj->getDocument());
+    }
+
+    /**
+     * Tests the findWordAt() method.
+     *
+     * @return void
+     */
+    public function testFindWordAt() {
+
+        $obj = new DocumentHelper($this->document);
+
+        $res = $obj->findWordAt(new Point(0, 0));
+        $this->assertEquals("Hello", $res->getContent());
+    }
+
+    /**
+     * Tests the findWordsIn() method.
+     *
+     * @return void
+     */
+    public function testFindWordsIn() {
+
+        $obj = new DocumentHelper($this->document);
+
+        $res = $obj->findWordsIn(new Point(0, 0), new Point(60, 20));
+        $this->assertCount(1, $res);
+
+        $this->assertEquals("Hello", $res[0]->getContent());
+    }
+
+    /**
+     * Tests the findWordsStartAt() method.
+     *
+     * @return void
+     */
+    public function testFindWordsStartAt() {
+
+        $obj = new DocumentHelper($this->document);
+
+        $res = $obj->findWordsStartAt(new Point(0, 0));
+        $this->assertCount(2, $res);
+
+        $this->assertEquals("Hello", $res[0]->getContent());
+        $this->assertEquals("World", $res[1]->getContent());
+    }
+}
