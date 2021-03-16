6fd7951e0d68200989b6eee8e39779ca6cdb1e61
webeweb <support@webeweb.fr>
2021-03-16 18:07:44 +0100

    Add CSV serializable and XML serializable interfaces

---
+++ src/Model/CsvSerializable.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Core\Model;
+
+/**
+ * CSV serializable.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Core\Model
+ */
+interface CsvSerializable {
+
+    /**
+     * CSV serialize.
+     *
+     * @return string Returns the serialized instance into CSV.
+     */
+    public function csvSerialize(): string;
+}
