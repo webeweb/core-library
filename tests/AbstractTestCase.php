b477d49658d50832c782fe6410a9b67cae5886b6
webeweb <support@webeweb.fr>
2021-07-21 14:59:41 +0200

    Update abstract test case

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -13,9 +13,6 @@ namespace WBW\Library\Core\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
-use WBW\Library\Core\Network\CURL\Configuration\CurlConfiguration;
-use WBW\Library\Core\Security\Authenticator;
-use WBW\Library\Core\Security\PasswordAuthentication;
 
 /**
  * Abstract test case.
@@ -25,27 +22,6 @@ use WBW\Library\Core\Security\PasswordAuthentication;
  * @abstract
  */
 abstract class AbstractTestCase extends TestCase {
-
-    /**
-     * Authenticator.
-     *
-     * @var Authenticator
-     */
-    protected $authenticator;
-
-    /**
-     * cURL configuration.
-     *
-     * @var CurlConfiguration
-     */
-    protected $curlConfiguration;
-
-    /**
-     * cURL resource path.
-     *
-     * @var string
-     */
-    protected $curlResourcePath;
 
     /**
      * Logger.
@@ -59,16 +35,6 @@ abstract class AbstractTestCase extends TestCase {
      */
     protected function setUp(): void {
         parent::setUp();
-
-        // Set an Authenticator mock.
-        $this->authenticator = new Authenticator("localhost", new PasswordAuthentication("username", "password"));
-
-        // Set a cURL configuration mock.
-        $this->curlConfiguration = new CurlConfiguration();
-        $this->curlConfiguration->setHost("https://webeweb.dev/");
-
-        // Set a cURL resource path mock.
-        $this->curlResourcePath = "curl-library.php";
 
         // Set a Logger mock.
         $this->logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
