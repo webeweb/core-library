589dff4bb7a451bd6175c1002fed96675a185fe2
webeweb <support@webeweb.fr>
2021-08-24 11:01:27 +0200

    Add string icon trait

---
+++ tests/traits/Strings/StringIconTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Strings;
+
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\Strings\TestStringIconTrait;
+
+/**
+ * String icon trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Traits\Tests\Strings
+ */
+class StringIconTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setIcon() method.
+     *
+     * @return void
+     */
+    public function testSetIcon(): void {
+
+        $obj = new TestStringIconTrait();
+
+        $obj->setIcon("icon");
+        $this->assertEquals("icon", $obj->getIcon());
+    }
+}
