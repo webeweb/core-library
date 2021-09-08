51c03f54ab14fab7ed507e771d2490b846807e6c
webeweb <support@webeweb.fr>
2021-09-08 16:23:35 +0200

    Add sending address trait

---
+++ tests/bill/Model/SendingAddressTraitTest.php
@@ -0,0 +1,102 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Bill\Tests\Model;
+
+use WBW\Library\Bill\Tests\AbstractTestCase;
+use WBW\Library\Bill\Tests\Fixtures\Model\TestSendingAddressTrait;
+
+/**
+ * Sending address trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Bill\Tests\Model
+ */
+class SendingAddressTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setSendingAddressAddressee() method.
+     *
+     * @return void
+     */
+    public function testSetSendingAddressAddressee(): void {
+
+        $obj = new TestSendingAddressTrait();
+
+        $obj->setSendingAddressAddressee("sendingAddressAddressee");
+        $this->assertEquals("sendingAddressAddressee", $obj->getSendingAddressAddressee());
+    }
+
+    /**
+     * Tests the setSendingAddressCountry() method.
+     *
+     * @return void
+     */
+    public function testSetSendingAddressCountry(): void {
+
+        $obj = new TestSendingAddressTrait();
+
+        $obj->setSendingAddressCountry("sendingAddressCountry");
+        $this->assertEquals("sendingAddressCountry", $obj->getSendingAddressCountry());
+    }
+
+    /**
+     * Tests the setSendingAddressHouseNumber() method.
+     *
+     * @return void
+     */
+    public function testSetSendingAddressHouseNumber(): void {
+
+        $obj = new TestSendingAddressTrait();
+
+        $obj->setSendingAddressHouseNumber("sendingAddressHouseNumber");
+        $this->assertEquals("sendingAddressHouseNumber", $obj->getSendingAddressHouseNumber());
+    }
+
+    /**
+     * Tests the setSendingAddressLocation() method.
+     *
+     * @return void
+     */
+    public function testSetSendingAddressLocation(): void {
+
+        $obj = new TestSendingAddressTrait();
+
+        $obj->setSendingAddressLocation("sendingAddressLocation");
+        $this->assertEquals("sendingAddressLocation", $obj->getSendingAddressLocation());
+    }
+
+    /**
+     * Tests the setSendingAddressPostalCode() method.
+     *
+     * @return void
+     */
+    public function testSetSendingAddressPostalCode(): void {
+
+        $obj = new TestSendingAddressTrait();
+
+        $obj->setSendingAddressPostalCode("sendingAddressPostalCode");
+        $this->assertEquals("sendingAddressPostalCode", $obj->getSendingAddressPostalCode());
+    }
+
+    /**
+     * Tests the setSendingAddressStreetName() method.
+     *
+     * @return void
+     */
+    public function testSetSendingAddressStreetName(): void {
+
+        $obj = new TestSendingAddressTrait();
+
+        $obj->setSendingAddressStreetName("sendingAddressStreetName");
+        $this->assertEquals("sendingAddressStreetName", $obj->getSendingAddressStreetName());
+    }
+}
