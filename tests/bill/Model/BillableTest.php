06690300e17f24636d6b431f005b0596de7084a5
webeweb <support@webeweb.fr>
2021-09-08 10:45:22 +0200

    Add billable model

---
+++ tests/bill/Model/BillableTest.php
@@ -0,0 +1,106 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Bill\Tests\Model;
+
+use DateTime;
+use Exception;
+use WBW\Library\Bill\Model\Billable;
+use WBW\Library\Bill\Model\BillableDetailInterface;
+use WBW\Library\Bill\Model\BillableInterface;
+use WBW\Library\Bill\Tests\AbstractTestCase;
+
+/**
+ * Billable test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Bill\Tests\Model
+ */
+class BillableTest extends AbstractTestCase {
+
+    /**
+     * Tests the removeDetail() method.
+     *
+     * @return void
+     */
+    public function testRemoveDetail(): void {
+
+        // Set a Billable detail mock.
+        $detail = $this->getMockBuilder(BillableDetailInterface::class)->getMock();
+
+        $obj = new Billable();
+
+        $obj->addDetail($detail);
+        $this->assertSame($detail, $obj->getDetails()[0]);
+
+        $obj->removeDetail($detail);
+        $this->assertEquals([], $obj->getDetails());
+    }
+
+    /**
+     * Tests the setParent() method.
+     *
+     * @return void
+     */
+    public function testSetParent(): void {
+
+        // Set a Billable mock.
+        $parent = $this->getMockBuilder(BillableInterface::class)->getMock();
+
+        $obj = new Billable();
+
+        $obj->setParent($parent);
+        $this->assertSame($parent, $obj->getParent());
+    }
+
+    /**
+     * Tests the setPaymentDate() method.
+     *
+     * @return void
+     * @throws Exception Throws an exception if an error occurs.
+     */
+    public function testSetPaymentDate(): void {
+
+        // Set a Payment date mock.
+        $paymentDate = new DateTime();
+
+        $obj = new Billable();
+
+        $obj->setPaymentDate($paymentDate);
+        $this->assertSame($paymentDate, $obj->getPaymentDate());
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new Billable();
+
+        $this->assertInstanceOf(BillableInterface::class, $obj);
+
+        $this->assertNull($obj->getComment());
+        $this->assertNull($obj->getCreatedAt());
+        $this->assertNull($obj->getDate());
+        $this->assertEquals([], $obj->getDetails());
+        $this->assertNull($obj->getDiscountRate());
+        $this->assertNull($obj->getExcludingVatTotal());
+        $this->assertNull($obj->getIncludingVatTotal());
+        $this->assertNull($obj->getNumber());
+        $this->assertNull($obj->getParent());
+        $this->assertNull($obj->getPaymentDate());
+        $this->assertNull($obj->getReference());
+        $this->assertNull($obj->getUpdatedAt());
+        $this->assertNull($obj->getVatTotal());
+    }
+}
