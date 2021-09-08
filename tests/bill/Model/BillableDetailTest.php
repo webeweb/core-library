931834c42a5da13e2a172a200e95326b3b066098
webeweb <support@webeweb.fr>
2021-09-08 17:40:43 +0200

    Update Billable detail model: - add __clone() method

--- tests/bill/Model/BillableDetailTest.php
+++ tests/bill/Model/BillableDetailTest.php
@@ -15,6 +15,7 @@ use WBW\Library\Bill\Model\BillableDetailInterface;
 use WBW\Library\Bill\Model\BillableInterface;
 use WBW\Library\Bill\Model\TaxableInterface;
 use WBW\Library\Bill\Tests\AbstractTestCase;
+use WBW\Library\Bill\Tests\Fixtures\Model\TestBillable;
 use WBW\Library\Bill\Tests\Fixtures\Model\TestBillableDetail;
 
 /**
@@ -72,6 +73,57 @@ class BillableDetailTest extends AbstractTestCase {
 
         $obj->setBillable($billable);
         $this->assertSame($billable, $obj->getBillable());
+    }
+
+    /**
+     * Tests the __clone() method.
+     *
+     * @return void
+     */
+    public function test__clone(): void {
+
+        // Set a Billable mock.
+        $billable = new TestBillable();
+
+        $obj = new TestBillableDetail();
+        $obj->setDiscountAmount(0.01);
+        $obj->setDiscountRate(0.02);
+        $obj->setExcludingVatPrice(0.03);
+        $obj->setIncludingVatPrice(0.04);
+        $obj->setVatAmount(0.05);
+        $obj->setVatRate(0.06);
+
+        $obj->setBillable($billable);
+        $obj->setComment("commant");
+        $obj->setDiscountAmount(0.07);
+        $obj->setDiscountRate(0.08);
+        $obj->setDiscountTotal(0.09);
+        $obj->setExcludingVatTotal(0.10);
+        $obj->setIncludingVatTotal(0.11);
+        $obj->setLabel("label");
+        $obj->setQuantity(0.12);
+        $obj->setReference("reference");
+        $obj->setVatTotal(0.13);
+
+        $res = clone $obj;
+        $this->assertEquals($obj->getDiscountAmount(), $res->getDiscountAmount());
+        $this->assertEquals($obj->getDiscountRate(), $res->getDiscountRate());
+        $this->assertEquals($obj->getExcludingVatPrice(), $res->getExcludingVatPrice());
+        $this->assertEquals($obj->getIncludingVatPrice(), $res->getIncludingVatPrice());
+        $this->assertEquals($obj->getVatAmount(), $res->getVatAmount());
+        $this->assertEquals($obj->getVatRate(), $res->getVatRate());
+
+        $this->assertEquals($obj->getBillable(), $res->getBillable());
+        $this->assertEquals($obj->getComment(), $res->getComment());
+        $this->assertEquals($obj->getDiscountAmount(), $res->getDiscountAmount());
+        $this->assertEquals($obj->getDiscountRate(), $res->getDiscountRate());
+        $this->assertEquals($obj->getDiscountTotal(), $res->getDiscountTotal());
+        $this->assertEquals($obj->getExcludingVatTotal(), $res->getExcludingVatTotal());
+        $this->assertEquals($obj->getIncludingVatTotal(), $res->getIncludingVatTotal());
+        $this->assertEquals($obj->getLabel(), $res->getLabel());
+        $this->assertEquals($obj->getQuantity(), $res->getQuantity());
+        $this->assertEquals($obj->getReference(), $res->getReference());
+        $this->assertEquals($obj->getVatTotal(), $res->getVatTotal());
     }
 
     /**
