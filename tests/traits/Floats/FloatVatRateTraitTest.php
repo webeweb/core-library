90bc87aeea26dcdc64f21d780cae5ea6b40f1310
webeweb <support@webeweb.fr>
2021-09-08 09:51:44 +0200

    Add float VAT rate trait

---
+++ tests/traits/Floats/FloatVatRateTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Floats;
+
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\Floats\TestFloatVatRateTrait;
+
+/**
+ * Float VAT rate trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Traits\Tests\Floats
+ */
+class FloatVatRateTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setVatRate() method.
+     *
+     * @return void
+     */
+    public function testSetVatRate(): void {
+
+        $obj = new TestFloatVatRateTrait();
+
+        $obj->setVatRate(0.123456789);
+        $this->assertEquals(0.123456789, $obj->getVatRate());
+    }
+}
