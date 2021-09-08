60f77a991a546733a00959412b978e76f9214816
webeweb <support@webeweb.fr>
2021-09-08 12:29:40 +0200

    Add float discount total trait

---
+++ tests/traits/Fixtures/Floats/TestFloatDiscountTotalTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Fixtures\Floats;
+
+use WBW\Library\Traits\Floats\FloatDiscountTotalTrait;
+
+/**
+ * Test float discount total trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Traits\Tests\Fixtures\Floats
+ */
+class TestFloatDiscountTotalTrait {
+
+    use FloatDiscountTotalTrait;
+}
---
+++ tests/traits/Floats/FloatDiscountTotalTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Floats;
+
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\Floats\TestFloatDiscountTotalTrait;
+
+/**
+ * Float discount total trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Traits\Tests\Floats
+ */
+class FloatDiscountTotalTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setDiscountTotal() method.
+     *
+     * @return void
+     */
+    public function testSetDiscountTotal(): void {
+
+        $obj = new TestFloatDiscountTotalTrait();
+
+        $obj->setDiscountTotal(0.123456789);
+        $this->assertEquals(0.123456789, $obj->getDiscountTotal());
+    }
+}
