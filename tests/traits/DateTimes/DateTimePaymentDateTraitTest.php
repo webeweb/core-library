9800daa7ba4f01ca3337797d0dc7bd4ae7989024
webeweb <support@webeweb.fr>
2021-09-09 11:12:44 +0200

    Add date/time payment date trait

---
+++ tests/traits/DateTimes/DateTimePaymentDateTraitTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\DateTimes;
+
+use DateTime;
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\DateTimes\TestDateTimePaymentDateTrait;
+
+/**
+ * Date/time payment date trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Traits\Tests\DateTimes
+ */
+class DateTimePaymentDateTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setPaymentDate() method.
+     *
+     * @return void
+     */
+    public function testSetPaymentDate(): void {
+
+        // Set a payment date mock.
+        $paymentDate = new DateTime();
+
+        $obj = new TestDateTimePaymentDateTrait();
+
+        $obj->setPaymentDate($paymentDate);
+        $this->assertSame($paymentDate, $obj->getPaymentDate());
+    }
+}
