9d07c8a049e2323ab45f744b264772f0e3ba0b3c
webeweb <support@webeweb.fr>
2021-09-16 14:56:03 +0200

    Add string VAT number trait

---
+++ tests/traits/Strings/StringVatNumberTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Strings;
+
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\Strings\TestStringVatNumberTrait;
+
+/**
+ * String VAT number trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Traits\Tests\Strings
+ */
+class StringVatNumberTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setVatNumber() method.
+     *
+     * @return void
+     */
+    public function testSetVatNumber(): void {
+
+        $obj = new TestStringVatNumberTrait();
+
+        $obj->setVatNumber("vatNumber");
+        $this->assertEquals("vatNumber", $obj->getVatNumber());
+    }
+}
