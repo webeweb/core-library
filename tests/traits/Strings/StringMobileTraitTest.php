51b29fd21a931f810e1301754d3d6e66b4307658
webeweb <support@webeweb.fr>
2021-09-22 12:26:21 +0200

    Add string mobile trait

---
+++ tests/traits/Strings/StringMobileTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Strings;
+
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\Strings\TestStringMobileTrait;
+
+/**
+ * String mobile trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Traits\Tests\Strings
+ */
+class StringMobileTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setMobile() method.
+     *
+     * @return void
+     */
+    public function testSetMobile(): void {
+
+        $obj = new TestStringMobileTrait();
+
+        $obj->setMobile("mobile");
+        $this->assertEquals("mobile", $obj->getMobile());
+    }
+}
