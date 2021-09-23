9e5222df8b52eff25f58c829ea2ff5628cd69823
webeweb <support@webeweb.fr>
2021-09-23 14:44:13 +0200

    Add Serializer keys interface

---
+++ src/bill/Serializer/SerializerKeys.php
@@ -0,0 +1,189 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Bill\Serializer;
+
+/**
+ * Serializer keys.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Bill\Serializer
+ */
+interface SerializerKeys {
+
+    /**
+     * Serializer key "billing address addressee".
+     *
+     * @var string
+     */
+    const BILLING_ADDRESS_ADDRESSEE = "billingAddressAddressee";
+
+    /**
+     * Serializer key "billing address country".
+     *
+     * @var string
+     */
+    const BILLING_ADDRESS_COUNTRY = "billingAddressCountry";
+
+    /**
+     * Serializer key "billing address house number".
+     *
+     * @var string
+     */
+    const BILLING_ADDRESS_HOUSE_NUMBER = "billingAddressHouseNumber";
+
+    /**
+     * Serializer key "billing address location".
+     *
+     * @var string
+     */
+    const BILLING_ADDRESS_LOCATION = "billingAddressLocation";
+
+    /**
+     * Serializer key "billing address postal code".
+     *
+     * @var string
+     */
+    const BILLING_ADDRESS_POSTAL_CODE = "billingAddressPostalCode";
+
+    /**
+     * Serializer key "billing address street name".
+     *
+     * @var string
+     */
+    const BILLING_ADDRESS_STREET_NAME = "billingAddressStreetName";
+
+    /**
+     * Serializer key "delivery address addressee".
+     *
+     * @var string
+     */
+    const DELIVERY_ADDRESS_ADDRESSEE = "deliveryAddressAddressee";
+
+    /**
+     * Serializer key "delivery address country".
+     *
+     * @var string
+     */
+    const DELIVERY_ADDRESS_COUNTRY = "deliveryAddressCountry";
+
+    /**
+     * Serializer key "delivery address house number".
+     *
+     * @var string
+     */
+    const DELIVERY_ADDRESS_HOUSE_NUMBER = "deliveryAddressHouseNumber";
+
+    /**
+     * Serializer key "delivery address location".
+     *
+     * @var string
+     */
+    const DELIVERY_ADDRESS_LOCATION = "deliveryAddressLocation";
+
+    /**
+     * Serializer key "delivery address postal code".
+     *
+     * @var string
+     */
+    const DELIVERY_ADDRESS_POSTAL_CODE = "deliveryAddressPostalCode";
+
+    /**
+     * Serializer key "delivery address street name".
+     *
+     * @var string
+     */
+    const DELIVERY_ADDRESS_STREET_NAME = "deliveryAddressStreetName";
+
+    /**
+     * Serializer key "discount amount".
+     *
+     * @var string
+     */
+    const DISCOUNT_AMOUNT = "discountAmount";
+
+    /**
+     * Serializer key "discount rate".
+     *
+     * @var string
+     */
+    const DISCOUNT_RATE = "discountRate";
+
+    /**
+     * Serializer key "excluding VAT price".
+     *
+     * @var string
+     */
+    const EXCLUDING_VAT_PRICE = "excludingVatPrice";
+
+    /**
+     * Serializer key "including VAT price".
+     *
+     * @var string
+     */
+    const INCLUDING_VAT_PRICE = "includingVatPrice";
+
+    /**
+     * Serializer key "sending address addressee".
+     *
+     * @var string
+     */
+    const SENDING_ADDRESS_ADDRESSEE = "sendingAddressAddressee";
+
+    /**
+     * Serializer key "sending address country".
+     *
+     * @var string
+     */
+    const SENDING_ADDRESS_COUNTRY = "sendingAddressCountry";
+
+    /**
+     * Serializer key "sending address house number".
+     *
+     * @var string
+     */
+    const SENDING_ADDRESS_HOUSE_NUMBER = "sendingAddressHouseNumber";
+
+    /**
+     * Serializer key "sending address location".
+     *
+     * @var string
+     */
+    const SENDING_ADDRESS_LOCATION = "sendingAddressLocation";
+
+    /**
+     * Serializer key "sending address postal code".
+     *
+     * @var string
+     */
+    const SENDING_ADDRESS_POSTAL_CODE = "sendingAddressPostalCode";
+
+    /**
+     * Serializer key "sending address street name".
+     *
+     * @var string
+     */
+    const SENDING_ADDRESS_STREET_NAME = "sendingAddressStreetName";
+
+    /**
+     * Serializer key "vat amount".
+     *
+     * @var string
+     */
+    const VAT_AMOUNT = "vatAmount";
+
+    /**
+     * Serializer key "vat rate".
+     *
+     * @var string
+     */
+    const VAT_RATE = "vatRate";
+}
---
+++ tests/bill/Serializer/SerializerKeysTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Bill\Tests\Serializer;
+
+use WBW\Library\Bill\Serializer\SerializerKeys;
+use WBW\Library\Bill\Tests\AbstractTestCase;
+
+/**
+ * Serializer keys test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Bill\Tests\Serializer
+ */
+class SerializerKeysTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("billingAddressAddressee", SerializerKeys::BILLING_ADDRESS_ADDRESSEE);
+        $this->assertEquals("billingAddressCountry", SerializerKeys::BILLING_ADDRESS_COUNTRY);
+        $this->assertEquals("billingAddressHouseNumber", SerializerKeys::BILLING_ADDRESS_HOUSE_NUMBER);
+        $this->assertEquals("billingAddressLocation", SerializerKeys::BILLING_ADDRESS_LOCATION);
+        $this->assertEquals("billingAddressPostalCode", SerializerKeys::BILLING_ADDRESS_POSTAL_CODE);
+        $this->assertEquals("billingAddressStreetName", SerializerKeys::BILLING_ADDRESS_STREET_NAME);
+        $this->assertEquals("deliveryAddressAddressee", SerializerKeys::DELIVERY_ADDRESS_ADDRESSEE);
+        $this->assertEquals("deliveryAddressCountry", SerializerKeys::DELIVERY_ADDRESS_COUNTRY);
+        $this->assertEquals("deliveryAddressHouseNumber", SerializerKeys::DELIVERY_ADDRESS_HOUSE_NUMBER);
+        $this->assertEquals("deliveryAddressLocation", SerializerKeys::DELIVERY_ADDRESS_LOCATION);
+        $this->assertEquals("deliveryAddressPostalCode", SerializerKeys::DELIVERY_ADDRESS_POSTAL_CODE);
+        $this->assertEquals("deliveryAddressStreetName", SerializerKeys::DELIVERY_ADDRESS_STREET_NAME);
+        $this->assertEquals("discountAmount", SerializerKeys::DISCOUNT_AMOUNT);
+        $this->assertEquals("discountRate", SerializerKeys::DISCOUNT_RATE);
+        $this->assertEquals("excludingVatPrice", SerializerKeys::EXCLUDING_VAT_PRICE);
+        $this->assertEquals("includingVatPrice", SerializerKeys::INCLUDING_VAT_PRICE);
+        $this->assertEquals("sendingAddressAddressee", SerializerKeys::SENDING_ADDRESS_ADDRESSEE);
+        $this->assertEquals("sendingAddressCountry", SerializerKeys::SENDING_ADDRESS_COUNTRY);
+        $this->assertEquals("sendingAddressHouseNumber", SerializerKeys::SENDING_ADDRESS_HOUSE_NUMBER);
+        $this->assertEquals("sendingAddressLocation", SerializerKeys::SENDING_ADDRESS_LOCATION);
+        $this->assertEquals("sendingAddressPostalCode", SerializerKeys::SENDING_ADDRESS_POSTAL_CODE);
+        $this->assertEquals("sendingAddressStreetName", SerializerKeys::SENDING_ADDRESS_STREET_NAME);
+        $this->assertEquals("vatAmount", SerializerKeys::VAT_AMOUNT);
+        $this->assertEquals("vatRate", SerializerKeys::VAT_RATE);
+    }
+}
