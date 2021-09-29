688b12f6598c3a42e7af0ee37d11a1d906c6e105
webeweb <support@webeweb.fr>
2021-09-29 14:15:20 +0200

    Add string object trait

---
+++ tests/traits/Strings/StringObjectTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Strings;
+
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\Strings\TestStringObjectTrait;
+
+/**
+ * String object trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Traits\Tests\Strings
+ */
+class StringObjectTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setObject() method.
+     *
+     * @return void
+     */
+    public function testSetObject(): void {
+
+        $obj = new TestStringObjectTrait();
+
+        $obj->setObject("object");
+        $this->assertEquals("object", $obj->getObject());
+    }
+}
