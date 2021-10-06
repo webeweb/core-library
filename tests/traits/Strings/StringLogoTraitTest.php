b2abebd9b31eba44077a3d56f2e0c6fe9055247f
webeweb <support@webeweb.fr>
2021-10-06 09:49:57 +0200

    Add string logo trait

---
+++ tests/traits/Strings/StringLogoTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Strings;
+
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\Strings\TestStringLogoTrait;
+
+/**
+ * String logo trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Traits\Tests\Strings
+ */
+class StringLogoTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setLogo() method.
+     *
+     * @return void
+     */
+    public function testSetLogo(): void {
+
+        $obj = new TestStringLogoTrait();
+
+        $obj->setLogo("logo");
+        $this->assertEquals("logo", $obj->getLogo());
+    }
+}
