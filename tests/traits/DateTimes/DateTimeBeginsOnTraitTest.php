b7ce4dc9d8f4c52440fdb8aeb9329f9edcf239a3
webeweb <support@webeweb.fr>
2021-10-21 17:44:39 +0200

    Add date/time begins on trait

---
+++ tests/traits/DateTimes/DateTimeBeginsOnTraitTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\DateTimes;
+
+use DateTime;
+use Exception;
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\DateTimes\TestDateTimeBeginsOnTrait;
+
+/**
+ * Date/time begins on trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Traits\Tests\DateTimes
+ */
+class DateTimeBeginsOnTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setBeginsOn() method.
+     *
+     * @return void
+     * @throws Exception Throws an exception if an error occurs.
+     */
+    public function testSetBeginsOn(): void {
+
+        // Set a date/time mock.
+        $beginsOn = new DateTime();
+
+        $obj = new TestDateTimeBeginsOnTrait();
+
+        $obj->setBeginsOn($beginsOn);
+        $this->assertSame($beginsOn, $obj->getBeginsOn());
+    }
+}
