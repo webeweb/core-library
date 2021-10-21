52c1031ba26b5b080b77ce0cfd53e1e189dec2f6
webeweb <support@webeweb.fr>
2021-10-21 17:45:01 +0200

    Add date/time ended on trait

---
+++ tests/traits/DateTimes/DateTimeEndedOnTraitTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\DateTimes;
+
+use DateTime;
+use Exception;
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\DateTimes\TestDateTimeEndedOnTrait;
+
+/**
+ * Date/time ended on trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Traits\Tests\DateTimes
+ */
+class DateTimeEndedOnTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setEndedOn() method.
+     *
+     * @return void
+     * @throws Exception Throws an exception if an error occurs.
+     */
+    public function testSetEndedOn(): void {
+
+        // Set a date/time mock.
+        $endedOn = new DateTime();
+
+        $obj = new TestDateTimeEndedOnTrait();
+
+        $obj->setEndedOn($endedOn);
+        $this->assertSame($endedOn, $obj->getEndedOn());
+    }
+}
