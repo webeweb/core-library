ed3de191fcd257e47a52cc5e4a10059172731700
webeweb <support@webeweb.fr>
2021-10-21 17:44:28 +0200

    Add date/time begins at trait

---
+++ tests/traits/Fixtures/DateTimes/TestDateTimeBeginsAtTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Fixtures\DateTimes;
+
+use WBW\Library\Traits\DateTimes\DateTimeBeginsAtTrait;
+
+/**
+ * Test date/time begins at trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Traits\Tests\Fixtures\DateTimes
+ */
+class TestDateTimeBeginsAtTrait {
+
+    use DateTimeBeginsAtTrait;
+}
