240423068da0e725c0e2c623a822f7025f97f7a9
webeweb <support@webeweb.fr>
2021-10-21 17:45:31 +0200

    Add date/time started at trait

---
+++ tests/traits/DateTimes/DateTimeStartedAtTraitTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\DateTimes;
+
+use DateTime;
+use Exception;
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\DateTimes\TestDateTimeStartedAtTrait;
+
+/**
+ * Date/time started at trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Traits\Tests\DateTimes
+ */
+class DateTimeStartedAtTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setStartedAt() method.
+     *
+     * @return void
+     * @throws Exception Throws an exception if an error occurs.
+     */
+    public function testSetStartedAt(): void {
+
+        // Set a date/time mock.
+        $startedAt = new DateTime();
+
+        $obj = new TestDateTimeStartedAtTrait();
+
+        $obj->setStartedAt($startedAt);
+        $this->assertSame($startedAt, $obj->getStartedAt());
+    }
+}
---
+++ tests/traits/Fixtures/DateTimes/TestDateTimeStartedAtTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Fixtures\DateTimes;
+
+use WBW\Library\Traits\DateTimes\DateTimeStartedAtTrait;
+
+/**
+ * Test date/time started at trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Traits\Tests\Fixtures\DateTimes
+ */
+class TestDateTimeStartedAtTrait {
+
+    use DateTimeStartedAtTrait;
+}
