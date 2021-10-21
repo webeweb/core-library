df2c7e905ecaaf76db819c41e599f14427acde25
webeweb <support@webeweb.fr>
2021-10-21 17:45:21 +0200

    Add date/time finishes on trait

---
+++ tests/traits/Fixtures/DateTimes/TestDateTimeFinishesOnTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Fixtures\DateTimes;
+
+use WBW\Library\Traits\DateTimes\DateTimeFinishesOnTrait;
+
+/**
+ * Test date/time finishes on trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Traits\Tests\Fixtures\DateTimes
+ */
+class TestDateTimeFinishesOnTrait {
+
+    use DateTimeFinishesOnTrait;
+}
