221b674024698f24b1d7ec0e48a30cd345701609
webeweb <support@webeweb.fr>
2021-11-09 11:07:55 +0100

    Fix file header

--- src/traits/Floats/FloatScoreTrait.php
+++ src/traits/Floats/FloatScoreTrait.php
@@ -6,7 +6,7 @@
  * (c) 2021 WEBEWEB
  *
  * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source score.
+ * file that was distributed with this source code.
  */
 
 namespace WBW\Library\Traits\Floats;
