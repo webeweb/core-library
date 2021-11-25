7fcb6208a5c004416b1434c9b1eb2469c903695d
webeweb <support@webeweb.fr>
2021-11-25 18:06:03 +0100

    Add compare() into Object helper

--- src/types/Helper/ObjectHelper.php
+++ src/types/Helper/ObjectHelper.php
@@ -22,6 +22,33 @@ use WBW\Library\Types\Exception\ObjectArgumentException;
 class ObjectHelper {
 
     /**
+     * Compare two objects.
+     *
+     * @param mixed|null $a The object A.
+     * @param mixed|null $b The object B.
+     * @return int|null Returns -1 when $a is not null and $b is null
+     *                      1 when $a is null and $b is not null
+     *                      0 when $a and $b are not null
+     *                      null when $a end $b are null
+     */
+    public static function compare($a, $b): ?int {
+
+        if (null === $a && null !== $b) {
+            return 1;
+        }
+
+        if (null === $a && null === $b) {
+            return null;
+        }
+
+        if (null !== $a && null === $b) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    /**
      * Determines if a value is an object.
      *
      * @param mixed $value The value.
---
+++ tests/types/Helper/ObjectHelperTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Types\Tests\Helper;
+
+use DateTime;
+use WBW\Library\Types\Helper\ObjectHelper;
+use WBW\Library\Types\Tests\AbstractTestCase;
+
+/**
+ * Object helper test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Types\Tests\Helper
+ */
+class ObjectHelperTest extends AbstractTestCase {
+
+    /**
+     * Tests the compare() method.
+     *
+     * @return void
+     */
+    public function testCompare(): void {
+
+        // Set a Date/time mock.
+        $arg = new DateTime();
+
+        $this->assertEquals(null, ObjectHelper::compare(null, null));
+
+        $this->assertEquals(-1, ObjectHelper::compare($arg, null));
+        $this->assertEquals(0, ObjectHelper::compare($arg, $arg));
+        $this->assertEquals(1, ObjectHelper::compare(null, $arg));
+    }
+}
