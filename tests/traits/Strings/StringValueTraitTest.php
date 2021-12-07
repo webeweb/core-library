571091ff884394950dc26ff7ac1792a2dedabf3c
webeweb <support@webeweb.fr>
2021-12-07 10:16:42 +0100

    Add string value trait

---
+++ tests/traits/Strings/StringValueTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Strings;
+
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\Strings\TestStringValueTrait;
+
+/**
+ * String value trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Traits\Tests\Strings
+ */
+class StringValueTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setValue() method.
+     *
+     * @return void
+     */
+    public function testSetValue(): void {
+
+        $obj = new TestStringValueTrait();
+
+        $obj->setValue("Value");
+        $this->assertEquals("Value", $obj->getValue());
+    }
+}
