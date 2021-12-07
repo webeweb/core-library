79f6fe28c2acac7e5645aea1556a0276497e3ebe
webeweb <support@webeweb.fr>
2021-12-07 10:16:36 +0100

    Add integer value trait

---
+++ tests/traits/Integers/IntegerValueTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Integers;
+
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\Integers\TestIntegerValueTrait;
+
+/**
+ * Integer value trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Traits\Tests\Integers
+ */
+class IntegerValueTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setValue() method.
+     *
+     * @return void
+     */
+    public function testSetValue(): void {
+
+        $obj = new TestIntegerValueTrait();
+
+        $obj->setValue(1);
+        $this->assertEquals(1, $obj->getValue());
+    }
+}
