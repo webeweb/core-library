99b6e05027cf6f12d05e2651c219f90dd9eb7f78
webeweb <support@webeweb.fr>
2021-12-07 10:16:10 +0100

    Add boolean value trait

---
+++ tests/traits/Booleans/BooleanValueTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Booleans;
+
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\Booleans\TestBooleanValueTrait;
+
+/**
+ * Boolean value trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Traits\Tests\Booleans
+ */
+class BooleanValueTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setValue() method.
+     *
+     * @return void
+     */
+    public function testSetValue(): void {
+
+        $obj = new TestBooleanValueTrait();
+
+        $obj->setValue(true);
+        $this->assertTrue($obj->getValue());
+    }
+}
---
+++ tests/traits/Fixtures/Booleans/TestBooleanValueTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Fixtures\Booleans;
+
+use WBW\Library\Traits\Booleans\BooleanValueTrait;
+
+/**
+ * Test boolean value trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Traits\Tests\Fixtures\Booleans
+ */
+class TestBooleanValueTrait {
+
+    use BooleanValueTrait;
+}
