cd69901e4befb2aa663e5c5b6a1d14c639f4197e
webeweb <support@webeweb.fr>
2022-01-07 18:00:02 +0100

    Add Payment term factory

---
+++ tests/accounting/Factory/PaymentTermFactoryTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Accounting\Tests\Factory;
+
+use WBW\Library\Accounting\Factory\PaymentTermFactory;
+use WBW\Library\Accounting\Model\PaymentTerm;
+use WBW\Library\Accounting\Tests\AbstractTestCase;
+
+/**
+ * Payment term factory test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Accounting\Tests\Factory
+ */
+class PaymentTermFactoryTest extends AbstractTestCase {
+
+    /**
+     * Tests the copy() method.
+     *
+     * @return void
+     */
+    public function testCopy(): void {
+
+        $obj = new PaymentTerm();
+        $obj->setCode("code");
+        $obj->setLabel("label");
+
+        $res = PaymentTermFactory::copy($obj, new PaymentTerm());
+        $this->assertEquals($obj->getCode(), $res->getCode());
+        $this->assertEquals($obj->getLabel(), $res->getLabel());
+    }
+}
