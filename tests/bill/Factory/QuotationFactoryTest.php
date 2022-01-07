134c043f18cc4e7f67f8b30ea89ad5a8d1dc2eee
webeweb <support@webeweb.fr>
2022-01-07 18:01:46 +0100

    Add Quotation factory

---
+++ tests/bill/Factory/QuotationFactoryTest.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Bill\Tests\Factory;
+
+use DateTime;
+use WBW\Library\Bill\Factory\QuotationFactory;
+use WBW\Library\Bill\Model\BillableDetailInterface;
+use WBW\Library\Bill\Model\Quotation;
+use WBW\Library\Bill\Model\QuotationInterface;
+use WBW\Library\Bill\Tests\AbstractTestCase;
+
+/**
+ * Quotation factory test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Bill\Tests\Factory
+ */
+class QuotationFactoryTest extends AbstractTestCase {
+
+    /**
+     * Tests the copy() method.
+     *
+     * @return void
+     */
+    public function testCopy(): void {
+
+        // Set a Quotation mock.
+        $parent = $this->getMockBuilder(QuotationInterface::class)->getMock();
+
+        // Set a Billable detail mock.
+        $detail = $this->getMockBuilder(BillableDetailInterface::class)->getMock();
+
+        $obj = new Quotation();
+        $obj->setComment("comment");
+        $obj->setCreatedAt(new DateTime());
+        $obj->setDate(new DateTime());
+        $obj->addDetail($detail);
+        $obj->setDiscountRate(0.01);
+        $obj->setDiscountTotal(0.02);
+        $obj->setExcludingVatTotal(0.03);
+        $obj->setExpirationDate(new DateTime());
+        $obj->setIncludingVatTotal(0.04);
+        $obj->setNumber("number");
+        $obj->setParent($parent);
+        $obj->setReference("reference");
+        $obj->setUpdatedAt(new DateTime());
+        $obj->setVatTotal(0.05);
+
+        $res = QuotationFactory::copy($obj, new Quotation());
+        $this->assertEquals($obj->getComment(), $res->getComment());
+        $this->assertEquals($obj->getCreatedAt(), $res->getCreatedAt());
+        $this->assertEquals($obj->getDate(), $res->getDate());
+        $this->assertEquals($obj->getDetails(), $res->getDetails());
+        $this->assertEquals($obj->getDiscountRate(), $res->getDiscountRate());
+        $this->assertEquals($obj->getDiscountTotal(), $res->getDiscountTotal());
+        $this->assertEquals($obj->getExcludingVatTotal(), $res->getExcludingVatTotal());
+        $this->assertEquals($obj->getExpirationDate(), $res->getExpirationDate());
+        $this->assertEquals($obj->getIncludingVatTotal(), $res->getIncludingVatTotal());
+        $this->assertEquals($obj->getNumber(), $res->getNumber());
+        $this->assertEquals($obj->getParent(), $res->getParent());
+        $this->assertEquals($obj->getReference(), $res->getReference());
+        $this->assertEquals($obj->getUpdatedAt(), $res->getUpdatedAt());
+        $this->assertEquals($obj->getVatTotal(), $res->getVatTotal());
+    }
+}
