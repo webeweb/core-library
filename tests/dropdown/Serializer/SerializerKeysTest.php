d7cfa878f79a0770c0c9c7d6dbeb5995dff7a7e8
webeweb <support@webeweb.fr>
2022-01-10 14:44:53 +0100

    Add Serializer keys interface

---
+++ tests/dropdown/Serializer/SerializerKeysTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Dropdown\Tests\Serializer;
+
+use WBW\Library\Dropdown\Serializer\SerializerKeys;
+use WBW\Library\Dropdown\Tests\AbstractTestCase;
+
+/**
+ * Serializer keys test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Dropdown\Tests\Serializer
+ */
+class SerializerKeysTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("byDefault", SerializerKeys::BY_DEFAULT);
+    }
+}
