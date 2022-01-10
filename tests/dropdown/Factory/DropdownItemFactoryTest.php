ed3007d3d7fc40b387cf35ca3588e9ab9c8c2c61
webeweb <support@webeweb.fr>
2022-01-10 14:45:27 +0100

    Add Dropdown item factory

---
+++ tests/dropdown/Factory/DropdownItemFactoryTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Dropdown\Tests\Factory;
+
+use WBW\Library\Dropdown\Factory\DropdownItemFactory;
+use WBW\Library\Dropdown\Model\DropdownItem;
+use WBW\Library\Dropdown\Tests\AbstractTestCase;
+
+/**
+ * Dropdown item factory test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Dropdown\Tests\Factory
+ */
+class DropdownItemFactoryTest extends AbstractTestCase {
+
+    /**
+     * Tests the copy() method.
+     *
+     * @return void
+     */
+    public function testCopy(): void {
+
+        $obj = new DropdownItem();
+        $obj->setByDefault(true);
+        $obj->setLabel("label");
+        $obj->setPosition(1);
+
+        $res = DropdownItemFactory::copy($obj, new DropdownItem());
+        $this->assertEquals($obj->getByDefault(), $res->getByDefault());
+        $this->assertEquals($obj->getLabel(), $res->getLabel());
+        $this->assertEquals($obj->getPosition(), $res->getPosition());
+    }
+}
