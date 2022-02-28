1d6c63f375faff2275d6fa7d1e1243919bd1beac
webeweb <support@webeweb.fr>
2022-02-28 10:43:43 +0100

    Add Fibonacci helper

---
+++ tests/maths/AbstractTestCase.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Maths\Tests;
+
+use WBW\Library\Core\Tests\AbstractTestCase as BaseTestCase;
+
+/**
+ * Abstract test case.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Maths\Tests
+ * @abstract
+ */
+abstract class AbstractTestCase extends BaseTestCase {
+
+}
