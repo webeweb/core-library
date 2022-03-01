a6d9c86006c7182ce391f8353fceb25c96e6150d
webeweb <support@webeweb.fr>
2022-03-01 18:11:57 +0100

    Add Badge interface

---
+++ tests/symfony/Assets/Badge/BadgeInterfaceTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Assets\Badge;
+
+use WBW\Library\Symfony\Assets\Badge\BadgeInterface;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+
+/**
+ * Badge interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Assets\Badge
+ */
+class BadgeInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Tests __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("danger", BadgeInterface::BADGE_DANGER);
+        $this->assertEquals("info", BadgeInterface::BADGE_INFO);
+        $this->assertEquals("success", BadgeInterface::BADGE_SUCCESS);
+        $this->assertEquals("warning", BadgeInterface::BADGE_WARNING);
+    }
+}
