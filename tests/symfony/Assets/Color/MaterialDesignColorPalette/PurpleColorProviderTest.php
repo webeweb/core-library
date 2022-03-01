82a4599c79d53fcf899e17c984841797531945b6
webeweb <support@webeweb.fr>
2022-03-01 15:57:46 +0100

    Add Purple color provider

---
+++ tests/symfony/Assets/Color/MaterialDesignColorPalette/PurpleColorProviderTest.php
@@ -0,0 +1,100 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Assets\Color\MaterialDesignColorPalette;
+
+use JsonSerializable;
+use WBW\Library\Symfony\Assets\Color\MaterialDesignColorPalette\PurpleColorProvider;
+use WBW\Library\Symfony\Assets\Color\MaterialDesignColorPaletteInterface;
+use WBW\Library\Symfony\Provider\Assets\Color\PurpleColorProviderInterface;
+use WBW\Library\Symfony\Provider\Assets\ColorProviderInterface;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+
+/**
+ * Purple color provider test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Assets\Color\MaterialDesignColorPalette
+ */
+class PurpleColorProviderTest extends AbstractTestCase {
+
+    /**
+     * Tests getColors()
+     *
+     * @return void
+     */
+    public function testGetColors(): void {
+
+        $obj = new PurpleColorProvider();
+
+        $res = $obj->getColors();
+        $this->assertCount(12, $res);
+
+        $this->assertArrayHasKey(MaterialDesignColorPaletteInterface::COLOR_50, $res);
+        $this->assertArrayHasKey(MaterialDesignColorPaletteInterface::COLOR_100, $res);
+        $this->assertArrayHasKey(MaterialDesignColorPaletteInterface::COLOR_200, $res);
+        $this->assertArrayHasKey(MaterialDesignColorPaletteInterface::COLOR_300, $res);
+        $this->assertArrayHasKey(MaterialDesignColorPaletteInterface::COLOR_400, $res);
+        $this->assertArrayHasKey(MaterialDesignColorPaletteInterface::COLOR_500, $res);
+        $this->assertArrayHasKey(MaterialDesignColorPaletteInterface::COLOR_600, $res);
+        $this->assertArrayHasKey(MaterialDesignColorPaletteInterface::COLOR_700, $res);
+        $this->assertArrayHasKey(MaterialDesignColorPaletteInterface::COLOR_A100, $res);
+        $this->assertArrayHasKey(MaterialDesignColorPaletteInterface::COLOR_A200, $res);
+        $this->assertArrayHasKey(MaterialDesignColorPaletteInterface::COLOR_A400, $res);
+        $this->assertArrayHasKey(MaterialDesignColorPaletteInterface::COLOR_A700, $res);
+
+        $this->assertEquals(PurpleColorProviderInterface::PURPLE_COLOR_50, $res[MaterialDesignColorPaletteInterface::COLOR_50]);
+        $this->assertEquals(PurpleColorProviderInterface::PURPLE_COLOR_100, $res[MaterialDesignColorPaletteInterface::COLOR_100]);
+        $this->assertEquals(PurpleColorProviderInterface::PURPLE_COLOR_200, $res[MaterialDesignColorPaletteInterface::COLOR_200]);
+        $this->assertEquals(PurpleColorProviderInterface::PURPLE_COLOR_300, $res[MaterialDesignColorPaletteInterface::COLOR_300]);
+        $this->assertEquals(PurpleColorProviderInterface::PURPLE_COLOR_400, $res[MaterialDesignColorPaletteInterface::COLOR_400]);
+        $this->assertEquals(PurpleColorProviderInterface::PURPLE_COLOR_500, $res[MaterialDesignColorPaletteInterface::COLOR_500]);
+        $this->assertEquals(PurpleColorProviderInterface::PURPLE_COLOR_500, $res[MaterialDesignColorPaletteInterface::COLOR_500]);
+        $this->assertEquals(PurpleColorProviderInterface::PURPLE_COLOR_600, $res[MaterialDesignColorPaletteInterface::COLOR_600]);
+        $this->assertEquals(PurpleColorProviderInterface::PURPLE_COLOR_700, $res[MaterialDesignColorPaletteInterface::COLOR_700]);
+        $this->assertEquals(PurpleColorProviderInterface::PURPLE_COLOR_A100, $res[MaterialDesignColorPaletteInterface::COLOR_A100]);
+        $this->assertEquals(PurpleColorProviderInterface::PURPLE_COLOR_A200, $res[MaterialDesignColorPaletteInterface::COLOR_A200]);
+        $this->assertEquals(PurpleColorProviderInterface::PURPLE_COLOR_A400, $res[MaterialDesignColorPaletteInterface::COLOR_A400]);
+        $this->assertEquals(PurpleColorProviderInterface::PURPLE_COLOR_A700, $res[MaterialDesignColorPaletteInterface::COLOR_A700]);
+    }
+
+    /**
+     * Tests getName()
+     *
+     * @return void
+     */
+    public function testGetName(): void {
+
+        $obj = new PurpleColorProvider();
+
+        $this->assertEquals(PurpleColorProviderInterface::PURPLE_COLOR_NAME, $obj->getName());
+    }
+
+    /**
+     * Tests __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("wbw.core.assets.color.material_design_color_palette.purple", PurpleColorProvider::SERVICE_NAME);
+
+        $obj = new PurpleColorProvider();
+
+        $this->assertInstanceOf(ColorProviderInterface::class, $obj);
+        $this->assertInstanceOf(JsonSerializable::class, $obj);
+
+        $this->assertInstanceOf(MaterialDesignColorPaletteInterface::class, $obj);
+        $this->assertInstanceOf(PurpleColorProviderInterface::class, $obj);
+
+        $this->assertEquals(MaterialDesignColorPaletteInterface::COLOR_DOMAIN, $obj->getDomain());
+    }
+}
