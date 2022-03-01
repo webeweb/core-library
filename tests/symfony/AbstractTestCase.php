5911bfa5364b84debb46b15d8548f075e6e6af0b
webeweb <support@webeweb.fr>
2022-03-01 14:52:42 +0100

    Add Symfony package

---
+++ tests/symfony/AbstractTestCase.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests;
+
+use WBW\Library\Core\Tests\AbstractTestCase as BaseTestCase;
+
+/**
+ * Abstract test case.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests
+ * @abstract
+ */
+abstract class AbstractTestCase extends BaseTestCase {
+
+}
