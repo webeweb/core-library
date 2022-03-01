aff4ae0d87308b77692bfea514c282ec2c730ada
webeweb <support@webeweb.fr>
2022-03-01 18:44:17 +0100

    Add Color provider trait

---
+++ tests/symfony/Provider/Assets/ColorProviderTraitTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Provider\Assets;
+
+use WBW\Library\Symfony\Provider\Assets\ColorProviderInterface;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Tests\Fixtures\Provider\Assets\TestColorProviderTrait;
+
+/**
+ * Color provider trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Provider\Assets
+ */
+class ColorProviderTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests setProvider()
+     *
+     * @return void
+     */
+    public function testSetProvider(): void {
+
+        // Set a Color provider mock.
+        $colorProvider = $this->getMockBuilder(ColorProviderInterface::class)->getMock();
+
+        $obj = new TestColorProviderTrait();
+
+        $obj->setColorProvider($colorProvider);
+        $this->assertSame($colorProvider, $obj->getColorProvider());
+    }
+}
