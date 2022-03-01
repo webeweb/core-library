79eab0cab87570259d90f506918430d5337f0d5b
webeweb <support@webeweb.fr>
2022-03-01 17:38:55 +0100

    Add Divider node

---
+++ src/symfony/Assets/Navigation/DividerNode.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Assets\Navigation;
+
+/**
+ * Divider node.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Assets\Navigation
+ */
+class DividerNode extends AbstractNavigationNode {
+
+    /**
+     * Constructor.
+     *
+     * @param string $label The label.
+     * @param string|null $icon The icon.
+     */
+    public function __construct(string $label, string $icon = null) {
+        parent::__construct($label, $icon, null, null);
+
+        $this->setEnable(true);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function addNode(AbstractNavigationNode $node): AbstractNavigationNode {
+        return $this;
+    }
+}
