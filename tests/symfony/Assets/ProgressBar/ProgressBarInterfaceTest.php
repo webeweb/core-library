115f34248994044dd11d58feff3fb185ef973661
webeweb <support@webeweb.fr>
2022-03-01 18:12:46 +0100

    Add Progress bar interface

---
+++ tests/symfony/Assets/ProgressBar/ProgressBarInterfaceTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Assets\ProgressBar;
+
+use WBW\Library\Symfony\Assets\ProgressBar\ProgressBarInterface;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+
+/**
+ * Progress bar interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Assets\ProgressBar
+ */
+class ProgressBarInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Tests __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("danger", ProgressBarInterface::PROGRESS_BAR_DANGER);
+        $this->assertEquals("info", ProgressBarInterface::PROGRESS_BAR_INFO);
+        $this->assertEquals("success", ProgressBarInterface::PROGRESS_BAR_SUCCESS);
+        $this->assertEquals("warning", ProgressBarInterface::PROGRESS_BAR_WARNING);
+    }
+}
