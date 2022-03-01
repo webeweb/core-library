06b95d5c56b463c6f9afde7d5ab020583bfe2245
webeweb <support@webeweb.fr>
2022-03-01 17:21:32 +0100

    Add Notification factory

---
+++ src/symfony/Assets/Notification/NotificationFactory.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Assets\Notification;
+
+/**
+ * Notification factory.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Assets\Notification
+ */
+class NotificationFactory {
+
+    /**
+     * Creates a danger notification.
+     *
+     * @param string $content The content.
+     * @return NotificationInterface Returns the danger notification.
+     */
+    public static function newDangerNotification(string $content): NotificationInterface {
+        return new DangerNotification($content);
+    }
+
+    /**
+     * Creates a default notification.
+     *
+     * @param string $content The content.
+     * @param string $type The type.
+     * @return NotificationInterface Returns the default notification.
+     */
+    public static function newDefaultNotification(string $content, string $type): NotificationInterface {
+        return new DefaultNotification($type, $content);
+    }
+
+    /**
+     * Creates a info notification.
+     *
+     * @param string $content The content.
+     * @return NotificationInterface Returns the info notification.
+     */
+    public static function newInfoNotification(string $content): NotificationInterface {
+        return new InfoNotification($content);
+    }
+
+    /**
+     * Creates a success notification.
+     *
+     * @param string $content The content.
+     * @return NotificationInterface Returns the success notification.
+     */
+    public static function newSuccessNotification(string $content): NotificationInterface {
+        return new SuccessNotification($content);
+    }
+
+    /**
+     * Creates a warning notification.
+     *
+     * @param string $content The content.
+     * @return NotificationInterface Returns the warning notification.
+     */
+    public static function newWarningNotification(string $content): NotificationInterface {
+        return new WarningNotification($content);
+    }
+}
---
+++ tests/symfony/Assets/Notification/NotificationFactoryTest.php
@@ -0,0 +1,95 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Assets\Notification;
+
+use WBW\Library\Symfony\Assets\Notification\NotificationFactory;
+use WBW\Library\Symfony\Assets\Notification\NotificationInterface;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+
+/**
+ * Notification factory test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Assets\Notification
+ */
+class NotificationFactoryTest extends AbstractTestCase {
+
+    /**
+     * Tests newDangerNotification()
+     *
+     * @return void
+     */
+    public function testNewDangerNotification(): void {
+
+        $obj = NotificationFactory::newDangerNotification("content");
+
+        $this->assertInstanceOf(NotificationInterface::class, $obj);
+        $this->assertEquals("content", $obj->getContent());
+        $this->assertEquals(NotificationInterface::NOTIFICATION_DANGER, $obj->getType());
+    }
+
+    /**
+     * Tests newDefaultNotification()
+     *
+     * @return void
+     */
+    public function testNewDefaultNotification(): void {
+
+        $obj = NotificationFactory::newDefaultNotification("content", "type");
+
+        $this->assertInstanceOf(NotificationInterface::class, $obj);
+        $this->assertEquals("content", $obj->getContent());
+        $this->assertEquals("type", $obj->getType());
+    }
+
+    /**
+     * Tests newInfoNotification()
+     *
+     * @return void
+     */
+    public function testNewInfoNotification(): void {
+
+        $obj = NotificationFactory::newInfoNotification("content");
+
+        $this->assertInstanceOf(NotificationInterface::class, $obj);
+        $this->assertEquals("content", $obj->getContent());
+        $this->assertEquals(NotificationInterface::NOTIFICATION_INFO, $obj->getType());
+    }
+
+    /**
+     * Tests newSuccessNotification()
+     *
+     * @return void
+     */
+    public function testNewSuccessNotification(): void {
+
+        $obj = NotificationFactory::newSuccessNotification("content");
+
+        $this->assertInstanceOf(NotificationInterface::class, $obj);
+        $this->assertEquals("content", $obj->getContent());
+        $this->assertEquals(NotificationInterface::NOTIFICATION_SUCCESS, $obj->getType());
+    }
+
+    /**
+     * Tests newWarningNotification()
+     *
+     * @return void
+     */
+    public function testNewWarningNotification(): void {
+
+        $obj = NotificationFactory::newWarningNotification("content");
+
+        $this->assertInstanceOf(NotificationInterface::class, $obj);
+        $this->assertEquals("content", $obj->getContent());
+        $this->assertEquals(NotificationInterface::NOTIFICATION_WARNING, $obj->getType());
+    }
+}
