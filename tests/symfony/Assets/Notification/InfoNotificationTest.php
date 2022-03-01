11a1cdc81e700bc0aeb4afd38bdcba5a597c89ca
webeweb <support@webeweb.fr>
2022-03-01 17:20:55 +0100

    Add Info notification

---
+++ tests/symfony/Assets/Notification/InfoNotificationTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Assets\Notification;
+
+use WBW\Library\Symfony\Assets\Notification\InfoNotification;
+use WBW\Library\Symfony\Assets\Notification\NotificationInterface;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+
+/**
+ * Info notification test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Assets\Notification
+ */
+class InfoNotificationTest extends AbstractTestCase {
+
+    /**
+     * Tests __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new InfoNotification("info");
+
+        $this->assertEquals(NotificationInterface::NOTIFICATION_INFO, $obj->getType());
+        $this->assertEquals("info", $obj->getContent());
+    }
+}
