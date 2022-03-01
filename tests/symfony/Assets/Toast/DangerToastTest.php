daca6b94384490ca4447df6cd73547ffb0d58dc4
webeweb <support@webeweb.fr>
2022-03-01 17:22:14 +0100

    Add Danger toast

---
+++ tests/symfony/Assets/Toast/DangerToastTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Assets\Toast;
+
+use WBW\Library\Symfony\Assets\Toast\DangerToast;
+use WBW\Library\Symfony\Assets\Toast\ToastInterface;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+
+/**
+ * Danger toast test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Assets\Toast
+ */
+class DangerToastTest extends AbstractTestCase {
+
+    /**
+     * Tests __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new DangerToast("danger");
+
+        $this->assertEquals(ToastInterface::TOAST_DANGER, $obj->getType());
+        $this->assertEquals("danger", $obj->getContent());
+    }
+}
