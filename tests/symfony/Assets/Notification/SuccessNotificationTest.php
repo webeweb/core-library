9e171a399ba9ebbea613f26cd10d1f1e1d50f58e
webeweb <support@webeweb.fr>
2022-03-01 17:21:07 +0100

    Add Success notification

---
+++ tests/symfony/Assets/Notification/SuccessNotificationTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Assets\Notification;
+
+use WBW\Library\Symfony\Assets\Notification\NotificationInterface;
+use WBW\Library\Symfony\Assets\Notification\SuccessNotification;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+
+/**
+ * Success notification test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Assets\Notification
+ */
+class SuccessNotificationTest extends AbstractTestCase {
+
+    /**
+     * Tests __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new SuccessNotification("success");
+
+        $this->assertEquals(NotificationInterface::NOTIFICATION_SUCCESS, $obj->getType());
+        $this->assertEquals("success", $obj->getContent());
+    }
+}
