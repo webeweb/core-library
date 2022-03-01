015e0f74461e14b18631f5b604e7415f4fe7d974
webeweb <support@webeweb.fr>
2022-03-01 14:54:22 +0100

    Add Quote provider interface

---
+++ src/symfony/Provider/Assets/QuoteProviderInterface.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Provider\Assets;
+
+use WBW\Library\Symfony\Assets\Quote\QuoteInterface;
+use WBW\Library\Symfony\Provider\ProviderInterface;
+
+/**
+ * Quote provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Provider\Assets
+ */
+interface QuoteProviderInterface extends ProviderInterface {
+
+    /**
+     * Tag name.
+     *
+     * @var string
+     */
+    const QUOTE_TAG_NAME = "wbw.core.provider.quote";
+
+    /**
+     * Get the authors.
+     *
+     * @return string[] Returns the authors.
+     */
+    public function getAuthors(): array;
+
+    /**
+     * Get the domain.
+     *
+     * @return string Returns the domain.
+     */
+    public function getDomain(): string;
+
+    /**
+     * Get the quotes.
+     *
+     * @return QuoteInterface[] Returns the quotes.
+     */
+    public function getQuotes(): array;
+
+    /**
+     * Initializes.
+     *
+     * @return void
+     */
+    public function init(): void;
+}
---
+++ tests/symfony/Provider/Assets/QuoteProviderInterfaceTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Provider\Assets;
+
+use WBW\Library\Symfony\Provider\Assets\QuoteProviderInterface;
+
+/**
+ * Quote provider interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Provider\Assets
+ */
+class QuoteProviderInterfaceTest {
+
+    /**
+     * Tests __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("wbw.core.provider.quote", QuoteProviderInterface::QUOTE_TAG_NAME);
+    }
+}
