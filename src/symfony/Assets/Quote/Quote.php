356abd2b343452a5a291bccf8ae35daff977e488
webeweb <support@webeweb.fr>
2022-03-01 14:55:50 +0100

    Add Quote

---
+++ src/symfony/Assets/Quote/Quote.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Assets\Quote;
+
+use WBW\Library\Traits\DateTimes\DateTimeDateTrait;
+use WBW\Library\Traits\Strings\StringContentTrait;
+
+/**
+ * Quote.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Assets\Quote
+ */
+class Quote implements QuoteInterface {
+
+    use DateTimeDateTrait;
+    use StringContentTrait;
+
+    /**
+     * Author.
+     *
+     * @var string
+     */
+    private $author;
+
+    /**
+     * Saint.
+     *
+     * @var string|null
+     */
+    private $saint;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAuthor(): ?string {
+        return $this->author;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getSaint(): ?string {
+        return $this->saint;
+    }
+
+    /**
+     * Set the author.
+     *
+     * @param string $author The author.
+     * @return Quote Returns this quote.
+     */
+    public function setAuthor(string $author): QuoteInterface {
+        $this->author = $author;
+        return $this;
+    }
+
+    /**
+     * Set the saint.
+     *
+     * @param string $saint The saint.
+     * @return Quote Returns this quote.
+     */
+    public function setSaint(string $saint): QuoteInterface {
+        $this->saint = $saint;
+        return $this;
+    }
+}
---
+++ tests/symfony/Assets/Quote/QuoteTest.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Assets\Quote;
+
+use WBW\Library\Symfony\Assets\Quote\Quote;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+
+/**
+ * Quote test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Assets\Quote
+ */
+class QuoteTest extends AbstractTestCase {
+
+    /**
+     * Tests setAuthor()
+     *
+     * @return void
+     */
+    public function testSetAuthor(): void {
+
+        $obj = new Quote();
+
+        $obj->setAuthor("author");
+        $this->assertEquals("author", $obj->getAuthor());
+    }
+
+    /**
+     * Tests setSaint()
+     *
+     * @return void
+     */
+    public function testSetSaint(): void {
+
+        $obj = new Quote();
+
+        $obj->setSaint("saint");
+        $this->assertEquals("saint", $obj->getSaint());
+    }
+
+    /**
+     * Tests __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new Quote();
+
+        $this->assertNull($obj->getAuthor());
+        $this->assertNull($obj->getContent());
+        $this->assertNull($obj->getDate());
+        $this->assertNull($obj->getSaint());
+    }
+}
