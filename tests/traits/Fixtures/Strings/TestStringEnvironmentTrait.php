1a3b250375a06daafb4962f987f584e2c2aa595a
webeweb <support@webeweb.fr>
2022-03-02 19:04:19 +0100

    Add string environment trait

---
+++ tests/traits/Fixtures/Strings/TestStringEnvironmentTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Fixtures\Strings;
+
+use WBW\Library\Traits\Strings\StringEnvironmentTrait;
+
+/**
+ * Test string environment trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Traits\Tests\Fixtures\Strings
+ */
+class TestStringEnvironmentTrait {
+
+    use StringEnvironmentTrait;
+}
---
+++ tests/traits/Strings/StringEnvironmentTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Strings;
+
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\Strings\TestStringEnvironmentTrait;
+
+/**
+ * String environment trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Traits\Tests\Strings
+ */
+class StringEnvironmentTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests setEnvironment()
+     *
+     * @return void
+     */
+    public function testSetEnvironment(): void {
+
+        $obj = new TestStringEnvironmentTrait();
+
+        $obj->setEnvironment("environment");
+        $this->assertEquals("environment", $obj->getEnvironment());
+    }
+}
