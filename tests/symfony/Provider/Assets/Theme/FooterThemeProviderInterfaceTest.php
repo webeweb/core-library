c202a64db8a3bd71239b826bba4b8aab220adfe2
webeweb <support@webeweb.fr>
2022-03-02 15:13:01 +0100

    Add Footer theme provider

---
+++ tests/symfony/Provider/Assets/Theme/FooterThemeProviderInterfaceTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Provider\Assets\Theme;
+
+use WBW\Library\Symfony\Provider\Assets\Theme\FooterThemeProviderInterface;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+
+/**
+ * Footer theme provider interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Provider\Assets\Theme
+ */
+class FooterThemeProviderInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Tests __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("wbw.core.provider.assets.theme.footer", FooterThemeProviderInterface::SERVICE_NAME);
+    }
+}
