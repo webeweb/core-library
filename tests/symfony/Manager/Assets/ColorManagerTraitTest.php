fbe20f0b82aa8e84ec677d7ed79e459220f2e078
webeweb <support@webeweb.fr>
2022-03-02 10:43:32 +0100

    Add Color manager trait

---
+++ tests/symfony/Manager/Assets/ColorManagerTraitTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Manager\Assets;
+
+use WBW\Library\Symfony\Manager\Assets\ColorManager;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Tests\Fixtures\Manager\Assets\TestColorManagerTrait;
+
+/**
+ * Color manager trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Manager\Assets
+ */
+class ColorManagerTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests setColorManager()
+     *
+     * @return void
+     */
+    public function testSetColorManager(): void {
+
+        // Set a Color manager mock.
+        $colorManager = $this->getMockBuilder(ColorManager::class)->getMock();
+
+        $obj = new TestColorManagerTrait();
+
+        $obj->setColorManager($colorManager);
+        $this->assertSame($colorManager, $obj->getColorManager());
+    }
+}
