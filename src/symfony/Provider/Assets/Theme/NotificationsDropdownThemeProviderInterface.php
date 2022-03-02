a70ba08def1005bc4a98d32527f7c56d3cc93ec2
webeweb <support@webeweb.fr>
2022-03-02 15:21:35 +0100

    Add Notification dropdown theme provider interface

---
+++ src/symfony/Provider/Assets/Theme/NotificationsDropdownThemeProviderInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Provider\Assets\Theme;
+
+use WBW\Library\Symfony\Provider\Assets\ThemeProviderInterface;
+
+/**
+ * Notifications dropdown theme provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Provider\Assets\Theme
+ */
+interface NotificationsDropdownThemeProviderInterface extends ThemeProviderInterface {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "wbw.core.provider.assets.theme.notifications_dropdown";
+
+    /**
+     * Get the notifications.
+     *
+     * @return array Returns the notifications.
+     */
+    public function getNotifications(): array;
+}
