e1b06d3d131226f3b7b54e4f1590dc9673c5c0ef
webeweb <support@webeweb.fr>
2022-03-02 10:43:21 +0100

    Add Color manager

---
+++ src/symfony/Manager/Assets/ColorManager.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Manager\Assets;
+
+use InvalidArgumentException;
+use WBW\Library\Symfony\Assets\Color\ColorHelper;
+use WBW\Library\Symfony\Exception\AlreadyRegisteredProviderException;
+use WBW\Library\Symfony\Manager\AbstractManager;
+use WBW\Library\Symfony\Manager\ManagerInterface;
+use WBW\Library\Symfony\Provider\Assets\ColorProviderInterface;
+use WBW\Library\Symfony\Provider\ProviderInterface;
+
+/**
+ * Color manager.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Manager\Assets
+ */
+class ColorManager extends AbstractManager {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "wbw.core.manager.assets.color";
+
+    /**
+     * {@inheritDoc}
+     */
+    public function addProvider(ProviderInterface $provider): ManagerInterface {
+
+        if (true === $this->contains($provider)) {
+            throw new AlreadyRegisteredProviderException($provider);
+        }
+
+        return parent::addProvider($provider);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function contains(ProviderInterface $provider): bool {
+
+        if (false === ($provider instanceof ColorProviderInterface)) {
+            throw new InvalidArgumentException("The provider must implements ColorProviderInterface");
+        }
+
+        $identifier = ColorHelper::getIdentifier($provider);
+        foreach ($this->getProviders() as $current) {
+
+            if ($identifier === ColorHelper::getIdentifier($current)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ tests/symfony/Manager/Assets/ColorManagerTest.php
@@ -0,0 +1,134 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Manager\Assets;
+
+use Exception;
+use InvalidArgumentException;
+use WBW\Library\Symfony\Exception\AlreadyRegisteredProviderException;
+use WBW\Library\Symfony\Manager\Assets\ColorManager;
+use WBW\Library\Symfony\Provider\Assets\ColorProviderInterface;
+use WBW\Library\Symfony\Provider\Assets\QuoteProviderInterface;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+
+/**
+ * Color manager test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Manager\Assets
+ */
+class ColorManagerTest extends AbstractTestCase {
+
+    /**
+     * Color provider.
+     *
+     * @var ColorProviderInterface
+     */
+    private $colorProvider;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set a Color provider mock.
+        $this->colorProvider = $this->getMockBuilder(ColorProviderInterface::class)->getMock();
+        $this->colorProvider->expects($this->any())->method("getDomain")->willReturn("domain");
+        $this->colorProvider->expects($this->any())->method("getName")->willReturn("name");
+    }
+
+    /**
+     * Tests addProvider()
+     *
+     * @return void
+     * @throws Exception Throws an exception if an error occurs.
+     */
+    public function testAddProvider(): void {
+
+        $obj = new ColorManager($this->logger);
+
+        $obj->addProvider($this->colorProvider);
+        $this->assertSame($this->colorProvider, $obj->getProviders()[0]);
+    }
+
+    /**
+     * Tests addProvider()
+     *
+     * @return void
+     * @throws Exception Throws an exception if an error occurs.
+     */
+    public function testAddProviderWithAlreadyRegisteredException(): void {
+
+        $obj = new ColorManager($this->logger);
+        $obj->addProvider($this->colorProvider);
+
+        try {
+
+            $obj->addProvider($this->colorProvider);
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(AlreadyRegisteredProviderException::class, $ex);
+        }
+    }
+
+    /**
+     * Tests contains()
+     *
+     * @return void
+     * @throws Exception Throws an exception if an error occurs.
+     */
+    public function testContains(): void {
+
+        $obj = new ColorManager($this->logger);
+
+        $this->assertFalse($obj->contains($this->colorProvider));
+
+        $obj->addProvider($this->colorProvider);
+        $this->assertTrue($obj->contains($this->colorProvider));
+    }
+
+    /**
+     * Tests contains()
+     *
+     * @return void
+     */
+    public function testContainsWithInvalidArgumentException(): void {
+
+        // Set a Quote provider mock.
+        $quoteProvider = $this->getMockBuilder(QuoteProviderInterface::class)->getMock();
+
+        $obj = new ColorManager($this->logger);
+
+        try {
+
+            $obj->contains($quoteProvider);
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(InvalidArgumentException::class, $ex);
+            $this->assertEquals("The provider must implements ColorProviderInterface", $ex->getMessage());
+        }
+    }
+
+    /**
+     * Tests __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("wbw.core.manager.assets.color", ColorManager::SERVICE_NAME);
+
+        $obj = new ColorManager($this->logger);
+
+        $this->assertEquals([], $obj->getProviders());
+    }
+}
