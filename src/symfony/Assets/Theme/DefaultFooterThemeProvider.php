4b2014370a72bde627ace55d23983af6df7be25b
webeweb <support@webeweb.fr>
2022-03-02 18:16:21 +0100

    Add default footer theme provider

---
+++ src/symfony/Assets/Theme/DefaultFooterThemeProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Assets\Theme;
+
+use WBW\Library\Symfony\Provider\Assets\Theme\FooterThemeProviderInterface;
+
+/**
+ * Default footer theme provider.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Assets\Theme
+ */
+class DefaultFooterThemeProvider implements FooterThemeProviderInterface {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+}
---
+++ tests/symfony/Assets/Theme/DefaultFooterThemeProviderTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Assets\Theme;
+
+use WBW\Library\Symfony\Assets\Theme\DefaultFooterThemeProvider;
+use WBW\Library\Symfony\Provider\Assets\Theme\FooterThemeProviderInterface;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+
+/**
+ * Default footer theme provider test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Assets\Theme
+ */
+class DefaultFooterThemeProviderTest extends AbstractTestCase {
+
+    /**
+     * Tests __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new DefaultFooterThemeProvider();
+
+        $this->assertInstanceOf(FooterThemeProviderInterface::class, $obj);
+    }
+}
