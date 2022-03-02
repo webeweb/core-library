978b872ba27e5febd8347c032d3f70c64533947d
webeweb <support@webeweb.fr>
2022-03-02 18:17:25 +0100

    Add default user info theme provider

---
+++ tests/symfony/Assets/Theme/DefaultUserInfoThemeProviderTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Assets\Theme;
+
+use WBW\Library\Symfony\Assets\Theme\DefaultUserInfoThemeProvider;
+use WBW\Library\Symfony\Provider\Assets\Theme\UserInfoThemeProviderInterface;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+
+/**
+ * Default user info theme provider test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Assets\Theme
+ */
+class DefaultUserInfoThemeProviderTest extends AbstractTestCase {
+
+    /**
+     * Tests provideRegisterLink()
+     *
+     * @return void
+     */
+    public function testProvideRegisterLink(): void {
+
+        $obj = new DefaultUserInfoThemeProvider();
+
+        $this->assertFalse($obj->provideRegisterLink());
+    }
+
+    /**
+     * Tests provideResettingLink()
+     *
+     * @return void
+     */
+    public function testProvideResettingLink(): void {
+
+        $obj = new DefaultUserInfoThemeProvider();
+
+        $this->assertFalse($obj->provideResettingLink());
+    }
+
+    /**
+     * Tests __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new DefaultUserInfoThemeProvider();
+
+        $this->assertInstanceOf(UserInfoThemeProviderInterface::class, $obj);
+    }
+}
