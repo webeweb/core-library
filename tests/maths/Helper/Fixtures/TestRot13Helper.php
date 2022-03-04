0a195515748909a03072f5c97a9c3bf38e17c5fb
webeweb <support@webeweb.fr>
2022-03-04 14:43:11 +0100

    Add ROT 13 helper

---
+++ tests/maths/Helper/Fixtures/TestRot13Helper.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Maths\Tests\Helper\Fixtures;
+
+use WBW\Library\Maths\Helper\Rot13Helper;
+
+/**
+ * Test ROT 13 helper.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Maths\Tests\Helper\Fixtures
+ */
+class TestRot13Helper extends Rot13Helper {
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function alphabet(): array {
+        return parent::alphabet();
+    }
+}
---
+++ tests/maths/Helper/Rot13HelperTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Maths\Tests\Helper;
+
+use WBW\Library\Maths\Helper\Rot13Helper;
+use WBW\Library\Maths\Tests\AbstractTestCase;
+use WBW\Library\Maths\Tests\Helper\Fixtures\TestRot13Helper;
+
+/**
+ * ROT 13 helper test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Maths\Tests\Helper
+ */
+class Rot13HelperTest extends AbstractTestCase {
+
+    /**
+     * Tests alphabet()
+     *
+     * @return void
+     */
+    public function testAlphabet(): void {
+
+        $this->assertCount(26, TestRot13Helper::alphabet());
+    }
+
+    /**
+     * Tests decode()
+     *
+     * @return void
+     */
+    public function testDecode(): void {
+
+        $this->assertNull(Rot13Helper::decode(null));
+        $this->assertEquals("", Rot13Helper::decode(""));
+        $this->assertEquals("HELLO WORLD !", Rot13Helper::encode("URYYB JBEYQ !"));
+        $this->assertEquals("Hello world !", Rot13Helper::encode("Uryyb jbeyq !"));
+    }
+
+    /**
+     * Tests encode()
+     *
+     * @return void
+     */
+    public function testEncode(): void {
+
+        $this->assertNull(Rot13Helper::encode(null));
+        $this->assertEquals("", Rot13Helper::encode(""));
+        $this->assertEquals("URYYB JBEYQ !", Rot13Helper::encode("HELLO WORLD !"));
+        $this->assertEquals("Uryyb jbeyq !", Rot13Helper::encode("Hello world !"));
+    }
+}
