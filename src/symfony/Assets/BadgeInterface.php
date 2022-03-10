f2142eb4418e0caaef448e91e26aff4dfe491d12
webeweb <support@webeweb.fr>
2022-03-10 13:58:51 +0100

    Fix Badge interface

--- src/symfony/Assets/BadgeInterface.php
+++ src/symfony/Assets/BadgeInterface.php
@@ -59,9 +59,9 @@ interface BadgeInterface extends JsonSerializable {
     /**
      * Get the type.
      *
-     * @return string Returns the type.
+     * @return string|null Returns the type.
      */
-    public function getType(): string;
+    public function getType(): ?string;
 
     /**
      * Set the content.
