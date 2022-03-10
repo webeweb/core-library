6687ff385e1ad19e1aa27c3467c7d323b4bf7399
webeweb <support@webeweb.fr>
2022-03-10 14:14:33 +0100

    Fix Badge model

--- src/symfony/Assets/AbstractBadge.php
+++ src/symfony/Assets/AbstractBadge.php
@@ -13,6 +13,7 @@ namespace WBW\Library\Symfony\Assets;
 
 use WBW\Library\Symfony\Serializer\JsonSerializer;
 use WBW\Library\Traits\Strings\StringContentTrait;
+use WBW\Library\Traits\Strings\StringTypeTrait;
 
 /**
  * Abstract badge.
@@ -24,21 +25,15 @@ use WBW\Library\Traits\Strings\StringContentTrait;
 abstract class AbstractBadge implements BadgeInterface {
 
     use StringContentTrait;
-
-    /**
-     * Type.
-     *
-     * @var string
-     */
-    private $type;
+    use StringTypeTrait;
 
     /**
      * Constructor.
      *
-     * @param string $type The type.
+     * @param string|null $type The type.
      * @param string|null $content The content.
      */
-    protected function __construct(string $type, ?string $content = null) {
+    protected function __construct(?string $type, ?string $content = null) {
         $this->setContent($content);
         $this->setType($type);
     }
@@ -46,25 +41,7 @@ abstract class AbstractBadge implements BadgeInterface {
     /**
      * {@inheritDoc}
      */
-    public function getType(): string {
-        return $this->type;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     public function jsonSerialize(): array {
         return JsonSerializer::serializeBadge($this);
-    }
-
-    /**
-     * Set the type.
-     *
-     * @param string $type The type.
-     * @return BadgeInterface Returns this badge.
-     */
-    protected function setType(string $type): BadgeInterface {
-        $this->type = $type;
-        return $this;
     }
 }
