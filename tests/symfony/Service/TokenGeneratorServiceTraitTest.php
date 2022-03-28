9d57f79b3d851969ba90e331eed9ddf0591aece0
webeweb <support@webeweb.fr>
2022-03-28 09:19:19 +0200

    Add Token generator service trait

---
+++ tests/symfony/Service/TokenGeneratorServiceTraitTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Service;
+
+use WBW\Library\Symfony\Service\TokenGeneratorService;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Tests\Fixtures\Service\TestTokenGeneratorServiceTrait;
+
+/**
+ * Token generator service trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Service
+ */
+class TokenGeneratorServiceTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests setTokenGeneratorService()
+     *
+     * @return void
+     */
+    public function testSetTokenGeneratorService(): void {
+
+        // Set a Token generator service mock.
+        $tokenGeneratorService = $this->getMockBuilder(TokenGeneratorService::class)->disableOriginalConstructor()->getMock();
+
+        $obj = new TestTokenGeneratorServiceTrait();
+
+        $obj->setTokenGeneratorService($tokenGeneratorService);
+        $this->assertSame($tokenGeneratorService, $obj->getTokenGeneratorService());
+    }
+}
