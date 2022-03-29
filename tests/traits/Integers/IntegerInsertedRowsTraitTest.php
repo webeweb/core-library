003a7b66bf4cbb5b21b91a32a02f3e115ed15941
webeweb <support@webeweb.fr>
2022-03-29 16:08:10 +0200

    Add integer inserted rows trait

---
+++ tests/traits/Integers/IntegerInsertedRowsTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Integers;
+
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\Integers\TestIntegerInsertedRowsTrait;
+
+/**
+ * Integer inserted rows trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Traits\Tests\Integers
+ */
+class IntegerInsertedRowsTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests setInsertedRows()
+     *
+     * @return void
+     */
+    public function testSetInsertedRows(): void {
+
+        $obj = new TestIntegerInsertedRowsTrait();
+
+        $obj->setInsertedRows(1);
+        $this->assertEquals(1, $obj->getInsertedRows());
+    }
+}
