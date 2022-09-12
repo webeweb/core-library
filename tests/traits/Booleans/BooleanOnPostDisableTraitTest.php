f39b5a82ad5e02d30b2c3a279a60945be077d30d
webeweb <support@webeweb.fr>
2022-09-12 09:06:11 +0200

    Add boolean on post disable trait

---
+++ tests/traits/Booleans/BooleanOnPostDisableTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Booleans;
+
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\Booleans\TestBooleanOnPostDisableTrait;
+
+/**
+ * Boolean on post disable trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Traits\Tests\Booleans
+ */
+class BooleanOnPostDisableTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests setOnPostDisable()
+     *
+     * @return void
+     */
+    public function testSetOnPostDisable(): void {
+
+        $obj = new TestBooleanOnPostDisableTrait();
+
+        $obj->setOnPostDisable(true);
+        $this->assertTrue($obj->getOnPostDisable());
+    }
+}
