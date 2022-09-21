700f902f53d36e55b1ac079458790b5fc6323c4e
webeweb <support@webeweb.fr>
2022-09-21 13:45:42 +0200

    Add integer status trait

---
+++ tests/traits/Fixtures/Integers/TestIntegerStatusTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Fixtures\Integers;
+
+use WBW\Library\Traits\Integers\IntegerStatusTrait;
+
+/**
+ * Test integer status trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Traits\Tests\Fixtures\Integers
+ */
+class TestIntegerStatusTrait {
+
+    use IntegerStatusTrait;
+}
