563cdcaa19894b7373e7c3176deb860472a07624
webeweb <support@webeweb.fr>
2022-10-06 19:10:50 +0200

    Add Stylesheet manager

---
+++ src/symfony/Manager/StylesheetManager.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Manager;
+
+use InvalidArgumentException;
+use WBW\Library\Symfony\Exception\AlreadyRegisteredProviderException;
+use WBW\Library\Symfony\Provider\ProviderInterface;
+use WBW\Library\Symfony\Provider\StylesheetProviderInterface;
+
+/**
+ * Stylesheet manager.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Manager
+ */
+class StylesheetManager extends AbstractManager {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "wbw.core.manager.stylesheet";
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addProvider(ProviderInterface $provider): ManagerInterface {
+
+        if (true === $this->contains($provider)) {
+            throw new AlreadyRegisteredProviderException($provider);
+        }
+
+        return parent::addProvider($provider);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function contains(ProviderInterface $provider): bool {
+
+        if (false === ($provider instanceof StylesheetProviderInterface)) {
+            throw new InvalidArgumentException("The provider must implements " . StylesheetProviderInterface::class);
+        }
+
+        return in_array($provider, $this->getProviders());
+    }
+
+    /**
+     * Get the stylesheets.
+     *
+     * @return array Returns the stylesheets.
+     */
+    public function getStylesheets(): array {
+
+        $stylesheets = [];
+
+        /** @var StylesheetProviderInterface $current */
+        foreach ($this->getProviders() as $current) {
+            $stylesheets = array_merge($stylesheets, $current->getStylesheets());
+        }
+
+        return $stylesheets;
+    }
+}
---
+++ tests/symfony/Manager/StylesheetManagerTest.php
@@ -0,0 +1,132 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Manager;
+
+use Exception;
+use InvalidArgumentException;
+use WBW\Library\Symfony\Exception\AlreadyRegisteredProviderException;
+use WBW\Library\Symfony\Manager\StylesheetManager;
+use WBW\Library\Symfony\Provider\ProviderInterface;
+use WBW\Library\Symfony\Provider\StylesheetProviderInterface;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+
+/**
+ * Stylesheet manager test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Manager
+ */
+class StylesheetManagerTest extends AbstractTestCase {
+
+    /**
+     * Stylesheet provider.
+     *
+     * @var StylesheetProviderInterface
+     */
+    private $stylesheetProvider;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set a Stylesheet provider mock.
+        $this->stylesheetProvider = $this->getMockBuilder(StylesheetProviderInterface::class)->getMock();
+    }
+
+    /**
+     * Tests addProvider()
+     *
+     * @return void
+     * @throws Exception Throws an exception if an error occurs.
+     */
+    public function testAddProvider(): void {
+
+        $obj = new StylesheetManager($this->logger);
+
+        $obj->addProvider($this->stylesheetProvider);
+        $this->assertSame($this->stylesheetProvider, $obj->getProviders()[0]);
+    }
+
+    /**
+     * Tests addProvider()
+     *
+     * @return void
+     * @throws Exception Throws an exception if an error occurs.
+     */
+    public function testAddProviderWithAlreadyRegisteredException(): void {
+
+        $obj = new StylesheetManager($this->logger);
+        $obj->addProvider($this->stylesheetProvider);
+
+        try {
+
+            $obj->addProvider($this->stylesheetProvider);
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(AlreadyRegisteredProviderException::class, $ex);
+        }
+    }
+
+    /**
+     * Tests contains()
+     *
+     * @return void
+     * @throws Exception Throws an exception if an error occurs.
+     */
+    public function testContains(): void {
+
+        $obj = new StylesheetManager($this->logger);
+
+        $this->assertFalse($obj->contains($this->stylesheetProvider));
+
+        $obj->addProvider($this->stylesheetProvider);
+        $this->assertTrue($obj->contains($this->stylesheetProvider));
+    }
+
+    /**
+     * Tests contains()
+     *
+     * @return void
+     */
+    public function testContainsWithInvalidArgumentException(): void {
+
+        // Set a Provider mock.
+        $provider = $this->getMockBuilder(ProviderInterface::class)->getMock();
+
+        $obj = new StylesheetManager($this->logger);
+
+        try {
+
+            $obj->contains($provider);
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(InvalidArgumentException::class, $ex);
+            $this->assertEquals("The provider must implements " . StylesheetProviderInterface::class, $ex->getMessage());
+        }
+    }
+
+    /**
+     * Tests __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("wbw.core.manager.stylesheet", StylesheetManager::SERVICE_NAME);
+
+        $obj = new StylesheetManager($this->logger);
+
+        $this->assertEquals([], $obj->getProviders());
+    }
+}
