cf3ce46e6629a1c4b9385761c661594ef6c3ddcb
webeweb <support@webeweb.fr>
2022-10-06 19:03:46 +0200

    Add Javascript provider interface

---
+++ src/symfony/Provider/JavascriptProviderInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Provider;
+
+/**
+ * Javascript provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Provider
+ */
+interface JavascriptProviderInterface extends ProviderInterface {
+
+    /**
+     * Tag name.
+     *
+     * @var string
+     */
+    const JAVASCRIPT_PROVIDER_TAG_NAME = "wbw.core.provider.javascript";
+
+    /**
+     * Get the javascripts.
+     *
+     * @return array Returns the javascripts.
+     */
+    public function getJavascripts(): array;
+}
---
+++ tests/symfony/Provider/JavascriptProviderInterfaceTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Provider;
+
+use WBW\Library\Symfony\Provider\JavascriptProviderInterface;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+
+/**
+ * Javascript provider interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Provider
+ */
+class JavascriptProviderInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Tests __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("wbw.core.provider.javascript", JavascriptProviderInterface::JAVASCRIPT_PROVIDER_TAG_NAME);
+    }
+}
