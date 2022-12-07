3cd3b7c70603cdb40e8a9cba5b55be0a6c0f67cc
webeweb <support@webeweb.fr>
2022-12-07 10:16:00 +0100

    Add Network trait

---
+++ tests/system/Model/NetworkTraitTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\System\Tests\Model;
+
+use WBW\Library\System\Model\NetworkInterface;
+use WBW\Library\System\Tests\AbstractTestCase;
+use WBW\Library\System\Tests\Fixtures\Model\TestNetworkTrait;
+
+/**
+ * Network trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\System\Tests\Model
+ */
+class NetworkTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests setNetwork()
+     *
+     * @return void
+     */
+    public function testSetNetwork(): void {
+
+        // Set a Network mock.
+        $network = $this->getMockBuilder(NetworkInterface::class)->getMock();
+
+        $obj = new TestNetworkTrait();
+        $obj->setNetwork($network);
+
+        $this->assertSame($network, $obj->getNetwork());
+    }
+}
