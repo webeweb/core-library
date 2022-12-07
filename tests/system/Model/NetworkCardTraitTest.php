cceafd4ccd44c1596cfece1eee3ecc08de811e01
webeweb <support@webeweb.fr>
2022-12-07 10:15:52 +0100

    Add Network card trait

---
+++ tests/system/Model/NetworkCardTraitTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\System\Tests\Model;
+
+use WBW\Library\System\Model\NetworkCardInterface;
+use WBW\Library\System\Tests\AbstractTestCase;
+use WBW\Library\System\Tests\Fixtures\Model\TestNetworkCardTrait;
+
+/**
+ * Network card trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\System\Tests\Model
+ */
+class NetworkCardTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests setNetworkCard()
+     *
+     * @return void
+     */
+    public function testSetNetworkCard(): void {
+
+        // Set a Network card mock.
+        $networkCard = $this->getMockBuilder(NetworkCardInterface::class)->getMock();
+
+        $obj = new TestNetworkCardTrait();
+        $obj->setNetworkCard($networkCard);
+
+        $this->assertSame($networkCard, $obj->getNetworkCard());
+    }
+}
