14e74ead761707a874c9178602288bc44ed427ee
webeweb <support@webeweb.fr>
2022-12-07 10:16:23 +0100

    Add Processor trait

---
+++ tests/system/Fixtures/Model/TestProcessorTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\System\Tests\Fixtures\Model;
+
+use WBW\Library\System\Model\ProcessorTrait;
+
+/**
+ * Test processor tait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\System\Tests\Fixtures\Model
+ */
+class TestProcessorTrait {
+
+    use ProcessorTrait {
+        setProcessor as public;
+    }
+}
---
+++ tests/system/Model/ProcessorTraitTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\System\Tests\Model;
+
+use WBW\Library\System\Model\ProcessorInterface;
+use WBW\Library\System\Tests\AbstractTestCase;
+use WBW\Library\System\Tests\Fixtures\Model\TestProcessorTrait;
+
+/**
+ * Processor trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\System\Tests\Model
+ */
+class ProcessorTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests setProcessor()
+     *
+     * @return void
+     */
+    public function testSetProcessor(): void {
+
+        // Set a Processor mock.
+        $processor = $this->getMockBuilder(ProcessorInterface::class)->getMock();
+
+        $obj = new TestProcessorTrait();
+        $obj->setProcessor($processor);
+
+        $this->assertSame($processor, $obj->getProcessor());
+    }
+}
