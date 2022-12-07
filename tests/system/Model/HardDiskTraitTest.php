b4a80a4f66eacc81d6b0f8e6f78f13b8b73cce93
webeweb <support@webeweb.fr>
2022-12-07 10:15:28 +0100

    Add Hard disk trait

---
+++ tests/system/Model/HardDiskTraitTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\System\Tests\Model;
+
+use WBW\Library\System\Model\HardDiskInterface;
+use WBW\Library\System\Tests\AbstractTestCase;
+use WBW\Library\System\Tests\Fixtures\Model\TestHardDiskTrait;
+
+/**
+ * Hard disk trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\System\Tests\Model
+ */
+class HardDiskTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests setHardDisk()
+     *
+     * @return void
+     */
+    public function testSetHardDisk(): void {
+
+        // Set a Hard disk mock.
+        $hardDisk = $this->getMockBuilder(HardDiskInterface::class)->getMock();
+
+        $obj = new TestHardDiskTrait();
+        $obj->setHardDisk($hardDisk);
+
+        $this->assertSame($hardDisk, $obj->getHardDisk());
+    }
+}
