7efa99c2148af6ab1926dda7820b74738c7ff56d
webeweb <support@webeweb.fr>
2023-01-23 17:26:51 +0100

    Add Repository report trait

---
+++ tests/symfony/Model/RepositoryReportTraitTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Model;
+
+use WBW\Library\Symfony\Model\RepositoryReportInterface;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Tests\Fixtures\Model\TestRepositoryReportTrait;
+
+/**
+ * Repository report trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Model
+ */
+class RepositoryReportTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests setRepositoryReport()
+     *
+     * @return void
+     */
+    public function testSetRepositoryReport(): void {
+
+        // Set a Repository report mock.
+        $repositoryReport = $this->getMockBuilder(RepositoryReportInterface::class)->getMock();
+
+        $obj = new TestRepositoryReportTrait();
+
+        $obj->setRepositoryReport($repositoryReport);
+        $this->assertSame($repositoryReport, $obj->getRepositoryReport());
+    }
+}
