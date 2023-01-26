c07fa8b0379b520d8dd765e4dbea5148a290aa88
webeweb <support@webeweb.fr>
2023-01-26 11:31:57 +0100

    Add Request detail trait

---
+++ tests/symfony/Fixtures/Model/TestRequestDetailTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Fixtures\Model;
+
+use WBW\Library\Symfony\Model\RequestDetailTrait;
+
+/**
+ * Test request detail trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Fixtures\Model
+ */
+class TestRequestDetailTrait {
+
+    use RequestDetailTrait {
+        setRequestDetail as public;
+    }
+}
---
+++ tests/symfony/Model/RequestDetailTraitTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Symfony\Tests\Model;
+
+use WBW\Library\Symfony\Model\RequestDetailInterface;
+use WBW\Library\Symfony\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Tests\Fixtures\Model\TestRequestDetailTrait;
+
+/**
+ * Request detail trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Tests\Model
+ */
+class RequestDetailTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests setRequestDetail()
+     *
+     * @return void
+     */
+    public function testSetRequestDetail(): void {
+
+        // Set a Request detail mock.
+        $requestDetail = $this->getMockBuilder(RequestDetailInterface::class)->getMock();
+
+        $obj = new TestRequestDetailTrait();
+
+        $obj->setRequestDetail($requestDetail);
+        $this->assertSame($requestDetail, $obj->getRequestDetail());
+    }
+}
