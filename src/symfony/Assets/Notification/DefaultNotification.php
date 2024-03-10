7df43575c80bf66e0b902de84c137399ff52fe41
webeweb <support@webeweb.fr>
2024-03-10 16:28:44 +0100

    Update Default notification: - fix PHPStan warnings

--- src/symfony/Assets/Notification/DefaultNotification.php
+++ src/symfony/Assets/Notification/DefaultNotification.php
@@ -24,8 +24,8 @@ class DefaultNotification extends AbstractNotification {
     /**
      * Constructor.
      *
-     * @param string|null $type The type.
-     * @param string|null $content The content.
+     * @param string $type The type.
+     * @param string $content The content.
      */
     public function __construct(string $type, string $content) {
         parent::__construct($type, $content);
