e324bacf0a2cb333b8c5f859be37c6098303f41a
webeweb <support@webeweb.fr>
2024-03-10 16:37:29 +0100

    Update abstract status: - fix PHPStan warnings

--- src/validator/Status/AbstractStatus.php
+++ src/validator/Status/AbstractStatus.php
@@ -77,6 +77,7 @@ abstract class AbstractStatus implements StatusInterface {
 
     /**
      * {@inheritDoc}
+     * @return array<string,mixed> Returns this serialized instance.
      */
     public function jsonSerialize(): array {
 
