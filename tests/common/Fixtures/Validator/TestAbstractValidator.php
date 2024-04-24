efc828f1a163522c53943ce6e39c265248608141
webeweb <support@webeweb.fr>
2024-04-24 12:05:42 +0200

    Add abstract validator test

---
+++ tests/common/Fixtures/Validator/TestAbstractValidator.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Common\Tests\Fixtures\Validator;
+
+use WBW\Library\Common\Validator\AbstractValidator;
+use WBW\Library\Common\Validator\RuleSetInterface;
+
+/**
+ * Test abstract validator.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Common\Tests\Fixtures\Validator
+ */
+class TestAbstractValidator extends AbstractValidator {
+
+    /**
+     * Constructor.
+     *
+     * @param RuleSetInterface $ruleSet The rule set.
+     */
+    public function __construct(RuleSetInterface $ruleSet) {
+        parent::__construct($ruleSet);
+    }
+}
