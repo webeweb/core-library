2142099b6a90c0a6c25ccf97679210a7b97b2af2
webeweb <support@webeweb.fr>
2024-04-24 12:21:38 +0200

    Move Paginate helper

---
+++ src/common/Database/Paginator.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Common\Database;
+
+/**
+ * Paginator.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Database
+ */
+class Paginator {
+
+    /**
+     * Count the pages.
+     *
+     * @param int $linesCount The lines count.
+     * @param int $divider The divider.
+     * @return int Returns the pages count in case of success, -1 otherwise.
+     */
+    public static function countPages(int $linesCount, int $divider): int {
+
+        if ($linesCount < 0 || $divider < 0) {
+            return -1;
+        }
+
+        $pagesCount = intval($linesCount / $divider);
+        if (0 < ($linesCount % $divider)) {
+            ++$pagesCount;
+        }
+
+        return $pagesCount;
+    }
+
+    /**
+     * Offset and limit.
+     *
+     * @param int $pagesCount The pages count.
+     * @param int $divider The divider.
+     * @param int $total The total.
+     * @return int[] Returns the offset and limit in case of success, -1 otherwise.
+     */
+    public static function offsetLimit(int $pagesCount, int $divider, int $total = -1): array {
+
+        if ($pagesCount < 0 || $divider < 0) {
+            return [-1, -1];
+        }
+
+        $offset = $pagesCount * $divider;
+        $limit  = $divider;
+
+        if (0 <= $total && ($total < $offset || $total < ($offset + $limit))) {
+            $offset = (static::countPages($total, $divider) - 1) * $divider;
+            $limit  = $total - $offset;
+        }
+
+        return [$offset, $limit];
+    }
+}
---
+++ tests/common/Database/PaginatorTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Common\Tests\Database;
+
+use WBW\Library\Common\Database\Paginator;
+use WBW\Library\Common\Tests\AbstractTestCase;
+
+/**
+ * Paginator test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Common\Tests\Database
+ */
+class PaginatorTest extends AbstractTestCase {
+
+    /**
+     * Test countPages()
+     *
+     * @return void
+     */
+    public function testCountPages(): void {
+
+        $this->assertEquals(-1, Paginator::countPages(-1, 300));
+        $this->assertEquals(-1, Paginator::countPages(0, -1));
+        $this->assertEquals(-1, Paginator::countPages(-1, -1));
+
+        $this->assertEquals(0, Paginator::countPages(0, 300));
+        $this->assertEquals(1, Paginator::countPages(1, 300));
+        $this->assertEquals(1, Paginator::countPages(300, 300));
+        $this->assertEquals(2, Paginator::countPages(301, 300));
+    }
+
+    /**
+     * Test offsetLimit()
+     *
+     * @return void
+     */
+    public function testOffsetAndLimit(): void {
+
+        $this->assertEquals([-1, -1], Paginator::offsetLimit(-1, 300));
+        $this->assertEquals([-1, -1], Paginator::offsetLimit(0, -1));
+        $this->assertEquals([-1, -1], Paginator::offsetLimit(-1, -1));
+
+        $this->assertEquals([0, 300], Paginator::offsetLimit(0, 300));
+        $this->assertEquals([300, 300], Paginator::offsetLimit(1, 300));
+        $this->assertEquals([90000, 300], Paginator::offsetLimit(300, 300));
+        $this->assertEquals([90300, 300], Paginator::offsetLimit(301, 300));
+
+        $this->assertEquals([0, 255], Paginator::offsetLimit(0, 300, 255));
+        $this->assertEquals([300, 255], Paginator::offsetLimit(1, 300, 555));
+        $this->assertEquals([90000, 255], Paginator::offsetLimit(300, 300, 90255));
+        $this->assertEquals([90300, 255], Paginator::offsetLimit(301, 300, 90555));
+    }
+}
