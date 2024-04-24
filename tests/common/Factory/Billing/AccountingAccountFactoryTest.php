64aad67c8933945082828419975a865b40ba64ac
webeweb <support@webeweb.fr>
2024-04-24 18:29:30 +0200

    Move Accounting account factory

--- tests/common/Factory/Billing/AccountingAccountFactoryTest.php
+++ tests/common/Factory/Billing/AccountingAccountFactoryTest.php
@@ -11,17 +11,17 @@ declare(strict_types = 1);
  * file that was distributed with this source code.
  */
 
-namespace WBW\Library\Accounting\Tests\Factory;
+namespace WBW\Library\Common\Tests\Factory\Billing;
 
-use WBW\Library\Accounting\Factory\AccountingAccountFactory;
-use WBW\Library\Accounting\Tests\AbstractTestCase;
+use WBW\Library\Common\Tests\AbstractTestCase;
+use WBW\Library\Common\Factory\Billing\AccountingAccountFactory;
 use WBW\Library\Common\Model\Billing\AccountingAccount;
 
 /**
  * Accounting account factory test.
  *
  * @author webeweb <https://github.com/webeweb>
- * @package WBW\Library\Accounting\Tests\Factory
+ * @package WBW\Library\Common\Tests\Factory\Billing
  */
 class AccountingAccountFactoryTest extends AbstractTestCase {
 
