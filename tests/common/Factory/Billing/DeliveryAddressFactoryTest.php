7e148a6ca9a98c756d35524ee06ba8c0062414a4
webeweb <support@webeweb.fr>
2024-04-24 19:41:39 +0200

    Move Delivery address factory

--- tests/common/Factory/Billing/DeliveryAddressFactoryTest.php
+++ tests/common/Factory/Billing/DeliveryAddressFactoryTest.php
@@ -11,17 +11,17 @@ declare(strict_types = 1);
  * file that was distributed with this source code.
  */
 
-namespace WBW\Library\Billing\Tests\Factory;
+namespace WBW\Library\Common\Tests\Factory\Billing;
 
-use WBW\Library\Billing\Factory\DeliveryAddressFactory;
-use WBW\Library\Billing\Tests\AbstractTestCase;
+use WBW\Library\Common\Factory\Billing\DeliveryAddressFactory;
+use WBW\Library\Common\Tests\AbstractTestCase;
 use WBW\Library\Common\Tests\Fixtures\Model\Billing\TestDeliveryAddressTrait;
 
 /**
  * Delivery address factory test.
  *
  * @author webeweb <https://github.com/webeweb>
- * @package WBW\Library\Billing\Tests\Factory
+ * @package WBW\Library\Common\Tests\Factory\Billing
  */
 class DeliveryAddressFactoryTest extends AbstractTestCase {
 
