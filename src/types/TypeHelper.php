6fd08f8c2f4d051c64e5be0a0caca431180b1153
webeweb <support@webeweb.fr>
2024-04-25 08:50:07 +0200

    Move Timestamp helper

--- src/types/TypeHelper.php
+++ src/types/TypeHelper.php
@@ -17,6 +17,7 @@ use DateTime;
 use InvalidArgumentException;
 use WBW\Library\Common\Exception\DateArgumentException;
 use WBW\Library\Common\Exception\TimestampArgumentException;
+use WBW\Library\Common\Helper\TimestampHelper;
 use WBW\Library\Types\Helper\ArrayHelper;
 use WBW\Library\Types\Helper\BooleanHelper;
 use WBW\Library\Types\Helper\DateTimeHelper;
@@ -27,7 +28,6 @@ use WBW\Library\Types\Helper\NumberHelper;
 use WBW\Library\Types\Helper\ObjectHelper;
 use WBW\Library\Types\Helper\ResourceHelper;
 use WBW\Library\Types\Helper\StringHelper;
-use WBW\Library\Types\Helper\TimestampHelper;
 
 /**
  * Type helper.
