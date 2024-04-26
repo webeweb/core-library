31bc80cd3e44e73a40ff8fd46c428e0e957072bb
webeweb <support@webeweb.fr>
2024-04-26 11:43:53 +0200

    Add JSON serializer

---
+++ src/Serializer/Response/JsonSerializer.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Common\Serializer\Response;
+
+use WBW\Library\Common\Serializer\SerializerKeys;
+use WBW\Library\Symfony\Response\DefaultJsonResponseDataInterface;
+use WBW\Library\Symfony\Response\SimpleJsonResponseDataInterface;
+
+/**
+ * JSON serializer.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Common\Serializer\Response
+ */
+class JsonSerializer {
+
+    /**
+     * Serialize a default JSON response data.
+     *
+     * @param DefaultJsonResponseDataInterface $model The model.
+     * @return array<string,mixed> Returns the serialized model.
+     */
+    public static function serializeDefaultJsonResponseData(DefaultJsonResponseDataInterface $model): array {
+
+        return [
+            SerializerKeys::SUCCESS => $model->getSuccess(),
+            SerializerKeys::DATA    => $model->getData(),
+            SerializerKeys::MESSAGE => $model->getMessage(),
+            SerializerKeys::ERRORS  => $model->getErrors(),
+        ];
+    }
+
+    /**
+     * Serialize a simple JSON response data.
+     *
+     * @param SimpleJsonResponseDataInterface $model The model.
+     * @return array<string,mixed> Returns the serialized model.
+     */
+    public static function serializeSimpleJsonResponseData(SimpleJsonResponseDataInterface $model): array {
+
+        return [
+            SerializerKeys::NOTIFY => $model->getNotify(),
+            SerializerKeys::STATUS => $model->getStatus(),
+        ];
+    }
+}
---
+++ tests/Serializer/Response/JsonSerializerTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Common\Tests\Serializer\Response;
+
+use WBW\Library\Common\Serializer\Response\JsonSerializer;
+use WBW\Library\Common\Serializer\SerializerKeys;
+use WBW\Library\Common\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Response\DefaultJsonResponseDataInterface;
+use WBW\Library\Symfony\Response\SimpleJsonResponseDataInterface;
+
+/**
+ * JSON serializer test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Common\Tests\Serializer\Response
+ */
+class JsonSerializerTest extends AbstractTestCase {
+
+    /**
+     * Test serializeDefaultJsonResponseData()
+     *
+     * @return void
+     */
+    public function testSerializeDefaultJsonResponseData(): void {
+
+        // Set a Default JSON response data mock.
+        $model = $this->getMockBuilder(DefaultJsonResponseDataInterface::class)->getMock();
+        $model->expects($this->any())->method("getSuccess")->willReturn(true);
+        $model->expects($this->any())->method("getData")->willReturn([]);
+        $model->expects($this->any())->method("getMessage")->willReturn(SerializerKeys::MESSAGE);
+        $model->expects($this->any())->method("getErrors")->willReturn([]);
+
+        $res = JsonSerializer::serializeDefaultJsonResponseData($model);
+        $this->assertCount(4, $res);
+
+        $this->assertEquals($model->getSuccess(), $res[SerializerKeys::SUCCESS]);
+        $this->assertEquals($model->getData(), $res[SerializerKeys::DATA]);
+        $this->assertEquals($model->getMessage(), $res[SerializerKeys::MESSAGE]);
+        $this->assertEquals($model->getErrors(), $res[SerializerKeys::ERRORS]);
+    }
+
+    /**
+     * Test serializeSimpleJsonResponseData()
+     *
+     * @return void
+     */
+    public function testSerializeSimpleJsonResponseData(): void {
+
+        // Set a Simple JSON response data mock.
+        $model = $this->getMockBuilder(SimpleJsonResponseDataInterface::class)->getMock();
+        $model->expects($this->any())->method("getNotify")->willReturn(SerializerKeys::NOTIFY);
+        $model->expects($this->any())->method("getStatus")->willReturn(1);
+
+        $res = JsonSerializer::serializeSimpleJsonResponseData($model);
+        $this->assertCount(2, $res);
+
+        $this->assertEquals($model->getNotify(), $res[SerializerKeys::NOTIFY]);
+        $this->assertEquals($model->getStatus(), $res[SerializerKeys::STATUS]);
+    }
+}
