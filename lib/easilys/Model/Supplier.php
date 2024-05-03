a4583bab3e0c3251b144cbe297b526cf4e3018be
webeweb <support@webeweb.fr>
2024-05-03 10:59:21 +0200

    Add Supplier model

---
+++ lib/easilys/Model/Supplier.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Model;
+
+use WBW\Library\Easilys\Traits\Booleans\BooleanIsActiveTrait;
+use WBW\Library\Easilys\Traits\Strings\StringCodeInternalTrait;
+use WBW\Library\Common\Traits\Integers\IntegerIdTrait;
+use WBW\Library\Common\Traits\Strings\StringLabelTrait;
+
+/**
+ * Supplier.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Model
+ */
+class Supplier {
+
+    use BooleanIsActiveTrait;
+    use IntegerIdTrait {
+        setId as public;
+    }
+    use StringCodeInternalTrait;
+    use StringLabelTrait;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+}
