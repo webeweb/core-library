14560b0011a90c514f98f26b4f64fe4ecbb08902
webeweb <support@webeweb.fr>
2024-05-03 10:42:30 +0200

    Add integer id site trait

---
+++ lib/easilys/Tests/Traits/Integers/IntegerIdSiteTraitTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Traits\Integers;
+
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+use WBW\Library\Easilys\Tests\Fixtures\Traits\Integers\TestIntegerIdSiteTrait;
+
+/**
+ * String id site trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Traits\Strings
+ */
+class IntegerIdSiteTraitTest extends AbstractTestCase {
+
+    /**
+     * Test setIdSite()
+     *
+     * @return void
+     */
+    public function testSetIdSite(): void {
+
+        $obj = new TestIntegerIdSiteTrait();
+
+        $obj->setIdSite(1);
+        $this->assertEquals(1, $obj->getIdSite());
+    }
+}
---
+++ lib/easilys/Traits/Integers/IntegerIdSiteTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Traits\Integers;
+
+/**
+ * Integer id site trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Traits\Strings
+ */
+trait IntegerIdSiteTrait {
+
+    /**
+     * Id site.
+     *
+     * @var int|null
+     */
+    protected $idSite;
+
+    /**
+     * Get the id site.
+     *
+     * @return int|null Returns the id site.
+     */
+    public function getIdSite(): ?int {
+        return $this->idSite;
+    }
+
+    /**
+     * Set the id site.
+     *
+     * @param int|null $idSite The id site.
+     * @return self Returns this instance.
+     */
+    public function setIdSite(?int $idSite): self {
+        $this->idSite = $idSite;
+        return $this;
+    }
+}
