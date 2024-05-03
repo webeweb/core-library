3529531e9bdad421fe16ba0f32fdd13d9a780fd1
webeweb <support@webeweb.fr>
2024-05-03 10:40:09 +0200

    Add boolean is validated trait

---
+++ lib/easilys/Tests/Traits/Booleans/BooleanIsValidatedTraitTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Traits\Booleans;
+
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+use WBW\Library\Easilys\Tests\Fixtures\Traits\Booleans\TestBooleanIsValidatedTrait;
+
+/**
+ * Boolean is validated trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Traits\Booleans
+ */
+class BooleanIsValidatedTraitTest extends AbstractTestCase {
+
+    /**
+     * Test setIsValidated()
+     *
+     * @return void
+     */
+    public function testSetIsValidated(): void {
+
+        $obj = new TestBooleanIsValidatedTrait();
+
+        $obj->setIsValidated(true);
+        $this->assertTrue($obj->getIsValidated());
+    }
+}
---
+++ lib/easilys/Traits/Booleans/BooleanIsValidatedTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Traits\Booleans;
+
+/**
+ * Boolean is validated trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Traits\Booleans
+ */
+trait BooleanIsValidatedTrait {
+
+    /**
+     * Is validated.
+     *
+     * @var bool|null
+     */
+    protected $isValidated;
+
+    /**
+     * Get the is validated.
+     *
+     * @return bool|null Returns the is validated.
+     */
+    public function getIsValidated(): ?bool {
+        return $this->isValidated;
+    }
+
+    /**
+     * Set the is validated.
+     *
+     * @param bool|null $isValidated The is validated.
+     * @return self Returns this instance.
+     */
+    public function setIsValidated(?bool $isValidated): self {
+        $this->isValidated = $isValidated;
+        return $this;
+    }
+}
