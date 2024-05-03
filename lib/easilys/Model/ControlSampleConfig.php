603778950953fbe35181ab668b05ad2a0bbb40fa
webeweb <support@webeweb.fr>
2024-05-03 10:51:42 +0200

    Add Control sample model

---
+++ lib/easilys/Model/ControlSampleConfig.php
@@ -0,0 +1,138 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Model;
+
+/**
+ * Control sample config.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Model
+ */
+class ControlSampleConfig {
+
+    /**
+     * Amount for consumption site.
+     *
+     * @var string|null
+     */
+    protected $amountForConsumptionSite;
+
+    /**
+     * Amount for production site.
+     *
+     * @var string|null
+     */
+    protected $amountForProductionSite;
+
+    /**
+     * Is control sample active.
+     *
+     * @var bool|null
+     */
+    protected $isControlSampleActive;
+
+    /**
+     * Is control sample billable.
+     *
+     * @var bool|null
+     */
+    protected $isControlSampleBillable;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * Get the amount for consumption site.
+     *
+     * @return string|null Returns the amount for consumption site.
+     */
+    public function getAmountForConsumptionSite(): ?string {
+        return $this->amountForConsumptionSite;
+    }
+
+    /**
+     * Get the amount for production site.
+     *
+     * @return string|null Returns the amount for production site.
+     */
+    public function getAmountForProductionSite(): ?string {
+        return $this->amountForProductionSite;
+    }
+
+    /**
+     * Get the is control sample active.
+     *
+     * @return bool|null Returns the is control sample active.
+     */
+    public function getIsControlSampleActive(): ?bool {
+        return $this->isControlSampleActive;
+    }
+
+    /**
+     * Get the is control sample billable.
+     *
+     * @return bool|null Returns the is control sample billable.
+     */
+    public function getIsControlSampleBillable(): ?bool {
+        return $this->isControlSampleBillable;
+    }
+
+    /**
+     * Set the amount for consumption site.
+     *
+     * @param string|null $amountForConsumptionSite The amount for consumption site.
+     * @return ControlSampleConfig Returns this control sample config.
+     */
+    public function setAmountForConsumptionSite(?string $amountForConsumptionSite): ControlSampleConfig {
+        $this->amountForConsumptionSite = $amountForConsumptionSite;
+        return $this;
+    }
+
+    /**
+     * Set the amount for production site.
+     *
+     * @param string|null $amountForProductionSite The amount for production site.
+     * @return ControlSampleConfig Returns this control sample config.
+     */
+    public function setAmountForProductionSite(?string $amountForProductionSite): ControlSampleConfig {
+        $this->amountForProductionSite = $amountForProductionSite;
+        return $this;
+    }
+
+    /**
+     * Set the cis control sample active.
+     *
+     * @param bool|null $isControlSampleActive The is control sample active.
+     * @return ControlSampleConfig
+     */
+    public function setIsControlSampleActive(?bool $isControlSampleActive): ControlSampleConfig {
+        $this->isControlSampleActive = $isControlSampleActive;
+        return $this;
+    }
+
+    /**
+     * Set the is control sample billable.
+     *
+     * @param bool|null $isControlSampleBillable The is control sample billable.
+     * @return ControlSampleConfig Returns this control sample config.
+     */
+    public function setIsControlSampleBillable(?bool $isControlSampleBillable): ControlSampleConfig {
+        $this->isControlSampleBillable = $isControlSampleBillable;
+        return $this;
+    }
+}
---
+++ lib/easilys/Tests/Model/ControlSampleConfigTest.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Model;
+
+use WBW\Library\Easilys\Model\ControlSampleConfig;
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+
+/**
+ * Control sample config test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Model
+ */
+class ControlSampleConfigTest extends AbstractTestCase {
+
+    /**
+     * Test setAmountForConsumptionSite()
+     *
+     * @return void
+     */
+    public function testSetAmountForConsumptionSite(): void {
+
+        $obj = new ControlSampleConfig();
+
+        $obj->setAmountForConsumptionSite("amountForConsumptionSite");
+        $this->assertEquals("amountForConsumptionSite", $obj->getAmountForConsumptionSite());
+    }
+
+    /**
+     * Test setAmountForProductionSite()
+     *
+     * @return void
+     */
+    public function testSetAmountForProductionSite(): void {
+
+        $obj = new ControlSampleConfig();
+
+        $obj->setAmountForProductionSite("amountForProductionSite");
+        $this->assertEquals("amountForProductionSite", $obj->getAmountForProductionSite());
+    }
+
+    /**
+     * Test setIsControlSampleActive()
+     *
+     * @return void
+     */
+    public function testSetIsControlSampleActive(): void {
+
+        $obj = new ControlSampleConfig();
+
+        $obj->setIsControlSampleActive(true);
+        $this->assertTrue($obj->getIsControlSampleActive());
+    }
+
+    /**
+     * Test setIsControlSampleBillable()
+     *
+     * @return void
+     */
+    public function testSetIsControlSampleBillable(): void {
+
+        $obj = new ControlSampleConfig();
+
+        $obj->setIsControlSampleBillable(true);
+        $this->assertTrue($obj->getIsControlSampleBillable());
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new ControlSampleConfig();
+
+        $this->assertNull($obj->getAmountForConsumptionSite());
+        $this->assertNull($obj->getAmountForProductionSite());
+        $this->assertNull($obj->getIsControlSampleActive());
+        $this->assertNull($obj->getIsControlSampleBillable());
+    }
+}
