df98771fcebb61be04518343df794911d5665c26
webeweb <support@webeweb.fr>
2024-05-03 10:53:40 +0200

    Add Generic model

---
+++ lib/easilys/Model/Generic.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Model;
+
+use WBW\Library\Easilys\Traits\Integers\IntegerIdSiteOwnerTrait;
+use WBW\Library\Common\Traits\Integers\IntegerIdTrait;
+use WBW\Library\Common\Traits\Strings\StringLabelTrait;
+
+/**
+ * Generic.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Model
+ */
+class Generic {
+
+    use IntegerIdSiteOwnerTrait;
+    use IntegerIdTrait {
+        setId as public;
+    }
+    use StringLabelTrait;
+
+    /**
+     * Generic family.
+     *
+     * @var GenericFamily|null
+     */
+    protected $genericFamily;
+
+    /**
+     * Production unit.
+     *
+     * @var ProductionUnit|null
+     */
+    protected $productionUnit;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * Get the generic family.
+     *
+     * @return GenericFamily|null Returns the generic family.
+     */
+    public function getGenericFamily(): ?GenericFamily {
+        return $this->genericFamily;
+    }
+
+    /**
+     * Get the production unit.
+     *
+     * @return ProductionUnit|null Returns the production unit.
+     */
+    public function getProductionUnit(): ?ProductionUnit {
+        return $this->productionUnit;
+    }
+
+    /**
+     * Set the generic family.
+     *
+     * @param GenericFamily|null $genericFamily The generic family.
+     * @return Generic Returns this generic.
+     */
+    public function setGenericFamily(?GenericFamily $genericFamily): Generic {
+        $this->genericFamily = $genericFamily;
+        return $this;
+    }
+
+    /**
+     * Set the production unit.
+     *
+     * @param ProductionUnit|null $productionUnit The production unit.
+     * @return Generic Returns this generic.
+     */
+    public function setProductionUnit(?ProductionUnit $productionUnit): Generic {
+        $this->productionUnit = $productionUnit;
+        return $this;
+    }
+}
---
+++ lib/easilys/Tests/Model/GenericTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Model;
+
+use WBW\Library\Easilys\Model\Generic;
+use WBW\Library\Easilys\Model\GenericFamily;
+use WBW\Library\Easilys\Model\ProductionUnit;
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+
+/**
+ * Generic test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Model
+ */
+class GenericTest extends AbstractTestCase {
+
+    /**
+     * Test setGenericFamily()
+     *
+     * @return void
+     */
+    public function testSetGenericFamily(): void {
+
+        // Set a Generic family mock.
+        $genericFamily = new GenericFamily();
+
+        $obj = new Generic();
+
+        $obj->setGenericFamily($genericFamily);
+        $this->assertSame($genericFamily, $obj->getGenericFamily());
+    }
+
+    /**
+     * Test setProductionUnit()
+     *
+     * @return void
+     */
+    public function testSetProductionUnit(): void {
+
+        // Set a Generic family mock.
+        $productionUnit = new ProductionUnit();
+
+        $obj = new Generic();
+
+        $obj->setProductionUnit($productionUnit);
+        $this->assertSame($productionUnit, $obj->getProductionUnit());
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new Generic();
+
+        $this->assertNull($obj->getId());
+        $this->assertNull($obj->getLabel());
+
+        $this->assertNull($obj->getGenericFamily());
+        $this->assertNull($obj->getProductionUnit());
+    }
+}
