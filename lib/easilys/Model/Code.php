db1e3022e796d0a0fa0eb8395eb54f59cf505edd
webeweb <support@webeweb.fr>
2024-05-03 10:51:28 +0200

    Add Code model

---
+++ lib/easilys/Model/Code.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Model;
+
+use WBW\Library\Common\Traits\Integers\IntegerIdTrait;
+
+/**
+ * Code.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Model
+ */
+class Code {
+
+    use IntegerIdTrait {
+        setId as public;
+    }
+
+    /**
+     * Label packaging
+     *
+     * @var string|null
+     */
+    protected $LabelPackaging;
+
+    /**
+     * Code packaging
+     *
+     * @var string|null
+     */
+    protected $codePackaging;
+
+    /**
+     * Id packaging
+     *
+     * @var int|null
+     */
+    protected $idPackagingUnit;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * Get the code packaging.
+     *
+     * @return string|null Returns the code packaging.
+     */
+    public function getCodePackaging(): ?string {
+        return $this->codePackaging;
+    }
+
+    /**
+     * Get the id packaging unit.
+     *
+     * @return int|null Returns the id packaging unit.
+     */
+    public function getIdPackagingUnit(): ?int {
+        return $this->idPackagingUnit;
+    }
+
+    /**
+     * Get the label packaging.
+     *
+     * @return string|null Returns the label packaging.
+     */
+    public function getLabelPackaging(): ?string {
+        return $this->LabelPackaging;
+    }
+
+    /**
+     * Set the code packaging.
+     *
+     * @param string|null $codePackaging The code packaging.
+     * @return Code Returns this code.
+     */
+    public function setCodePackaging(?string $codePackaging): Code {
+        $this->codePackaging = $codePackaging;
+        return $this;
+    }
+
+    /**
+     * Set the id packaging unit.
+     *
+     * @param int|null $idPackagingUnit The id packaging unit.
+     * @return Code Returns this code.
+     */
+    public function setIdPackagingUnit(?int $idPackagingUnit): Code {
+        $this->idPackagingUnit = $idPackagingUnit;
+        return $this;
+    }
+
+    /**
+     * Set the label packaging.
+     *
+     * @param string|null $LabelPackaging The label packaging.
+     * @return Code Returns this code.
+     */
+    public function setLabelPackaging(?string $LabelPackaging): Code {
+        $this->LabelPackaging = $LabelPackaging;
+        return $this;
+    }
+}
---
+++ lib/easilys/Tests/Model/CodeTest.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Model;
+
+use WBW\Library\Easilys\Model\Code;
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+
+/**
+ * Code test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Model
+ */
+class CodeTest extends AbstractTestCase {
+
+    /**
+     * Test setCodePackaging()
+     *
+     * @return void
+     */
+    public function testSetCodePackaging() {
+
+        $obj = new Code();
+
+        $obj->setCodePackaging("codePackaging");
+        $this->assertEquals("codePackaging", $obj->getCodePackaging());
+    }
+
+    /**
+     * Test setIdPackagingUnit()
+     *
+     * @return void
+     */
+    public function testSetIdPackagingUnit() {
+
+        $obj = new Code();
+
+        $obj->setIdPackagingUnit(1);
+        $this->assertEquals(1, $obj->getIdPackagingUnit());
+    }
+
+    /**
+     * Test setLabelPackaging()
+     *
+     * @return void
+     */
+    public function testSetLabelPackaging() {
+
+        $obj = new Code();
+
+        $obj->setLabelPackaging("codePackaging");
+        $this->assertEquals("codePackaging", $obj->getLabelPackaging());
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new Code();
+
+        $this->assertNull($obj->getId());
+
+        $this->assertNull($obj->getCodePackaging());
+        $this->assertNull($obj->getIdPackagingUnit());
+        $this->assertNull($obj->getLabelPackaging());
+    }
+}
