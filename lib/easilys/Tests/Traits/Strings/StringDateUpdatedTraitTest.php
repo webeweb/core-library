d1716f922dbc1425ab7723ce5624bc69bb5000f6
webeweb <support@webeweb.fr>
2024-05-03 10:45:03 +0200

    Add string date updated trait

---
+++ lib/easilys/Tests/Traits/Strings/StringDateUpdatedTraitTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Traits\Strings;
+
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+use WBW\Library\Easilys\Tests\Fixtures\Traits\Strings\TestStringDateUpdatedTrait;
+
+/**
+ * String date updated trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Traits\Strings
+ */
+class StringDateUpdatedTraitTest extends AbstractTestCase {
+
+    /**
+     * Test setDateUpdated()
+     *
+     * @return void
+     */
+    public function testSetDateUpdated(): void {
+
+        $obj = new TestStringDateUpdatedTrait();
+
+        $obj->setDateUpdated("dateUpdated");
+        $this->assertEquals("dateUpdated", $obj->getDateUpdated());
+    }
+}
---
+++ lib/easilys/Traits/Strings/StringDateUpdatedTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Traits\Strings;
+
+/**
+ * String date updated trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Traits\Strings
+ */
+trait StringDateUpdatedTrait {
+
+    /**
+     * Date updated.
+     *
+     * @var string|null
+     */
+    protected $dateUpdated;
+
+    /**
+     * Get the date updated.
+     *
+     * @return string|null Returns the date updated.
+     */
+    public function getDateUpdated(): ?string {
+        return $this->dateUpdated;
+    }
+
+    /**
+     * Set the date updated.
+     *
+     * @param string|null $dateUpdated The date updated.
+     * @return self Returns this instance.
+     */
+    public function setDateUpdated(?string $dateUpdated): self {
+        $this->dateUpdated = $dateUpdated;
+        return $this;
+    }
+}
