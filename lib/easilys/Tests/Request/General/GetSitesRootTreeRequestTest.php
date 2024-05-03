cbc277baa850970167bac9a897017cedf9d2f535
webeweb <support@webeweb.fr>
2024-05-03 12:34:26 +0200

    Add GET sites root tree request

---
+++ lib/easilys/Tests/Request/General/GetSitesRootTreeRequestTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Request\General;
+
+use WBW\Library\Easilys\Request\AbstractRequest;
+use WBW\Library\Easilys\Request\General\GetSitesRootTreeRequest;
+use WBW\Library\Easilys\Response\General\GetSitesResponse;
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+
+/**
+ * GET sites root tree request test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Request\General
+ */
+class GetSitesRootTreeRequestTest extends AbstractTestCase {
+
+    /**
+     * Test deserializeResponse()
+     *
+     * @return void
+     */
+    public function testDeserializeResponse(): void {
+
+        $obj = new GetSitesRootTreeRequest();
+
+        $res = $obj->deserializeResponse("");
+        $this->assertInstanceOf(GetSitesResponse::class, $res);
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("/sites/root/:id/tree/:idTree", GetSitesRootTreeRequest::RESOURCE_PATH);
+
+        $obj = new GetSitesRootTreeRequest();
+
+        $this->assertInstanceOf(AbstractRequest::class, $obj);
+
+        $this->assertEquals(GetSitesRootTreeRequest::RESOURCE_PATH, $obj->getResourcePath());
+    }
+}
