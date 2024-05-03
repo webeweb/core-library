d6a3ed94e6f757128494250137b3fa7108e24a85
webeweb <support@webeweb.fr>
2024-05-03 10:49:36 +0200

    Add Address model

---
+++ lib/easilys/Model/Address.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Model;
+
+use WBW\Library\Easilys\Traits\Booleans\BooleanIsMainTrait;
+use WBW\Library\Common\Traits\Strings\StringCountryTrait;
+use WBW\Library\Common\Traits\Strings\StringIdTrait;
+use WBW\Library\Common\Traits\Strings\StringLabelTrait;
+use WBW\Library\Common\Traits\Strings\StringPostalCodeTrait;
+use WBW\Library\Common\Traits\Strings\StringTypeTrait;
+
+/**
+ * Address.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Model
+ */
+class Address {
+
+    use BooleanIsMainTrait;
+    use StringCountryTrait;
+    use StringIdTrait;
+    use StringLabelTrait;
+    use StringPostalCodeTrait;
+    use StringTypeTrait;
+
+    /**
+     * Address.
+     *
+     * @var string|null
+     */
+    protected $address;
+
+    /**
+     * City.
+     *
+     * @var string|null
+     */
+    protected $city;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * Get the address.
+     *
+     * @return string|null Returns the address.
+     */
+    public function getAddress(): ?string {
+        return $this->address;
+    }
+
+    /**
+     * Get the city.
+     *
+     * @return string|null Returns the city.
+     */
+    public function getCity(): ?string {
+        return $this->city;
+    }
+
+    /**
+     * Set the address.
+     *
+     * @param string|null $address The address.
+     * @return Address Returns this address.
+     */
+    public function setAddress(?string $address): Address {
+        $this->address = $address;
+        return $this;
+    }
+
+    /**
+     * Set the city.
+     *
+     * @param string|null $city The city.
+     * @return Address Returns this address.
+     */
+    public function setCity(?string $city): Address {
+        $this->city = $city;
+        return $this;
+    }
+}
---
+++ lib/easilys/Tests/Model/AddressTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Model;
+
+use WBW\Library\Easilys\Model\Address;
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+
+/**
+ * Address test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Model
+ */
+class AddressTest extends AbstractTestCase {
+
+    /**
+     * Test setAddress()
+     *
+     * @return void
+     */
+    public function testSetAddress(): void {
+
+        $obj = new Address();
+
+        $obj->setAddress("address");
+        $this->assertEquals("address", $obj->getAddress());
+    }
+
+    /**
+     * Test setCity()
+     *
+     * @return void
+     */
+    public function testSetCity(): void {
+
+        $obj = new Address();
+
+        $obj->setCity("city");
+        $this->assertEquals("city", $obj->getCity());
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new Address();
+
+        $this->assertNull($obj->getId());
+        $this->assertNull($obj->getCountry());
+        $this->assertNull($obj->getIsMain());
+        $this->assertNull($obj->getLabel());
+        $this->assertNull($obj->getPostalCode());
+        $this->assertNull($obj->getType());
+
+        $this->assertNull($obj->getAddress());
+        $this->assertNull($obj->getCity());
+    }
+}
