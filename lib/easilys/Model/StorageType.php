3735369173b64a7fe1081c1b33c37a12da3a32dc
webeweb <support@webeweb.fr>
2024-05-03 10:59:01 +0200

    Add Storage type model

---
+++ lib/easilys/Model/StorageType.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Model;
+
+use WBW\Library\Common\Traits\Integers\IntegerIdTrait;
+use WBW\Library\Common\Traits\Strings\StringLabelTrait;
+
+/**
+ * Storage type.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Model
+ */
+class StorageType {
+
+    use IntegerIdTrait {
+        setId as public;
+    }
+    use StringLabelTrait;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+}
