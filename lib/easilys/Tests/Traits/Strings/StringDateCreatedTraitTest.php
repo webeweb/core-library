403ae778c385dc03878dac1fed2b3ada5b512b31
webeweb <support@webeweb.fr>
2024-05-03 10:44:19 +0200

    Add string date created trait

---
+++ lib/easilys/Tests/Traits/Strings/StringDateCreatedTraitTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Traits\Strings;
+
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+use WBW\Library\Easilys\Tests\Fixtures\Traits\Strings\TestStringDateCreatedTrait;
+
+/**
+ * String date created trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Traits\Strings
+ */
+class StringDateCreatedTraitTest extends AbstractTestCase {
+
+    /**
+     * Test setDateCreated()
+     *
+     * @return void
+     */
+    public function testSetDateCreated(): void {
+
+        $obj = new TestStringDateCreatedTrait();
+
+        $obj->setDateCreated("dateCreated");
+        $this->assertEquals("dateCreated", $obj->getDateCreated());
+    }
+}
---
+++ lib/easilys/Traits/Strings/StringDateCreatedTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Traits\Strings;
+
+/**
+ * String date created trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Traits\Strings
+ */
+trait StringDateCreatedTrait {
+
+    /**
+     * Date created.
+     *
+     * @var string|null
+     */
+    protected $dateCreated;
+
+    /**
+     * Get the date created.
+     *
+     * @return string|null Returns the date created.
+     */
+    public function getDateCreated(): ?string {
+        return $this->dateCreated;
+    }
+
+    /**
+     * Set the date created.
+     *
+     * @param string|null $dateCreated The date created.
+     * @return self Returns this instance.
+     */
+    public function setDateCreated(?string $dateCreated): self {
+        $this->dateCreated = $dateCreated;
+        return $this;
+    }
+}
