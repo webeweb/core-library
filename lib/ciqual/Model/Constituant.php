a2b17ba3ca630d6a81a387068d637fe794c2cc9c
webeweb <support@webeweb.fr>
2024-05-03 15:47:05 +0200

    Add Constituant model

---
+++ lib/ciqual/Model/Constituant.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Ciqual\Model;
+
+use JsonSerializable;
+use WBW\Library\Ciqual\Serializer\JsonSerializer;
+use WBW\Library\Ciqual\Traits\Strings\StringNomEngTrait;
+use WBW\Library\Ciqual\Traits\Strings\StringNomFrTrait;
+use WBW\Library\Common\Traits\Strings\StringCodeTrait;
+
+/**
+ * Constituant.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Ciqual\Model
+ */
+class Constituant implements JsonSerializable {
+
+    use StringCodeTrait;
+    use StringNomEngTrait;
+    use StringNomFrTrait;
+
+    /**
+     * DOM node name.
+     *
+     * @var string
+     */
+    const DOM_NODE_NAME = "CONST";
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * {@inheritDoc}
+     * @return array<string,mixed> Returns this serialized instance.
+     */
+    public function jsonSerialize(): array {
+        return JsonSerializer::serializeConstituant($this);
+    }
+}
---
+++ lib/ciqual/Tests/Model/ConstituantTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Ciqual\Tests\Model;
+
+use JsonSerializable;
+use WBW\Library\Ciqual\Model\Constituant;
+use WBW\Library\Ciqual\Tests\AbstractTestCase;
+
+/**
+ * Constituant test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Ciqual\Tests\Model
+ */
+class ConstituantTest extends AbstractTestCase {
+
+    /**
+     * Test jsonSerialize()
+     *
+     * @return void
+     */
+    public function testJsonSerialize(): void {
+
+        $obj = new Constituant();
+
+        $this->assertIsArray($obj->jsonSerialize());
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("CONST", Constituant::DOM_NODE_NAME);
+
+        $obj = new Constituant();
+        $this->assertInstanceOf(JsonSerializable::class, $obj);
+
+        $this->assertNull($obj->getCode());
+        $this->assertNull($obj->getNomEng());
+        $this->assertNull($obj->getNomFr());
+    }
+}
