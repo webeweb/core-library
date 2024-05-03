11b9ead3b6c3bf6f4d44d3540c2e61f50c05f23a
webeweb <support@webeweb.fr>
2024-05-03 10:54:08 +0200

    Add Linked user model

---
+++ lib/easilys/Model/LinkedUser.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Model;
+
+use WBW\Library\Common\Traits\Integers\IntegerIdTrait;
+use WBW\Library\Common\Traits\Strings\StringFirstNameTrait;
+use WBW\Library\Common\Traits\Strings\StringLastNameTrait;
+
+/**
+ * Linked user.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Model
+ */
+class LinkedUser {
+
+    use IntegerIdTrait {
+        setId as public;
+    }
+    use StringFirstNameTrait;
+    use StringLastNameTrait;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+}
---
+++ lib/easilys/Tests/Model/LinkedUserTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Model;
+
+use WBW\Library\Easilys\Model\LinkedUser;
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+
+/**
+ * Linked user test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Model
+ */
+class LinkedUserTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new LinkedUser();
+
+        $this->assertNull($obj->getId());
+        $this->assertNull($obj->getFirstName());
+        $this->assertNull($obj->getLastName());
+    }
+}
