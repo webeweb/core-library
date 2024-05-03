f94075ab79a6258a5571142e6a93bee595c8f918
webeweb <support@webeweb.fr>
2024-05-03 11:03:31 +0200

    Add Params model

---
+++ lib/easilys/Model/Params.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Model;
+
+/**
+ * Params.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Model
+ */
+class Params {
+
+    /**
+     * Supplier order auto receipt.
+     *
+     * @var bool|null
+     */
+    protected $supplierOrderAutoReceipt;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * Get the supplier order auto receipt.
+     *
+     * @return bool|null Returns the supplier order auto receipt
+     */
+    public function getSupplierOrderAutoReceipt(): ?bool {
+        return $this->supplierOrderAutoReceipt;
+    }
+
+    /**
+     * Set the supplier order auto receipt.
+     *
+     * @param bool|null $supplierOrderAutoReceipt The supplier order auto receipt.
+     * @return Params Returns this params.
+     */
+    public function setSupplierOrderAutoReceipt(?bool $supplierOrderAutoReceipt): Params {
+        $this->supplierOrderAutoReceipt = $supplierOrderAutoReceipt;
+        return $this;
+    }
+}
---
+++ lib/easilys/Tests/Model/ParamsTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Model;
+
+use WBW\Library\Easilys\Model\Params;
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+
+/**
+ * Params test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Model
+ */
+class ParamsTest extends AbstractTestCase {
+
+    /**
+     * Test setSupplierOrderAutoReceipt()
+     *
+     * @return void
+     */
+    public function testSetSupplierOrderAutoReceipt(): void {
+
+        $obj = new Params();
+
+        $obj->setSupplierOrderAutoReceipt(true);
+        $this->assertTrue($obj->getSupplierOrderAutoReceipt());
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new Params();
+
+        $this->assertNull($obj->getSupplierOrderAutoReceipt());
+    }
+}
