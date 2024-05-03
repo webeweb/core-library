8303add8d1409b576589716534ab1094bfa9989a
webeweb <support@webeweb.fr>
2024-05-03 10:50:59 +0200

    Add Certification model

---
+++ lib/easilys/Model/Certification.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Model;
+
+use WBW\Library\Common\Traits\Integers\IntegerIdTrait;
+use WBW\Library\Common\Traits\Strings\StringCodeTrait;
+use WBW\Library\Common\Traits\Strings\StringImageTrait;
+use WBW\Library\Common\Traits\Strings\StringLabelTrait;
+
+/**
+ * Certification.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Model
+ */
+class Certification {
+
+    use IntegerIdTrait {
+        setId as public;
+    }
+    use StringCodeTrait;
+    use StringImageTrait;
+    use StringLabelTrait;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+}
