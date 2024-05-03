4d6dd8673ec2d44306bc38d4dcbf860131ef3af4
webeweb <support@webeweb.fr>
2024-05-03 10:53:30 +0200

    Add Fax model

---
+++ lib/easilys/Tests/Model/FaxTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Model;
+
+use WBW\Library\Easilys\Model\Fax;
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+
+/**
+ * Fax test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Model
+ */
+class FaxTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new Fax();
+
+        $this->assertNull($obj->getLabel());
+        $this->assertNull($obj->getNumber());
+    }
+}
