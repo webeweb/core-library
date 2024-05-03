85051f0022200b6996c8ff84b9cd02270f55afcc
webeweb <support@webeweb.fr>
2024-05-03 11:00:07 +0200

    Add Tag model

---
+++ lib/easilys/Model/Tag.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Model;
+
+use WBW\Library\Common\Traits\Integers\IntegerIdTrait;
+use WBW\Library\Common\Traits\Integers\IntegerTypeTrait;
+use WBW\Library\Common\Traits\Strings\StringLabelTrait;
+
+/**
+ * Tag.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Model
+ */
+class Tag {
+
+    use IntegerIdTrait {
+        setId as public;
+    }
+    use IntegerTypeTrait;
+    use StringLabelTrait;
+
+    /**
+     * Background color.
+     *
+     * @var string|null
+     */
+    protected $backgroundColor;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * Get the background color.
+     *
+     * @return string|null Returns the background color.
+     */
+    public function getBackgroundColor(): ?string {
+        return $this->backgroundColor;
+    }
+
+    /**
+     * Set the background color.
+     *
+     * @param string|null $backgroundColor The background color.
+     * @return Tag Returns this tag.
+     */
+    public function setBackgroundColor(?string $backgroundColor): Tag {
+        $this->backgroundColor = $backgroundColor;
+        return $this;
+    }
+}
