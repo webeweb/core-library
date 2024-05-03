e58fda1e858adc7283a26ae18175ef85842207a2
webeweb <support@webeweb.fr>
2024-05-03 11:00:15 +0200

    Add Tax model

---
+++ lib/easilys/Model/Tax.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Model;
+
+use WBW\Library\Common\Traits\Floats\FloatValueTrait;
+use WBW\Library\Common\Traits\Integers\IntegerIdTrait;
+use WBW\Library\Common\Traits\Strings\StringLabelTrait;
+
+/**
+ * Tax.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Model
+ */
+class Tax {
+
+    use FloatValueTrait;
+    use IntegerIdTrait {
+        setId as public;
+    }
+    use StringLabelTrait;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+}
---
+++ lib/easilys/Tests/Model/TaxTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Model;
+
+use WBW\Library\Easilys\Model\Tax;
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+
+/**
+ * Tax test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Model
+ */
+class TaxTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new Tax();
+
+        $this->assertNull($obj->getId());
+        $this->assertNull($obj->getLabel());
+        $this->assertNull($obj->getValue());
+    }
+}
