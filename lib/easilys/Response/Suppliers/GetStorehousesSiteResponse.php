bebdf79866a9312596a9d8412d700ccec9e448f3
webeweb <support@webeweb.fr>
2024-05-03 12:20:38 +0200

    Add GET storehouses site response

---
+++ lib/easilys/Response/Suppliers/GetStorehousesSiteResponse.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Response\Suppliers;
+
+use WBW\Library\Easilys\Model\Storehouse;
+use WBW\Library\Easilys\Response\AbstractResponse;
+
+/**
+ * GET storehouses site response.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Response\Suppliers
+ */
+class GetStorehousesSiteResponse extends AbstractResponse {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Add a data.
+     *
+     * @param Storehouse|null $data The data.
+     * @return GetStorehousesSiteResponse Returns this GET storehouses site response.
+     */
+    public function addData(?Storehouse $data): GetStorehousesSiteResponse {
+
+        if (null !== $data) {
+            $this->data[] = $data;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get the data.
+     *
+     * @return Storehouse[] Returns the data.
+     */
+    public function getData(): array {
+        return $this->data;
+    }
+}
---
+++ lib/easilys/Tests/Response/Suppliers/GetStorehousesSiteResponseTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Response\Suppliers;
+
+use WBW\Library\Easilys\Model\Storehouse;
+use WBW\Library\Easilys\Response\AbstractResponse;
+use WBW\Library\Easilys\Response\Suppliers\GetStorehousesSiteResponse;
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+
+/**
+ * GET storehouses site response test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Response\Suppliers
+ */
+class GetStorehousesSiteResponseTest extends AbstractTestCase {
+
+    /**
+     * Test addData()
+     *
+     * @return void
+     */
+    public function testAddData(): void {
+
+        // Set a data mock.
+        $data = new Storehouse();
+
+        $obj = new GetStorehousesSiteResponse();
+
+        $obj->addData($data);
+        $this->assertSame($data, $obj->getData()[0]);
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new GetStorehousesSiteResponse();
+
+        $this->assertInstanceOf(AbstractResponse::class, $obj);
+
+        $this->assertEquals([], $obj->getData());
+    }
+}
