626fd169a5f3bc75079ced263510696dccede0b3
webeweb <support@webeweb.fr>
2024-05-03 10:53:51 +0200

    Add Generic family model

---
+++ lib/easilys/Model/GenericFamily.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Model;
+
+use WBW\Library\Common\Traits\Integers\IntegerIdTrait;
+use WBW\Library\Common\Traits\Strings\StringLabelTrait;
+
+/**
+ * Generic family.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Model
+ */
+class GenericFamily {
+
+    use IntegerIdTrait {
+        setId as public;
+    }
+    use StringLabelTrait;
+
+    /**
+     * Parent.
+     *
+     * @var GenericFamily|null
+     */
+    protected $parent;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * Get the parent.
+     *
+     * @return GenericFamily|null Returns the parent.
+     */
+    public function getParent(): ?GenericFamily {
+        return $this->parent;
+    }
+
+    /**
+     * Set the parent.
+     *
+     * @param GenericFamily|null $parent The parent.
+     * @return GenericFamily Returns this generic family.
+     */
+    public function setParent(?GenericFamily $parent): GenericFamily {
+        $this->parent = $parent;
+        return $this;
+    }
+}
