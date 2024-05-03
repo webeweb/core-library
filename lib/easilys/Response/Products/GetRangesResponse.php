d6719d0d9926de5e3f0ab040618fdb05b6dbfcca
webeweb <support@webeweb.fr>
2024-05-03 12:20:11 +0200

    Add GET ranges response

---
+++ lib/easilys/Response/Products/GetRangesResponse.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Response\Products;
+
+use WBW\Library\Easilys\Model\Range;
+use WBW\Library\Easilys\Response\AbstractResponse;
+
+/**
+ * GET ranges response.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Response\Products
+ */
+class GetRangesResponse extends AbstractResponse {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Add a data.
+     *
+     * @param Range|null $data The data.
+     * @return GetRangesResponse Returns this GET ranges response.
+     */
+    public function addData(?Range $data): GetRangesResponse {
+
+        if (null !== $data) {
+            $this->data[] = $data;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get the data.
+     *
+     * @return Range[] Returns the data.
+     */
+    public function getData(): array {
+        return $this->data;
+    }
+}
