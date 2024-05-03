abb3f6008a045463c8c8983eae64a8517b10304c
webeweb <support@webeweb.fr>
2024-05-03 10:43:26 +0200

    Add string code internal

---
+++ lib/easilys/Tests/Traits/Strings/StringCodeInternalTraitTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Traits\Strings;
+
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+use WBW\Library\Easilys\Tests\Fixtures\Traits\Strings\TestStringCodeInternalTrait;
+
+/**
+ * String code internal trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Traits\Strings
+ */
+class StringCodeInternalTraitTest extends AbstractTestCase {
+
+    /**
+     * Test setCodeInternal()
+     *
+     * @return void
+     */
+    public function testSetCodeInternal(): void {
+
+        $obj = new TestStringCodeInternalTrait();
+
+        $obj->setCodeInternal("codeInternal");
+        $this->assertEquals("codeInternal", $obj->getCodeInternal());
+    }
+}
---
+++ lib/easilys/Traits/Strings/StringCodeInternalTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Traits\Strings;
+
+/**
+ * String code internal trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Traits\Strings
+ */
+trait StringCodeInternalTrait {
+
+    /**
+     * Code internal.
+     *
+     * @var string|null
+     */
+    protected $codeInternal;
+
+    /**
+     * Get the code internal.
+     *
+     * @return string|null Returns the code internal.
+     */
+    public function getCodeInternal(): ?string {
+        return $this->codeInternal;
+    }
+
+    /**
+     * Set the code internal.
+     *
+     * @param string|null $codeInternal The code internal.
+     * @return self Returns this instance.
+     */
+    public function setCodeInternal(?string $codeInternal): self {
+        $this->codeInternal = $codeInternal;
+        return $this;
+    }
+}
