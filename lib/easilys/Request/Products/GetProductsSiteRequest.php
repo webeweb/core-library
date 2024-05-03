a982880cf8f889c6bdd656a1825d34c261e70a08
webeweb <support@webeweb.fr>
2024-05-03 12:35:20 +0200

    Add GET products site request

---
+++ lib/easilys/Request/Products/GetProductsSiteRequest.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Request\Products;
+
+use WBW\Library\Easilys\Request\AbstractRequest;
+use WBW\Library\Easilys\Response\AbstractResponse;
+use WBW\Library\Easilys\Serializer\Products\ResponseDeserializer;
+use WBW\Library\Easilys\Traits\Integers\IntegerIdSiteTrait;
+use WBW\Library\Common\Provider\SubstituableRequestInterface;
+
+/**
+ * GET products site request.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Request\Products
+ */
+class GetProductsSiteRequest extends AbstractRequest implements SubstituableRequestInterface {
+
+    use IntegerIdSiteTrait;
+
+    /**
+     * Resource path.
+     *
+     * @var string
+     */
+    const RESOURCE_PATH = "/products/site/:idSite";
+
+    /**
+     * {@inheritdoc
+     */
+    public function deserializeResponse(string $rawResponse): AbstractResponse {
+        return ResponseDeserializer::deserializeGetProductsSiteResponse($rawResponse);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getResourcePath(): string {
+        return self::RESOURCE_PATH;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getSubstituables(): array {
+
+        return [
+            ":idSite" => $this->getIdSite(),
+        ];
+    }
+}
---
+++ lib/easilys/Tests/Request/Products/GetProductsSiteRequestTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Request\Products;
+
+use WBW\Library\Easilys\Request\AbstractRequest;
+use WBW\Library\Easilys\Request\Products\GetProductsSiteRequest;
+use WBW\Library\Easilys\Response\Products\GetProductsSiteResponse;
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+
+/**
+ * GET products site request test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Request\Products
+ */
+class GetProductsSiteRequestTest extends AbstractTestCase {
+
+    /**
+     * Test deserializeResponse()
+     *
+     * @return void
+     */
+    public function testDeserializeResponse(): void {
+
+        $obj = new GetProductsSiteRequest();
+
+        $res = $obj->deserializeResponse("");
+        $this->assertInstanceOf(GetProductsSiteResponse::class, $res);
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("/products/site/:idSite", GetProductsSiteRequest::RESOURCE_PATH);
+
+        $obj = new GetProductsSiteRequest();
+
+        $this->assertInstanceOf(AbstractRequest::class, $obj);
+
+        $this->assertEquals(GetProductsSiteRequest::RESOURCE_PATH, $obj->getResourcePath());
+    }
+}
