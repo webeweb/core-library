744d0a3a1a96b661f6fa3738e2e29b723d3446f0
webeweb <support@webeweb.fr>
2024-05-03 12:19:17 +0200

    Add GET price list headers site response

---
+++ lib/easilys/Response/Products/GetPriceListHeadersSiteResponse.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Response\Products;
+
+use WBW\Library\Easilys\Model\PriceList;
+use WBW\Library\Easilys\Response\AbstractResponse;
+
+/**
+ * GET price list headers site response.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Response\Products
+ */
+class GetPriceListHeadersSiteResponse extends AbstractResponse {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Add a data.
+     *
+     * @param PriceList|null $data The data.
+     * @return GetPriceListHeadersSiteResponse Returns this GET price list headers site response.
+     */
+    public function addData(?PriceList $data): GetPriceListHeadersSiteResponse {
+
+        if (null !== $data) {
+            $this->data[] = $data;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get the data.
+     *
+     * @return PriceList[] Returns the data.
+     */
+    public function getData(): array {
+        return $this->data;
+    }
+}
