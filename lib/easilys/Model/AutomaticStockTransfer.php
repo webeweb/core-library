c2782e1324712fd30923f57a987c8d5e932f0278
webeweb <support@webeweb.fr>
2024-05-03 10:50:21 +0200

    Add Automatic stock transfer model

---
+++ lib/easilys/Model/AutomaticStockTransfer.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Model;
+
+/**
+ * Automatic stock transfer.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Model
+ */
+class AutomaticStockTransfer {
+
+    /**
+     * From consumption.
+     *
+     * @var bool|null
+     */
+    protected $fromConsumption;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * Get the from consumption.
+     *
+     * @return bool|null Returns the from consumption.
+     */
+    public function getFromConsumption(): ?bool {
+        return $this->fromConsumption;
+    }
+
+    /**
+     * Set the from consumption.
+     *
+     * @param bool|null $fromConsumption The from consumption.
+     * @return AutomaticStockTransfer Returns this automatic stock transfer.
+     */
+    public function setFromConsumption(?bool $fromConsumption): AutomaticStockTransfer {
+        $this->fromConsumption = $fromConsumption;
+        return $this;
+    }
+}
---
+++ lib/easilys/Tests/Model/AutomaticStockTransferTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Model;
+
+use WBW\Library\Easilys\Model\AutomaticStockTransfer;
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+
+/**
+ * Automatic stock transfer test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Model
+ */
+class AutomaticStockTransferTest extends AbstractTestCase {
+
+    /**
+     * Test setFromConsumption()
+     *
+     * @return void
+     */
+    public function testSetFromConsumption(): void {
+
+        $obj = new AutomaticStockTransfer();
+
+        $obj->setFromConsumption(true);
+        $this->assertTrue( $obj->getFromConsumption());
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new AutomaticStockTransfer();
+
+        $this->assertNull($obj->getFromConsumption());
+    }
+}
