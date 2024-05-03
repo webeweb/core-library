ac2dc04c8229c9cb62171887ecfc9d101e653bc6
webeweb <support@webeweb.fr>
2024-05-03 12:35:49 +0200

    Add GET units request

---
+++ lib/easilys/Request/Products/GetUnitsRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Request\Products;
+
+use WBW\Library\Easilys\Request\AbstractRequest;
+use WBW\Library\Easilys\Response\AbstractResponse;
+use WBW\Library\Easilys\Serializer\Products\ResponseDeserializer;
+use WBW\Library\Easilys\Traits\Integers\IntegerIdSiteTrait;
+
+/**
+ * GET units request.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Request\Products
+ */
+class GetUnitsRequest extends AbstractRequest {
+
+    use IntegerIdSiteTrait;
+
+    /**
+     * Resource path.
+     *
+     * @var string
+     */
+    const RESOURCE_PATH = "/units";
+
+    /**
+     * {@inheritdoc
+     */
+    public function deserializeResponse(string $rawResponse): AbstractResponse {
+        return ResponseDeserializer::deserializeGetUnitsResponse($rawResponse);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getResourcePath(): string {
+        return self::RESOURCE_PATH;
+    }
+}
---
+++ lib/easilys/Tests/Request/Products/GetUnitsRequestTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Request\Products;
+
+use WBW\Library\Easilys\Request\AbstractRequest;
+use WBW\Library\Easilys\Request\Products\GetUnitsRequest;
+use WBW\Library\Easilys\Response\Products\GetUnitsResponse;
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+
+/**
+ * GET units request test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Request\Products
+ */
+class GetUnitsRequestTest extends AbstractTestCase {
+
+    /**
+     * Test deserializeResponse()
+     *
+     * @return void
+     */
+    public function testDeserializeResponse(): void {
+
+        $obj = new GetUnitsRequest();
+
+        $res = $obj->deserializeResponse("");
+        $this->assertInstanceOf(GetUnitsResponse::class, $res);
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("/units", GetUnitsRequest::RESOURCE_PATH);
+
+        $obj = new GetUnitsRequest();
+
+        $this->assertInstanceOf(AbstractRequest::class, $obj);
+
+        $this->assertEquals(GetUnitsRequest::RESOURCE_PATH, $obj->getResourcePath());
+    }
+}
