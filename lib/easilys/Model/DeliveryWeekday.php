522366ec4675fdc2205cb79ede90fef2c8e3fb48
webeweb <support@webeweb.fr>
2024-05-03 10:52:35 +0200

    Add Delivery weekday model

---
+++ lib/easilys/Model/DeliveryWeekday.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Model;
+
+/**
+ * Delivery weekday.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Model
+ */
+class DeliveryWeekday {
+
+    /**
+     * Weekday.
+     *
+     * @var string|null
+     */
+    protected $weekday;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * Get the weekday.
+     *
+     * @return string|null Returns the weekday.
+     */
+    public function getWeekday(): ?string {
+        return $this->weekday;
+    }
+
+    /**
+     * Set the weekday.
+     *
+     * @param string|null $weekday The weekday.
+     * @return DeliveryWeekday Returns this delivery weekday.
+     */
+    public function setWeekday(?string $weekday): DeliveryWeekday {
+        $this->weekday = $weekday;
+        return $this;
+    }
+}
---
+++ lib/easilys/Tests/Model/DeliveryWeekdayTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Model;
+
+use WBW\Library\Easilys\Model\DeliveryWeekday;
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+
+/**
+ * Delivery weekday test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Model
+ */
+class DeliveryWeekdayTest extends AbstractTestCase {
+
+    /**
+     * Test setWeekday()
+     *
+     * @return void
+     */
+    public function testSetWeekday(): void {
+
+        $obj = new DeliveryWeekday();
+
+        $obj->setWeekday("weekday");
+        $this->assertEquals("weekday", $obj->getWeekday());
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new DeliveryWeekday();
+
+        $this->assertNull($obj->getWeekday());
+    }
+}
