2a4033519a61d35e5e46805f09f8451be2b65151
webeweb <support@webeweb.fr>
2024-05-03 12:20:00 +0200

    Add GET qualities response

---
+++ lib/easilys/Response/Products/GetQualitiesResponse.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Response\Products;
+
+use WBW\Library\Easilys\Model\Quality;
+use WBW\Library\Easilys\Response\AbstractResponse;
+
+/**
+ * GET qualities response.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Response\Products
+ */
+class GetQualitiesResponse extends AbstractResponse {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Add a data.
+     *
+     * @param Quality|null $data The data.
+     * @return GetQualitiesResponse Returns this GET qualities response.
+     */
+    public function addData(?Quality $data): GetQualitiesResponse {
+
+        if (null !== $data) {
+            $this->data[] = $data;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get the data.
+     *
+     * @return Quality[] Returns the data.
+     */
+    public function getData(): array {
+        return $this->data;
+    }
+}
---
+++ lib/easilys/Tests/Response/Products/GetQualitieResponseTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Response\Products;
+
+use WBW\Library\Easilys\Model\Quality;
+use WBW\Library\Easilys\Response\AbstractResponse;
+use WBW\Library\Easilys\Response\Products\GetQualitiesResponse;
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+
+/**
+ * GET qualities response test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Response\Products
+ */
+class GetQualitieResponseTest extends AbstractTestCase {
+
+    /**
+     * Test addData()
+     *
+     * @return void
+     */
+    public function testAddData(): void {
+
+        // Set a data mock.
+        $data = new Quality();
+
+        $obj = new GetQualitiesResponse();
+
+        $obj->addData($data);
+        $this->assertSame($data, $obj->getData()[0]);
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new GetQualitiesResponse();
+
+        $this->assertInstanceOf(AbstractResponse::class, $obj);
+
+        $this->assertEquals([], $obj->getData());
+    }
+}
