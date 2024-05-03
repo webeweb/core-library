960428a44da0d9a7c648db9bc41ba87a9e75d94c
webeweb <support@webeweb.fr>
2024-05-03 10:50:03 +0200

    Add Analytical group

---
+++ lib/easilys/Tests/Model/AnalyticalGroupTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Easilys\Tests\Model;
+
+use WBW\Library\Easilys\Model\AnalyticalGroup;
+use WBW\Library\Easilys\Tests\AbstractTestCase;
+
+/**
+ * Analytical group test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Easilys\Tests\Model
+ */
+class AnalyticalGroupTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new AnalyticalGroup();
+
+        $this->assertNull($obj->getId());
+        $this->assertNull($obj->getLabel());
+    }
+}
