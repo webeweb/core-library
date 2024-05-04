e41cb161d2806fea58498e941bb314ff68f4e606
webeweb <support@webeweb.fr>
2024-05-04 14:49:21 +0200

    Add Query builder type interface

---
+++ lib/querybuilder/Model/QueryBuilderTypeInterface.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Model;
+
+/**
+ * QueryBuilder type interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Model
+ */
+interface QueryBuilderTypeInterface {
+
+    /**
+     * Type "boolean".
+     *
+     * @var string
+     */
+    public const TYPE_BOOLEAN = "boolean";
+
+    /**
+     * Type "date".
+     *
+     * @var string
+     */
+    public const TYPE_DATE = "date";
+
+    /**
+     * Type "datetime".
+     *
+     * @var string
+     */
+    public const TYPE_DATETIME = "datetime";
+
+    /**
+     * Type "double".
+     *
+     * @var string
+     */
+    public const TYPE_DOUBLE = "double";
+
+    /**
+     * Type "integer".
+     *
+     * @var string
+     */
+    public const TYPE_INTEGER = "integer";
+
+    /**
+     * Type "string".
+     *
+     * @var string
+     */
+    public const TYPE_STRING = "string";
+
+    /**
+     * Type "time".
+     *
+     * @var string
+     */
+    public const TYPE_TIME = "time";
+}
---
+++ lib/querybuilder/Tests/Model/QueryBuilderTypeInterfaceTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Tests\Model;
+
+use WBW\Library\QueryBuilder\Model\QueryBuilderTypeInterface;
+use WBW\Library\QueryBuilder\Tests\AbstractTestCase;
+
+/**
+ * QueryBuilder type interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Tests\API
+ */
+class QueryBuilderTypeInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("boolean", QueryBuilderTypeInterface::TYPE_BOOLEAN);
+        $this->assertEquals("date", QueryBuilderTypeInterface::TYPE_DATE);
+        $this->assertEquals("datetime", QueryBuilderTypeInterface::TYPE_DATETIME);
+        $this->assertEquals("double", QueryBuilderTypeInterface::TYPE_DOUBLE);
+        $this->assertEquals("integer", QueryBuilderTypeInterface::TYPE_INTEGER);
+        $this->assertEquals("string", QueryBuilderTypeInterface::TYPE_STRING);
+        $this->assertEquals("time", QueryBuilderTypeInterface::TYPE_TIME);
+    }
+}
