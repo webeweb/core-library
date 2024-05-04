d3f6e24b8e8f9a3e5720b0f3a0cdeafe6e4f3026
webeweb <support@webeweb.fr>
2024-05-04 14:49:13 +0200

    Add Query builder rule set interface

---
+++ lib/querybuilder/Model/QueryBuilderRuleSetInterface.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Model;
+
+/**
+ * QueryBuilder rule set interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Model
+ */
+interface QueryBuilderRuleSetInterface {
+
+    /**
+     * Get the condition.
+     *
+     * @return string|null Returns the condition.
+     */
+    public function getCondition(): ?string;
+
+    /**
+     * Get the rules.
+     *
+     * @return array Returns the rules.
+     */
+    public function getRules(): array;
+
+    /**
+     * Get the valid.
+     *
+     * @return bool Returns the valid.
+     */
+    public function getValid(): bool;
+}
