ba52246afb5d8cbf3bc8f3371b0e343c58b412cf
webeweb <support@webeweb.fr>
2024-05-04 15:26:38 +0200

    Add JSON serializer

---
+++ lib/querybuilder/Serializer/JsonSerializer.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Serializer;
+
+use WBW\Library\Common\Helper\ArrayHelper;
+use WBW\Library\QueryBuilder\Model\QueryBuilderFilterInterface;
+use WBW\Library\QueryBuilder\Model\QueryBuilderFilterSetInterface;
+use WBW\Library\QueryBuilder\Model\QueryBuilderValidationInterface;
+
+/**
+ * JSON serializer.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Serializer
+ */
+class JsonSerializer {
+
+    /**
+     * Serialize a filter.
+     *
+     * @param QueryBuilderFilterInterface $model The filter.
+     * @return array<string,mixed> Returns the serialized filter.
+     */
+    public static function serializeQueryBuilderFilter(QueryBuilderFilterInterface $model): array {
+
+        $output = [];
+
+        ArrayHelper::set($output, SerializerKeys::ID, $model->getId(), [null]);
+        ArrayHelper::set($output, SerializerKeys::FIELD, $model->getField(), [null]);
+        ArrayHelper::set($output, SerializerKeys::LABEL, $model->getLabel(), [null]);
+        ArrayHelper::set($output, SerializerKeys::TYPE, $model->getType(), [null]);
+        ArrayHelper::set($output, SerializerKeys::INPUT, $model->getInput(), [null]);
+        ArrayHelper::set($output, SerializerKeys::VALUES, $model->getValues(), [null]);
+        ArrayHelper::set($output, SerializerKeys::MULTIPLE, $model->getMultiple(), [null, false]);
+
+        if (null !== $model->getValues()) {
+            $output[SerializerKeys::VALIDATION] = $model->getValidation()->jsonSerialize();
+        }
+
+        ArrayHelper::set($output, SerializerKeys::OPERATORS, $model->getOperators(), [null, []]);
+
+        return $output;
+    }
+
+    /**
+     * Serialize a filter set.
+     *
+     * @param QueryBuilderFilterSetInterface $model The filter set.
+     * @return array<string,mixed> Returns the serialized filter set.
+     */
+    public static function serializeQueryBuilderFilterSet(QueryBuilderFilterSetInterface $model): array {
+
+        $output = [];
+
+        foreach ($model->getFilters() as $current) {
+            $output[] = static::serializeQueryBuilderFilter($current);
+        }
+
+        return $output;
+    }
+
+    /**
+     * Serialize a validation.
+     *
+     * @param QueryBuilderValidationInterface $validation The validation.
+     * @return array<string,mixed> Returns the serialized validation.
+     */
+    public static function serializeQueryBuilderValidation(QueryBuilderValidationInterface $validation): array {
+
+        $output = [];
+
+        ArrayHelper::set($output, SerializerKeys::FORMAT, $validation->getFormat(), [null]);
+        ArrayHelper::set($output, SerializerKeys::MIN, $validation->getMin(), [null]);
+        ArrayHelper::set($output, SerializerKeys::MAX, $validation->getMax(), [null]);
+        ArrayHelper::set($output, SerializerKeys::STEP, $validation->getStep(), [null]);
+        ArrayHelper::set($output, SerializerKeys::MESSAGES, $validation->getMessages(), [null]);
+        ArrayHelper::set($output, SerializerKeys::ALLOW_EMPTY_VALUE, $validation->getAllowEmptyValue(), [null]);
+        ArrayHelper::set($output, SerializerKeys::CALLBACK, $validation->getCallback(), [null]);
+
+        return $output;
+    }
+}
---
+++ lib/querybuilder/Tests/Serializer/JsonSerializerTest.php
@@ -0,0 +1,126 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Tests\Serializer;
+
+use WBW\Library\QueryBuilder\Model\QueryBuilderFilterInterface;
+use WBW\Library\QueryBuilder\Model\QueryBuilderFilterSetInterface;
+use WBW\Library\QueryBuilder\Model\QueryBuilderInputInterface;
+use WBW\Library\QueryBuilder\Model\QueryBuilderOperatorInterface;
+use WBW\Library\QueryBuilder\Model\QueryBuilderTypeInterface;
+use WBW\Library\QueryBuilder\Model\QueryBuilderValidationInterface;
+use WBW\Library\QueryBuilder\Serializer\JsonSerializer;
+use WBW\Library\QueryBuilder\Serializer\SerializerKeys;
+use WBW\Library\QueryBuilder\Tests\AbstractTestCase;
+
+/**
+ * JSON serializer test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Tests\Serializer
+ */
+class JsonSerializerTest extends AbstractTestCase {
+
+    /**
+     * Test serializeQueryBuilderFilter()
+     *
+     * @return void
+     */
+    public function testSerializeQueryBuilderFilter(): void {
+
+        // Set a QueryBuilder validation mock.
+        $qbValidation = $this->getMockBuilder(QueryBuilderValidationInterface::class)->getMock();
+        $qbValidation->expects($this->any())->method("jsonSerialize")->willReturn(JsonSerializer::serializeQueryBuilderValidation($qbValidation));
+
+        // Set a QueryBuilder filter mock.
+        $model = $this->getMockBuilder(QueryBuilderFilterInterface::class)->getMock();
+        $model->expects($this->any())->method("getId")->willReturn(SerializerKeys::ID);
+        $model->expects($this->any())->method("getField")->willReturn(SerializerKeys::FIELD);
+        $model->expects($this->any())->method("getInput")->willReturn(QueryBuilderInputInterface::INPUT_NUMBER);
+        $model->expects($this->any())->method("getLabel")->willReturn(SerializerKeys::LABEL);
+        $model->expects($this->any())->method("getMultiple")->willReturn(true);
+        $model->expects($this->any())->method("getOperators")->willReturn([QueryBuilderOperatorInterface::OPERATOR_EQUAL]);
+        $model->expects($this->any())->method("getType")->willReturn(QueryBuilderTypeInterface::TYPE_STRING);
+        $model->expects($this->any())->method("getValidation")->willReturn($qbValidation);
+        $model->expects($this->any())->method("getValues")->willReturn([SerializerKeys::VALUES]);
+
+        $res = JsonSerializer::serializeQueryBuilderFilter($model);
+        $this->assertCount(9, $res);
+
+        $this->assertEquals($model->getId(), $res[SerializerKeys::ID]);
+        $this->assertEquals($model->getField(), $res[SerializerKeys::FIELD]);
+        $this->assertEquals($model->getInput(), $res[SerializerKeys::INPUT]);
+        $this->assertEquals($model->getLabel(), $res[SerializerKeys::LABEL]);
+        $this->assertEquals($model->getMultiple(), $res[SerializerKeys::MULTIPLE]);
+        $this->assertEquals($model->getOperators(), $res[SerializerKeys::OPERATORS]);
+        $this->assertEquals($model->getType(), $res[SerializerKeys::TYPE]);
+        $this->assertIsArray($res[SerializerKeys::VALIDATION]);
+        $this->assertEquals($model->getValues(), $res[SerializerKeys::VALUES]);
+    }
+
+    /**
+     * Test serializeQueryBuilderSet()
+     *
+     * @return void
+     */
+    public function testSerializeQueryBuilderFilterSet(): void {
+
+        // Set a QueryBuilder filter mock.
+        $qbFilter = $this->getMockBuilder(QueryBuilderFilterInterface::class)->getMock();
+        $qbFilter->expects($this->any())->method("getId")->willReturn(SerializerKeys::ID);
+        $qbFilter->expects($this->any())->method("getType")->willReturn(QueryBuilderTypeInterface::TYPE_STRING);
+        $qbFilter->expects($this->any())->method("getOperators")->willReturn([QueryBuilderOperatorInterface::OPERATOR_EQUAL]);
+        $qbFilter->expects($this->any())->method("jsonSerialize")->willReturn(JsonSerializer::serializeQueryBuilderFilter($qbFilter));
+
+        // Set a QueryBuilder filter set mock.
+        $model = $this->getMockBuilder(QueryBuilderFilterSetInterface::class)->getMock();
+        $model->expects($this->any())->method("getFilters")->willReturn([$qbFilter]);
+
+        $res = JsonSerializer::serializeQueryBuilderFilterSet($model);
+        $this->assertCount(1, $res);
+
+        $this->assertEquals($qbFilter->getId(), $res[0][SerializerKeys::ID]);
+        $this->assertEquals($qbFilter->getLabel(), $res[0][SerializerKeys::LABEL]);
+        $this->assertEquals($qbFilter->getOperators(), $res[0][SerializerKeys::OPERATORS]);
+        $this->assertEquals($qbFilter->getType(), $res[0][SerializerKeys::TYPE]);
+    }
+
+    /**
+     * Test serializeQueryBuilderValidation()
+     *
+     * @return void
+     */
+    public function testSerializeQueryBuilderValidation(): void {
+
+        // Set a QueryBuilder validation mock.
+        $model = $this->getMockBuilder(QueryBuilderValidationInterface::class)->getMock();
+        $model->expects($this->any())->method("getAllowEmptyValue")->willReturn(true);
+        $model->expects($this->any())->method("getCallback")->willReturn(SerializerKeys::CALLBACK);
+        $model->expects($this->any())->method("getFormat")->willReturn(SerializerKeys::FORMAT);
+        $model->expects($this->any())->method("getMax")->willReturn(SerializerKeys::MAX);
+        $model->expects($this->any())->method("getMessages")->willReturn([]);
+        $model->expects($this->any())->method("getMin")->willReturn(SerializerKeys::MIN);
+        $model->expects($this->any())->method("getStep")->willReturn(0);
+
+        $res = JsonSerializer::serializeQueryBuilderValidation($model);
+        $this->assertCount(7, $res);
+
+        $this->assertEquals($model->getAllowEmptyValue(), $res[SerializerKeys::ALLOW_EMPTY_VALUE]);
+        $this->assertEquals($model->getCallback(), $res[SerializerKeys::CALLBACK]);
+        $this->assertEquals($model->getFormat(), $res[SerializerKeys::FORMAT]);
+        $this->assertEquals($model->getMax(), $res[SerializerKeys::MAX]);
+        $this->assertEquals($model->getMessages(), $res[SerializerKeys::MESSAGES]);
+        $this->assertEquals($model->getMin(), $res[SerializerKeys::MIN]);
+        $this->assertEquals($model->getStep(), $res[SerializerKeys::STEP]);
+    }
+}
