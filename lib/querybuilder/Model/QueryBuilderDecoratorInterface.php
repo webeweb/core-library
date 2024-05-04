57aba6fd685bbc0fb509d87e7ed74b00fe0fe7e3
webeweb <support@webeweb.fr>
2024-05-04 14:48:19 +0200

    Add Query builder decorator interface

---
+++ lib/querybuilder/Model/QueryBuilderDecoratorInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Model;
+
+use InvalidArgumentException;
+
+/**
+ * QueryBuilder decorator interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Model
+ */
+interface QueryBuilderDecoratorInterface {
+
+    /**
+     * Default separator.
+     *
+     * @var string
+     */
+    public const DEFAULT_SEPARATOR = "{{ separator }}";
+
+    /**
+     * Convert the rule into a SQL string representation.
+     *
+     * @param QueryBuilderRuleInterface $rule The rule.
+     * @param bool $wrap Wrap ?
+     * @return string Returns a SQL string representing the rule.
+     * @throws InvalidArgumentException Throws an invalid argument exception if the type is invalid.
+     */
+    public function toSql(QueryBuilderRuleInterface $rule, bool $wrap = false): string;
+}
---
+++ lib/querybuilder/Tests/Model/QueryBuilderDecoratorInterfaceTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Tests\Model;
+
+use WBW\Library\QueryBuilder\Model\QueryBuilderDecoratorInterface;
+use WBW\Library\QueryBuilder\Tests\AbstractTestCase;
+
+/**
+ * Query builder decorator interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Tests\Model
+ */
+class QueryBuilderDecoratorInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("{{ separator }}", QueryBuilderDecoratorInterface::DEFAULT_SEPARATOR);
+    }
+}
