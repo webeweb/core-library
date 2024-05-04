6734d34389ab1cc73da5be0509afe21feeca8f73
webeweb <support@webeweb.fr>
2024-05-04 14:53:24 +0200

    Add Serializer keys

---
+++ lib/querybuilder/Serializer/SerializerKeys.php
@@ -0,0 +1,170 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Serializer;
+
+/**
+ * Serializer keys.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Serializer
+ */
+interface SerializerKeys {
+
+    /**
+     * Serializer key "allow_empty_value".
+     *
+     * @var string
+     */
+    public const ALLOW_EMPTY_VALUE = "allow_empty_value";
+
+    /**
+     * Serializer key "callback".
+     *
+     * @var string
+     */
+    public const CALLBACK = "callback";
+
+    /**
+     * Serializer key "condition".
+     *
+     * @var string
+     */
+    public const CONDITION = "condition";
+
+    /**
+     * Serializer key "field".
+     *
+     * @var string
+     */
+    public const FIELD = "field";
+
+    /**
+     * Serializer key "format".
+     *
+     * @var string
+     */
+    public const FORMAT = "format";
+
+    /**
+     * Serializer key "id".
+     *
+     * @var string
+     */
+    public const ID = "id";
+
+    /**
+     * Serializer key "input".
+     *
+     * @var string
+     */
+    public const INPUT = "input";
+
+    /**
+     * Serializer key "label".
+     *
+     * @var string
+     */
+    public const LABEL = "label";
+
+    /**
+     * Serializer key "max".
+     *
+     * @var string
+     */
+    public const MAX = "max";
+
+    /**
+     * Serializer key "messages".
+     *
+     * @var string
+     */
+    public const MESSAGES = "messages";
+
+    /**
+     * Serializer key "min".
+     *
+     * @var string
+     */
+    public const MIN = "min";
+
+    /**
+     * Serializer key "multiple".
+     *
+     * @var string
+     */
+    public const MULTIPLE = "multiple";
+
+    /**
+     * Serializer key "operator".
+     *
+     * @var string
+     */
+    public const OPERATOR = "operator";
+
+    /**
+     * Serializer key "operators".
+     *
+     * @var string
+     */
+    public const OPERATORS = "operators";
+
+    /**
+     * Serializer key "rules".
+     *
+     * @var string
+     */
+    public const RULES = "rules";
+
+    /**
+     * Serializer key "step".
+     *
+     * @var string
+     */
+    public const STEP = "step";
+
+    /**
+     * Serializer key "type".
+     *
+     * @var string
+     */
+    public const TYPE = "type";
+
+    /**
+     * Serializer key "valid".
+     *
+     * @var string
+     */
+    public const VALID = "valid";
+
+    /**
+     * Serializer key "validation".
+     *
+     * @var string
+     */
+    public const VALIDATION = "validation";
+
+    /**
+     * Serializer key "value".
+     *
+     * @var string
+     */
+    public const VALUE = "value";
+
+    /**
+     * Serializer key "values".
+     *
+     * @var string
+     */
+    public const VALUES = "values";
+}
---
+++ lib/querybuilder/Tests/Serializer/SerializerKeysTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Tests\Serializer;
+
+use WBW\Library\QueryBuilder\Serializer\SerializerKeys;
+use WBW\Library\QueryBuilder\Tests\AbstractTestCase;
+
+/**
+ * Serializer keys test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Tests\Serializer
+ */
+class SerializerKeysTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("allow_empty_value", SerializerKeys::ALLOW_EMPTY_VALUE);
+        $this->assertEquals("callback", SerializerKeys::CALLBACK);
+        $this->assertEquals("condition", SerializerKeys::CONDITION);
+        $this->assertEquals("field", SerializerKeys::FIELD);
+        $this->assertEquals("format", SerializerKeys::FORMAT);
+        $this->assertEquals("id", SerializerKeys::ID);
+        $this->assertEquals("input", SerializerKeys::INPUT);
+        $this->assertEquals("label", SerializerKeys::LABEL);
+        $this->assertEquals("max", SerializerKeys::MAX);
+        $this->assertEquals("messages", SerializerKeys::MESSAGES);
+        $this->assertEquals("min", SerializerKeys::MIN);
+        $this->assertEquals("multiple", SerializerKeys::MULTIPLE);
+        $this->assertEquals("operator", SerializerKeys::OPERATOR);
+        $this->assertEquals("operators", SerializerKeys::OPERATORS);
+        $this->assertEquals("rules", SerializerKeys::RULES);
+        $this->assertEquals("step", SerializerKeys::STEP);
+        $this->assertEquals("type", SerializerKeys::TYPE);
+        $this->assertEquals("valid", SerializerKeys::VALID);
+        $this->assertEquals("validation", SerializerKeys::VALIDATION);
+        $this->assertEquals("value", SerializerKeys::VALUE);
+        $this->assertEquals("values", SerializerKeys::VALUES);
+    }
+}
