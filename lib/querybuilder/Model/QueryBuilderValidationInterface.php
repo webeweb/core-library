ab886364f51aedade8a2cc6d61eab2fc79352c01
webeweb <support@webeweb.fr>
2024-05-04 14:49:29 +0200

    Add Query builder validator interface

---
+++ lib/querybuilder/Model/QueryBuilderValidationInterface.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Model;
+
+use JsonSerializable;
+
+/**
+ * QueryBuilder validation interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Model
+ */
+interface QueryBuilderValidationInterface extends JsonSerializable {
+
+    /**
+     * Get the allow empty value.
+     *
+     * @return bool|null Returns  the allow empty value.
+     */
+    public function getAllowEmptyValue(): ?bool;
+
+    /**
+     * Get the callback.
+     *
+     * @return string|null
+     */
+    public function getCallback(): ?string;
+
+    /**
+     * Get the format.
+     *
+     * @return mixed Returns the format.
+     */
+    public function getFormat();
+
+    /**
+     * Get the max.
+     *
+     * @return mixed Returns the max.
+     */
+    public function getMax();
+
+    /**
+     * Get the messages.
+     *
+     * @return array|null Returns the messages.
+     */
+    public function getMessages(): ?array;
+
+    /**
+     * Get the min.
+     *
+     * @return mixed Returns the min.
+     */
+    public function getMin();
+
+    /**
+     * Get the step.
+     *
+     * @return mixed Returns the step.
+     */
+    public function getStep();
+}
