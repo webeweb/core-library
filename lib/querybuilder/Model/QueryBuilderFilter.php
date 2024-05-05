409ba929a6ec4d6b12796e1438d4733f2064a3de
webeweb <support@webeweb.fr>
2024-05-05 14:22:19 +0200

    Add QueryBuilder filter model

---
+++ lib/querybuilder/Model/QueryBuilderFilter.php
@@ -0,0 +1,215 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Model;
+
+use InvalidArgumentException;
+use WBW\Library\QueryBuilder\Factory\QueryBuilderFactory;
+use WBW\Library\QueryBuilder\Serializer\JsonSerializer;
+
+/**
+ * QueryBuilder filter.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Model
+ */
+class QueryBuilderFilter extends AbstractQueryBuilder implements QueryBuilderFilterInterface {
+
+    /**
+     * Decorator.
+     *
+     * @var QueryBuilderDecoratorInterface|null
+     */
+    private $decorator;
+
+    /**
+     * Label.
+     *
+     * @var string|null
+     */
+    private $label;
+
+    /**
+     * Multiple.
+     *
+     * @var bool|null
+     */
+    private $multiple;
+
+    /**
+     * Operators.
+     *
+     * @var string[]|null
+     */
+    private $operators;
+
+    /**
+     * Validation.
+     *
+     * @var QueryBuilderValidationInterface|null
+     */
+    private $validation;
+
+    /**
+     * Values.
+     *
+     * @var mixed[]|null
+     */
+    private $values;
+
+    /**
+     * Constructor.
+     *
+     * @param string $id The id.
+     * @param string $type The type.
+     * @param string[] $operators The operators.
+     * @throws InvalidArgumentException Throws an invalid argument exception if an argument is invalid.
+     */
+    public function __construct(string $id, string $type, array $operators) {
+        parent::__construct();
+
+        $this->setId($id);
+        $this->setLabel("");
+        $this->setMultiple(false);
+        $this->setOperators($operators);
+        $this->setType($type);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDecorator(): ?QueryBuilderDecoratorInterface {
+        return $this->decorator;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getLabel(): string {
+        return $this->label;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getMultiple(): bool {
+        return $this->multiple;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOperators(): array {
+        return $this->operators;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getValidation(): ?QueryBuilderValidationInterface {
+        return $this->validation;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getValues(): ?array {
+        return $this->values;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @return array<string,mixed> Returns this serialized instance.
+     */
+    public function jsonSerialize(): array {
+        return JsonSerializer::serializeQueryBuilderFilter($this);
+    }
+
+    /**
+     * Set the decorator.
+     *
+     * @param QueryBuilderDecoratorInterface|null $decorator The decorator.
+     * @return QueryBuilderFilterInterface Returns this filter.
+     */
+    public function setDecorator(?QueryBuilderDecoratorInterface $decorator): QueryBuilderFilterInterface {
+        $this->decorator = $decorator;
+        return $this;
+    }
+
+    /**
+     * Set the label.
+     *
+     * @param string $label The label.
+     * @return QueryBuilderFilterInterface Returns this filter.
+     */
+    public function setLabel(string $label): QueryBuilderFilterInterface {
+        $this->label = $label;
+        return $this;
+    }
+
+    /**
+     * Set the multiple.
+     *
+     * @param bool $multiple The multiple.
+     * @return QueryBuilderFilterInterface Returns this filter.
+     */
+    public function setMultiple(bool $multiple): QueryBuilderFilterInterface {
+        $this->multiple = $multiple;
+        return $this;
+    }
+
+    /**
+     * Set the operators.
+     *
+     * @param string[] $operators The operators.
+     * @return QueryBuilderFilterInterface Returns this filter.
+     * @throws InvalidArgumentException Throws an invalid argument exception if an operator is invalid.
+     */
+    public function setOperators(array $operators): QueryBuilderFilterInterface {
+
+        $enum = QueryBuilderFactory::enumOperators();
+
+        foreach ($operators as $current) {
+
+            if (null !== $current && false === array_key_exists($current, $enum)) {
+                throw new InvalidArgumentException(sprintf('The operator "%s" is invalid', $current));
+            }
+        }
+
+        $this->operators = $operators;
+
+        return $this;
+    }
+
+    /**
+     * Set the validation.
+     *
+     * @param QueryBuilderValidationInterface|null $validation The validation.
+     * @return QueryBuilderFilterInterface Returns this filter.
+     */
+    public function setValidation(?QueryBuilderValidationInterface $validation): QueryBuilderFilterInterface {
+        $this->validation = $validation;
+        return $this;
+    }
+
+    /**
+     * Set the values.
+     *
+     * @param mixed[]|null $values The values.
+     * @return QueryBuilderFilterInterface Returns this filter.
+     */
+    public function setValues(?array $values): QueryBuilderFilterInterface {
+        $this->values = $values;
+        return $this;
+    }
+}
---
+++ lib/querybuilder/Tests/Model/QueryBuilderFilterTest.php
@@ -0,0 +1,174 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Tests\Model;
+
+use InvalidArgumentException;
+use JsonSerializable;
+use Throwable;
+use WBW\Library\QueryBuilder\Model\QueryBuilderDecoratorInterface;
+use WBW\Library\QueryBuilder\Model\QueryBuilderFilter;
+use WBW\Library\QueryBuilder\Model\QueryBuilderFilterInterface;
+use WBW\Library\QueryBuilder\Model\QueryBuilderOperatorInterface;
+use WBW\Library\QueryBuilder\Model\QueryBuilderTypeInterface;
+use WBW\Library\QueryBuilder\Model\QueryBuilderValidationInterface;
+use WBW\Library\QueryBuilder\Tests\AbstractTestCase;
+
+/**
+ * QueryBuilder filter test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Tests\Model
+ */
+class QueryBuilderFilterTest extends AbstractTestCase {
+
+    /**
+     * Test jsonSerialize()
+     *
+     * @return void
+     */
+    public function testJsonSerialize(): void {
+
+        $obj = new QueryBuilderFilter("id", QueryBuilderTypeInterface::TYPE_STRING, [QueryBuilderOperatorInterface::OPERATOR_EQUAL]);
+
+        $this->assertIsArray($obj->jsonSerialize());
+    }
+
+    /**
+     * Test setDecorator()
+     *
+     * @return void
+     */
+    public function testSetDecorator(): void {
+
+        // Set a QueryBuilder decorator mock.
+        $qbDecorator = $this->getMockBuilder(QueryBuilderDecoratorInterface::class)->getMock();
+
+        $obj = new QueryBuilderFilter("id", QueryBuilderTypeInterface::TYPE_BOOLEAN, []);
+
+        $obj->setDecorator($qbDecorator);
+        $this->assertSame($qbDecorator, $obj->getDecorator());
+    }
+
+    /**
+     * Test setLabel()
+     *
+     * @return void
+     */
+    public function testSetLabel(): void {
+
+        $obj = new QueryBuilderFilter("id", QueryBuilderTypeInterface::TYPE_BOOLEAN, []);
+
+        $obj->setLabel("label");
+        $this->assertEquals("label", $obj->getLabel());
+    }
+
+    /**
+     * Test setMultiple()
+     *
+     * @return void
+     */
+    public function testSetMultiple(): void {
+
+        $obj = new QueryBuilderFilter("id", QueryBuilderTypeInterface::TYPE_BOOLEAN, []);
+
+        $obj->setMultiple(true);
+        $this->assertTrue($obj->getMultiple());
+    }
+
+    /**
+     * Test setOperators()
+     *
+     * @return void
+     */
+    public function testSetOperators(): void {
+
+        $obj = new QueryBuilderFilter("id", QueryBuilderTypeInterface::TYPE_BOOLEAN, []);
+
+        $obj->setOperators([QueryBuilderOperatorInterface::OPERATOR_EQUAL]);
+        $this->assertEquals([QueryBuilderOperatorInterface::OPERATOR_EQUAL], $obj->getOperators());
+    }
+
+    /**
+     * Test setOperators()
+     *
+     * @return void
+     */
+    public function testSetOperatorsWithInvalidArgumentException(): void {
+
+        $obj = new QueryBuilderFilter("id", QueryBuilderTypeInterface::TYPE_BOOLEAN, []);
+
+        try {
+
+            $obj->setOperators(["exception"]);
+        } catch (Throwable $ex) {
+
+            $this->assertInstanceOf(InvalidArgumentException::class, $ex);
+            $this->assertEquals('The operator "exception" is invalid', $ex->getMessage());
+        }
+    }
+
+    /**
+     * Test setValidation()
+     *
+     * @return void
+     */
+    public function testSetValidation(): void {
+
+        // Set a QueryBuilder validation mock.
+        $qbValidation = $this->getMockBuilder(QueryBuilderValidationInterface::class)->getMock();
+
+        $obj = new QueryBuilderFilter("id", QueryBuilderTypeInterface::TYPE_BOOLEAN, []);
+
+        $obj->setValidation($qbValidation);
+        $this->assertSame($qbValidation, $obj->getValidation());
+    }
+
+    /**
+     * Test setValues()
+     *
+     * @return void
+     */
+    public function testSetValues(): void {
+
+        $obj = new QueryBuilderFilter("id", QueryBuilderTypeInterface::TYPE_BOOLEAN, []);
+
+        $obj->setValues(["values"]);
+        $this->assertEquals(["values"], $obj->getValues());
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new QueryBuilderFilter("id", QueryBuilderTypeInterface::TYPE_BOOLEAN, []);
+
+        $this->assertInstanceOf(JsonSerializable::class, $obj);
+        $this->assertInstanceOf(QueryBuilderFilterInterface::class, $obj);
+
+        $this->assertEquals("id", $obj->getId());
+        $this->assertNull($obj->getField());
+        $this->assertNull($obj->getInput());
+        $this->assertEquals(QueryBuilderTypeInterface::TYPE_BOOLEAN, $obj->getType());
+
+        $this->assertNull($obj->getDecorator());
+        $this->assertEquals("", $obj->getLabel());
+        $this->assertFalse($obj->getMultiple());
+        $this->assertEquals([], $obj->getOperators());
+        $this->assertNull($obj->getValidation());
+        $this->assertNull($obj->getValues());
+    }
+}
