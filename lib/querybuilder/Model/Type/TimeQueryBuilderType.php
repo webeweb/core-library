c72515535eee0790a84c7a2d2355f2e74952cbb8
webeweb <support@webeweb.fr>
2024-05-06 10:16:56 +0200

    Add Time QueryBuilder type model

---
+++ lib/querybuilder/Model/Type/TimeQueryBuilderType.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Model\Type;
+
+use WBW\Library\QueryBuilder\Model\AbstractQueryBuilderType;
+use WBW\Library\QueryBuilder\Model\QueryBuilderRuleInterface;
+
+/**
+ * Time QueryBuilder type.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Model\Type
+ */
+class TimeQueryBuilderType extends AbstractQueryBuilderType {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct(self::TYPE_TIME);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function toSql(QueryBuilderRuleInterface $rule, bool $wrap = false): string {
+
+        $str = addslashes($rule->getValue());
+
+        return true === $wrap ? "'$str'" : $str;
+    }
+}
