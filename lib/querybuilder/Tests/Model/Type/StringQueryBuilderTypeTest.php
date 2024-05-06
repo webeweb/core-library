cd2ea4e45937231473acf36991989a62178fc4b2
webeweb <support@webeweb.fr>
2024-05-06 10:16:47 +0200

    Add String QueryBuilder type model

---
+++ lib/querybuilder/Tests/Model/Type/StringQueryBuilderTypeTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Tests\Model\Type;
+
+use WBW\Library\QueryBuilder\Model\QueryBuilderDecoratorInterface;
+use WBW\Library\QueryBuilder\Model\QueryBuilderRuleInterface;
+use WBW\Library\QueryBuilder\Model\QueryBuilderTypeInterface;
+use WBW\Library\QueryBuilder\Model\Type\StringQueryBuilderType;
+use WBW\Library\QueryBuilder\Tests\AbstractTestCase;
+
+/**
+ * String QueryBuilder type test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Tests\Model\Type
+ */
+class StringQueryBuilderTypeTest extends AbstractTestCase {
+
+    /**
+     * Test toSql()
+     *
+     * @return void
+     */
+    public function testToSql(): void {
+
+        // Set a QueryBuilder rule mock.
+        $rule = $this->getMockBuilder(QueryBuilderRuleInterface::class)->getMock();
+        $rule->expects($this->any())->method("getValue")->willReturn("string");
+
+        $obj = new StringQueryBuilderType();
+
+        $this->assertEquals("string", $obj->toSql($rule));
+    }
+
+    /**
+     * Test toSql()
+     *
+     * @return void
+     */
+    public function testToSqlWithWrap(): void {
+
+        // Set a QueryBuilder rule mock.
+        $rule = $this->getMockBuilder(QueryBuilderRuleInterface::class)->getMock();
+        $rule->expects($this->any())->method("getValue")->willReturn("string");
+
+        $obj = new StringQueryBuilderType();
+
+        $this->assertEquals("'string'", $obj->toSql($rule, true));
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new StringQueryBuilderType();
+
+        $this->assertInstanceOf(QueryBuilderTypeInterface::class, $obj);
+        $this->assertInstanceOf(QueryBuilderDecoratorInterface::class, $obj);
+
+        $this->assertEquals(QueryBuilderTypeInterface::TYPE_STRING, $obj->getType());
+    }
+}
