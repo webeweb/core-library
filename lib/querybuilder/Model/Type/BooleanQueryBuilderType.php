f0668e8371dd18147de4364c9394fbd4e52d9458
webeweb <support@webeweb.fr>
2024-05-06 10:15:54 +0200

    Add Boolean QueryBuilder type model

---
+++ lib/querybuilder/Model/Type/BooleanQueryBuilderType.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Model\Type;
+
+use WBW\Library\Common\Helper\IntegerHelper;
+use WBW\Library\QueryBuilder\Model\AbstractQueryBuilderType;
+use WBW\Library\QueryBuilder\Model\QueryBuilderRuleInterface;
+
+/**
+ * Boolean QueryBuilder type.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Model\Type
+ */
+class BooleanQueryBuilderType extends AbstractQueryBuilderType {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct(self::TYPE_BOOLEAN);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function toSql(QueryBuilderRuleInterface $rule, bool $wrap = false): string {
+        return sprintf("%d", IntegerHelper::parseBoolean($rule->getValue()));
+    }
+}
