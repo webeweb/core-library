0d1e58baa68d4a82a12ba6b81d1c2065aa54af9c
webeweb <support@webeweb.fr>
2024-05-06 10:06:27 +0200

    Add Is empty QueryBuilder operator model

---
+++ lib/querybuilder/Tests/Model/Operator/IsEmptyQueryBuilderOperatorTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Tests\Model\Operator;
+
+use WBW\Library\QueryBuilder\Model\Operator\IsEmptyQueryBuilderOperator;
+use WBW\Library\QueryBuilder\Model\QueryBuilderDecoratorInterface;
+use WBW\Library\QueryBuilder\Model\QueryBuilderOperatorInterface;
+use WBW\Library\QueryBuilder\Model\QueryBuilderRuleInterface;
+use WBW\Library\QueryBuilder\Tests\AbstractTestCase;
+
+/**
+ * Is empty QueryBuilder operator test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Tests\Model\Operator
+ */
+class IsEmptyQueryBuilderOperatorTest extends AbstractTestCase {
+
+    /**
+     * Test toSql()
+     *
+     * @return void
+     */
+    public function testToSql(): void {
+
+        // Set a QueryBuilder rule mock.
+        $rule = $this->getMockBuilder(QueryBuilderRuleInterface::class)->getMock();
+        $rule->expects($this->any())->method("getField")->willReturn("field");
+
+        $obj = new IsEmptyQueryBuilderOperator();
+
+        $this->assertEquals("field IS NULL", $obj->toSql($rule));
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new IsEmptyQueryBuilderOperator();
+
+        $this->assertInstanceOf(QueryBuilderOperatorInterface::class, $obj);
+        $this->assertInstanceOf(QueryBuilderDecoratorInterface::class, $obj);
+
+        $this->assertEquals(QueryBuilderOperatorInterface::OPERATOR_IS_EMPTY, $obj->getOperator());
+    }
+}
