6379978335db76440bbfb41f6b17c86d60abac4e
webeweb <support@webeweb.fr>
2024-05-06 10:05:26 +0200

    Add Ends with QueryBuilder operator model

---
+++ lib/querybuilder/Model/Operator/EndsWithQueryBuilderOperator.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Model\Operator;
+
+use WBW\Library\QueryBuilder\Factory\QueryBuilderFactory;
+use WBW\Library\QueryBuilder\Model\QueryBuilderRuleInterface;
+
+/**
+ * Ends with QueryBuilder operator.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Model\Operator
+ */
+class EndsWithQueryBuilderOperator extends AbstractQueryBuilderOperator {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct(self::OPERATOR_ENDS_WITH);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function toSql(QueryBuilderRuleInterface $rule, bool $wrap = false): string {
+
+        $qbt = QueryBuilderFactory::newType($rule->getType());
+
+        $sql = [
+            parent::toSql($rule, $wrap),
+            sprintf("'%%%s'", $qbt->toSql($rule)),
+        ];
+
+        return implode(" ", $sql);
+    }
+}
