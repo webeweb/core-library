f1363208f6cdbdb2599dbb57ed224208c04b2d0e
webeweb <support@webeweb.fr>
2024-05-06 10:06:17 +0200

    Add In QueryBuilder operator model

---
+++ lib/querybuilder/Model/Operator/InQueryBuilderOperator.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\QueryBuilder\Model\Operator;
+
+use WBW\Library\QueryBuilder\Model\QueryBuilderRuleInterface;
+use WBW\Library\QueryBuilder\Model\Type\ArrayQueryBuilderType;
+
+/**
+ * In QueryBuilder operator.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\QueryBuilder\Model\Operator
+ */
+class InQueryBuilderOperator extends AbstractQueryBuilderOperator {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct(self::OPERATOR_IN);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function toSql(QueryBuilderRuleInterface $rule, bool $wrap = false): string {
+
+        $qbt = new ArrayQueryBuilderType();
+        $tmp = $qbt->toSql($rule, true);
+
+        $sql = [
+            parent::toSql($rule, $wrap),
+            sprintf("(%s)", str_replace(self::DEFAULT_SEPARATOR, ", ", $tmp)),
+        ];
+
+        return implode(" ", $sql);
+    }
+}
