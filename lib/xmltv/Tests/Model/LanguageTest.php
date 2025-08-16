fdba7cdc3631662a5ed4898c306a36bdc93e1284
webeweb <support@webeweb.fr>
2025-08-16 16:32:31 +0200

    Add Language model (copied from xmltv library)

---
+++ lib/xmltv/Tests/Model/LanguageTest.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Library\XmlTv\Tests\Model;
+
+use WBW\Library\XmlTv\Model\Language;
+use WBW\Library\XmlTv\Tests\AbstractTestCase;
+
+/**
+ * Language test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\XmlTv\Tests\Model
+ */
+class LanguageTest extends AbstractTestCase {
+
+    /**
+     * Test jsonSerialize()
+     *
+     * @return void
+     */
+    public function testJsonSerialize(): void {
+
+        $obj = new Language();
+        $obj->setContent("content");
+        $obj->setLang("lang");
+
+        $res = $obj->jsonSerialize();
+        $this->assertCount(2, $res);
+
+        $this->assertArrayHasKey("lang", $res);
+        $this->assertArrayHasKey("content", $res);
+    }
+
+    /**
+     * Test xmlSerialize()
+     *
+     * @return void
+     */
+    public function testXmlSerialize(): void {
+
+        $obj = new Language();
+        $obj->setContent("content");
+        $obj->setLang("lang");
+
+        $res = '<language lang="lang">content</language>';
+        $this->assertEquals($res, $obj->xmlSerialize());
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("language", Language::DOM_NODE_NAME);
+
+        $obj = new Language();
+
+        $this->assertNull($obj->getContent());
+        $this->assertNull($obj->getLang());
+    }
+}
