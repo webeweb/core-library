d5c427c181a75efcbe77f8c23e5530324ebac6f7
webeweb <support@webeweb.fr>
2025-08-16 16:18:57 +0200

    Add Editor model (copied from xmltv library)

---
+++ lib/xmltv/Tests/Model/EditorTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Library\XmlTv\Tests\Model;
+
+use WBW\Library\XmlTv\Model\Editor;
+use WBW\Library\XmlTv\Tests\AbstractTestCase;
+
+/**
+ * Editor test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\XmlTv\Tests\Model
+ */
+class EditorTest extends AbstractTestCase {
+
+    /**
+     * Test xmlSerialize()
+     *
+     * @return void
+     */
+    public function testXmlSerialize(): void {
+
+        $obj = new Editor();
+        $obj->setContent("content");
+
+        $res = '<editor>content</editor>';
+        $this->assertEquals($res, $obj->xmlSerialize());
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("editor", Editor::DOM_NODE_NAME);
+
+        $obj = new Editor();
+
+        $this->assertNull($obj->getContent());
+    }
+}
