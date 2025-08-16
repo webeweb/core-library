128444e8f5e01463afc1119f55d6df543a3c88c5
webeweb <support@webeweb.fr>
2025-08-16 16:38:17 +0200

    Add URL model (copied from xmltv library)

---
+++ lib/xmltv/Model/Url.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Library\XmlTv\Model;
+
+use WBW\Library\Common\Traits\Strings\StringContentTrait;
+use WBW\Library\XmlTv\Serializer\JsonSerializer;
+use WBW\Library\XmlTv\Serializer\XmlSerializer;
+
+/**
+ * URL.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\XmlTv\Model
+ */
+class Url extends AbstractModel {
+
+    use StringContentTrait;
+
+    /**
+     * DOM node name.
+     *
+     * @var string
+     */
+    public const DOM_NODE_NAME = "url";
+
+    /**
+     * {@inheritDoc}
+     * @return array<string,mixed> Returns this serialized instance.
+     */
+    public function jsonSerialize(): array {
+        return JsonSerializer::serializeUrl($this);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function xmlSerialize(): string {
+        return XmlSerializer::serializeUrl($this);
+    }
+}
