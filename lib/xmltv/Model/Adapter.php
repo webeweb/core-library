0156839c71d2cbd3737f1e8ba372859b059ef357
webeweb <support@webeweb.fr>
2025-08-16 16:13:34 +0200

    Add Adapter model (copied from xmltv library)

---
+++ lib/xmltv/Model/Adapter.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Library\XmlTv\Model;
+
+use WBW\Library\XmlTv\Serializer\XmlSerializer;
+
+/**
+ * Adapter.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\XmlTv\Model
+ */
+class Adapter extends AbstractCredit {
+
+    /**
+     * DOM node name.
+     *
+     * @var string
+     */
+    public const DOM_NODE_NAME = "adapter";
+
+    /**
+     * {@inheritDoc}
+     */
+    public function xmlSerialize(): string {
+        return XmlSerializer::serializeAdapter($this);
+    }
+}
