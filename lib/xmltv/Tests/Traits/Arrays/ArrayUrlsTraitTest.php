877004761081a9cc142b471fd2c8e45ac72ebc31
webeweb <support@webeweb.fr>
2025-08-16 16:48:22 +0200

    Add array URLs trait (copied from xmltv library)

---
+++ lib/xmltv/Tests/Traits/Arrays/ArrayUrlsTraitTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Library\XmlTv\Tests\Traits\Arrays;
+
+use WBW\Library\XmlTv\Model\Url;
+use WBW\Library\XmlTv\Tests\AbstractTestCase;
+use WBW\Library\XmlTv\Tests\Fixtures\Traits\Arrays\TestArrayUrlsTrait;
+
+/**
+ * Array URLs trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\XmlTv\Tests\Traits\Arrays
+ */
+class ArrayUrlsTraitTest extends AbstractTestCase {
+
+    /**
+     * Test addUrl()
+     *
+     * @return void
+     */
+    public function testAddUrl(): void {
+
+        // Set an URL mock.
+        $url = new Url();
+
+        $obj = new TestArrayUrlsTrait();
+
+        $obj->addUrl($url);
+        $this->assertSame($url, $obj->getUrls()[0]);
+        $this->assertEquals(1, $obj->getNumberUrls());
+        $this->assertTrue($obj->hasUrls());
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new TestArrayUrlsTrait();
+
+        $this->assertEquals([], $obj->getUrls());
+        $this->assertEquals(0, $obj->getNumberUrls());
+        $this->assertFalse($obj->hasUrls());
+    }
+}
---
+++ lib/xmltv/Traits/Arrays/ArrayUrlsTrait.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Library\XmlTv\Traits\Arrays;
+
+use WBW\Library\XmlTv\Model\Url;
+
+/**
+ * Array URLs trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\XmlTv\Traits\Arrays
+ */
+trait ArrayUrlsTrait {
+
+    /**
+     * URLs.
+     *
+     * @var Url[]
+     */
+    protected $urls;
+
+    /**
+     * Add an URL.
+     *
+     * @param Url $url The URL.
+     * @return self Returns this instance.
+     */
+    public function addUrl(Url $url): self {
+        $this->urls[] = $url;
+        return $this;
+    }
+
+    /**
+     * Get the number of URLs.
+     *
+     * @return int Returns the number of URLs.
+     */
+    public function getNumberUrls(): int {
+        return count($this->getUrls());
+    }
+
+    /**
+     * Get the URLs.
+     *
+     * @return Url[] Returns the URLs.
+     */
+    public function getUrls(): array {
+        return $this->urls;
+    }
+
+    /**
+     * Determine if this instance has URLs.
+     *
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    public function hasUrls(): bool {
+        return 1 <= $this->getNumberUrls();
+    }
+
+    /**
+     * Set the URLs.
+     *
+     * @param Url[] $urls The URLs.
+     * @return self Returns this instance.
+     */
+    protected function setUrls(array $urls): self {
+        $this->urls = $urls;
+        return $this;
+    }
+}
