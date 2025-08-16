67c6c9e0ff3395eea0946b83701015ed05f9cc30
webeweb <support@webeweb.fr>
2025-08-16 16:15:40 +0200

    Add Country model (copied from xmltv library)

---
+++ lib/xmltv/Tests/Model/CountryTest.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Library\XmlTv\Tests\Model;
+
+use WBW\Library\XmlTv\Model\Country;
+use WBW\Library\XmlTv\Tests\AbstractTestCase;
+
+/**
+ * Country test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\XmlTv\Tests\Model
+ */
+class CountryTest extends AbstractTestCase {
+
+    /**
+     * Test jsonSerialize()
+     *
+     * @return void
+     */
+    public function testJsonSerialize(): void {
+
+        $obj = new Country();
+        $obj->setContent("content");
+        $obj->setLang("lang");
+
+        $res = $obj->jsonSerialize();
+        $this->assertCount(2, $res);
+
+        $this->assertArrayHasKey("lang", $res);
+        $this->assertArrayHasKey("content", $res);
+    }
+
+    /**
+     * Test xmlSerialize()
+     *
+     * @return void
+     */
+    public function testXmlSerialize(): void {
+
+        $obj = new Country();
+        $obj->setContent("content");
+        $obj->setLang("lang");
+
+        $res = '<country lang="lang">content</country>';
+        $this->assertEquals($res, $obj->xmlSerialize());
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("country", Country::DOM_NODE_NAME);
+
+        $obj = new Country();
+
+        $this->assertNull($obj->getContent());
+        $this->assertNull($obj->getLang());
+    }
+}
