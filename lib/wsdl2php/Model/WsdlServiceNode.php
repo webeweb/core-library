1a498c4dbe509ff256d04a265c8cc5d1199db551
webeweb <support@webeweb.fr>
2025-10-01 08:42:22 +0200

    Add wsdl:service node

---
+++ lib/wsdl2php/Model/WsdlServiceNode.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Library\Wsdl2Php\Model;
+
+use DOMNode;
+use WBW\Library\Common\Traits\Strings\StringNameTrait;
+
+/**
+ * wsdl:service node.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Wsdl2Php\Model
+ */
+class WsdlServiceNode extends AbstractNode implements IndexedNode {
+
+    use StringNameTrait;
+
+    /**
+     * Node name.
+     *
+     * @var string
+     */
+    const NODE_NAME = "wsdl:service";
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getIndexName(): ?string {
+        return $this->getName();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function init(DOMNode $domNode): void {
+        $this->setName($this->getAttributeValue($domNode, "name"));
+    }
+}
