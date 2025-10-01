0fedbd57c2df15cb1b68403f0665df20c911d991
webeweb <support@webeweb.fr>
2025-10-01 08:30:58 +0200

    Add soap12:operation node

---
+++ lib/wsdl2php/Model/Soap12OperationNode.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the core-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Library\Wsdl2Php\Model;
+
+/**
+ * soap12:operation node.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Wsdl2Php\Model
+ */
+class Soap12OperationNode extends SoapOperationNode {
+
+    /**
+     * Node name.
+     *
+     * @var string
+     */
+    const NODE_NAME = "soap12:operation";
+}
